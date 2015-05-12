8b3430406b50ff49ac437e87df3d19301ca19825
Rasmus Lelumees <rasmus.lelumees@gmail.com>
2015-05-12 15:22:04 +0300

    Add database scripts to create database and add test data.

---
+++ src/test/resources/import.sql
@@ -0,0 +1,5 @@
+-- Materials
+
+insert into material(id, title) values(1, 'Mathematics textbook for 9th grade');
+insert into material(id, title) values(2, 'Mathematics textbook for 8th grade');
+insert into material(id, title) values(3, 'Mathematics textbook for 7th grade');
