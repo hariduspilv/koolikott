c1e9614e83764d7ac81f46a3bbf4b3f201f057c1
jordan <jordan@netgroup.ee>
2015-05-14 16:37:50 +0300

    Creating Transaction manager

--- src/main/java/ee/hm/dop/dao/MaterialDAO.java
+++ src/main/java/ee/hm/dop/dao/MaterialDAO.java
@@ -8,18 +8,11 @@ import javax.persistence.EntityManager;
 import ee.hm.dop.model.Material;
 
 public class MaterialDAO {
-	
-	protected EntityManager entityManager;
-	  
-	@Inject
-	public MaterialDAO(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
-	
-	public List<Material> getAllMaterials() {
-		entityManager.getTransaction().begin();
-		List<Material> resultList = entityManager.createQuery("from Material", Material.class).getResultList();
-		entityManager.getTransaction().commit();
-		return resultList;
-	}
+
+    @Inject
+    protected EntityManager entityManager;
+
+    public List<Material> getAllMaterials() {
+        return entityManager.createQuery("from Material", Material.class).getResultList();
+    }
 }
