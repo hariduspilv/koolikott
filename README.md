db8b34f7bbbaf693f4a95672b687bfdeb51d86bb
jordan <jordan@netgroup.ee>
2015-05-20 16:56:43 +0300

    Adding the possibility to override default database configuration

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# DOP
+# eKoolikott
+
+# Installation
+
+No need to install, just download dop.jar and you are ready to go!
+
+# Start application
+
+By default, application will run at port 8080. The rest API is available under context path **rest/**. After starting application you can access it at:
+
+	http://localhost:8080/rest/path/to/resource
+
+## Default configuration
+
+	java -jar dop.jar
+	
+## Custom configuration
+
+The custom configuration file can be placed anywhere visible to the application and may have any name.
+
+	jave -jar -Dconfig="/path/to/custom.properties"
+
+# Configuration
+
+The application can be configured using a custom properties file. It can be placed anywhere visible to the application and may have any name.
+
+The available properties are:
+
+### Database
+
+* **db.url** - the database url
+* **db.username** - the database username
+* **db.password** - the database password for given username 
--- src/main/java/ee/hm/dop/guice/provider/ConfigurationProvider.java
+++ src/main/java/ee/hm/dop/guice/provider/ConfigurationProvider.java
@@ -1,7 +1,11 @@
 package ee.hm.dop.guice.provider;
 
+import static java.lang.String.format;
+
+import java.io.File;
 import java.net.URL;
 
+import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
@@ -19,19 +23,60 @@ public class ConfigurationProvider implements Provider<Configuration> {
     private static Logger logger = LoggerFactory.getLogger(ConfigurationProvider.class);
     private static String DEFAULT_CONFIGURATION_FILE_NAME = "default.properties";
 
-    private Configuration configuration;
+    private CompositeConfiguration configuration;
 
     @Override
     public synchronized Configuration get() {
 
         if (configuration == null) {
+            init();
+        }
+
+        return configuration;
+    }
+
+    private void init() {
+        configuration = new CompositeConfiguration();
+
+        Configuration customConfiguration = loadCustomConfiguration();
+        if (customConfiguration != null) {
+            configuration.addConfiguration(customConfiguration);
+        }
+
+        configuration.addConfiguration(loadDefaultConfiguration());
+    }
+
+    private Configuration loadCustomConfiguration() {
+        Configuration configuration = null;
+
+        String configurationPath = System.getProperty("config");
+        if (configurationPath != null) {
+            logger.info(format("Loading custom configuration file from [%s].", configurationPath));
+
             try {
-                URL resource = getClass().getClassLoader().getResource(DEFAULT_CONFIGURATION_FILE_NAME);
-                configuration = new PropertiesConfiguration(resource);
-                logger.info(String.format("Default configuration loaded from [%s]", resource.toExternalForm()));
+                File config = new File(configurationPath);
+                configuration = new PropertiesConfiguration(config);
+                logger.info(format("Custom configuration loaded from [%s]", config.getAbsolutePath()));
             } catch (Exception e) {
-                throw new RuntimeException("Unable to load default configuration!", e);
+                throw new RuntimeException("Unable to load custom configuration!", e);
             }
+        } else {
+            logger.info("No custom configuration file set.");
+        }
+
+        return configuration;
+    }
+
+    private Configuration loadDefaultConfiguration() {
+        Configuration configuration;
+
+        try {
+            URL resource = getClass().getClassLoader().getResource(DEFAULT_CONFIGURATION_FILE_NAME);
+            configuration = new PropertiesConfiguration(resource);
+            logger.info(String.format("Default configuration loaded from [%s]", resource.toExternalForm()));
+
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load default configuration!", e);
         }
 
         return configuration;
