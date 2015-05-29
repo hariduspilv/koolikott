a6d7ada4055c208fe3a3f4ad9972a436b244c5a8
silvermuru <silx99@gmail.com>
2015-05-29 14:25:21 +0300

    #95162540 Material list page link to material page

--- app/search/result/searchResult.js
+++ app/search/result/searchResult.js
@@ -1,20 +1,23 @@
 define(['app'], function(app)
 {
-    app.controller('searchResultController', ['$scope', "serverCallService", function($scope, serverCallService) {
+    app.controller('searchResultController', ['$scope', "serverCallService", '$location', function($scope, serverCallService, $location) {
     	var params = {};
     	serverCallService.makeGet("/rest/material/getAll", params, getAllMaterialSuccess, getAllMaterialFail);
     	
     	function getAllMaterialSuccess(data) {
 
-    		if (isEmpty(data)) {
-    			log('No data returned by session search.');
-			} else {
-				$scope.materials = data;
-			}
+            if (isEmpty(data)) {
+                log('No data returned by session search.');
+                } else {
+                        $scope.materials = data;
+                }
     	}
     	
     	function getAllMaterialFail(data, status) {
-    		console.log('Session search failed.')
+            console.log('Session search failed.')
     	}
+        $scope.showDetails = function(detainee) {
+            $location.path('/material');
+        }
     }]);
 });
