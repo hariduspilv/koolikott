b51aaf5ab22fe868a2599b656f3d110cf02b7a5f
jordan <jordan@netgroup.ee>
2015-06-02 15:27:06 +0300

    Refactoring: moving all view folders to views directory.

--- app/routes.js
+++ app/routes.js
@@ -4,14 +4,14 @@ define([], function()
         defaultRoutePath: '/',
         routes: {
             '/': {
-                templateUrl: 'app/home/home.html',
+                templateUrl: 'app/views/home/home.html',
                 controller: 'homeController',
                 dependencies: [
                     'home/home'
                 ]
             },
             '/search/result': {
-                templateUrl: 'app/search/result/searchResult.html',
+                templateUrl: 'app/views/search/result/searchResult.html',
                 controller: 'searchResultController',
                 dependencies: [
                     'search/result/searchResult',
@@ -20,7 +20,7 @@ define([], function()
                 ]
             },
             '/material': {
-                templateUrl: 'app/material/material.html',
+                templateUrl: 'app/views/material/material.html',
                 controller: 'materialController',
                 dependencies: [
                     'material/material'
