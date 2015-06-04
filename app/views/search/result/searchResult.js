93bdfba9a3d1ba76a14d2e8826c4ef6ddd333373
jordan <jordan@netgroup.ee>
2015-06-04 18:08:43 +0300

    #95694026 - Showing IssueDate at Search Result page Material box

--- app/views/search/result/searchResult.js
+++ app/views/search/result/searchResult.js
@@ -1,6 +1,7 @@
 define(['app'], function(app)
 {
-    app.controller('searchResultController', ['$scope', "serverCallService", function($scope, serverCallService) {
+    app.controller('searchResultController', ['$scope', "serverCallService", "$filter", 
+             function($scope, serverCallService, $filter) {
     	var params = {};
     	serverCallService.makeGet("rest/material/getAll", params, getAllMaterialSuccess, getAllMaterialFail);
     	
@@ -15,6 +16,35 @@ define(['app'], function(app)
     	
     	function getAllMaterialFail(data, status) {
             console.log('Session search failed.')
+    	}
+    	
+    	$scope.formatIssueDate = function(issueDate) {
+    		if (!issueDate) {
+    			return;
+    		}
+    		
+    		if (issueDate.day && issueDate.month && issueDate.year) {
+    			// full date
+    			return formatDay(issueDate.day) + "." + formatMonth(issueDate.month) + "." + formatYear(issueDate.year); 
+    		} else if (issueDate.month && issueDate.year) {
+    			// month date
+    			return formatMonth(issueDate.month) + "." + formatYear(issueDate.year); 
+    		} else if (issueDate.year) {
+    			// year date
+    			return formatYear(issueDate.year); 
+    		}
+    	}
+    	
+    	function formatDay(day) {
+    		return day > 9 ? "" + day : "0" + day; 
+    	}
+    	
+    	function formatMonth(month) {
+    		return month > 9 ? "" + month : "0" + month; 
+    	}
+    	
+    	function formatYear(year) {
+    		return year < 0 ? year * -1 : year; 
     	}
     }]);
 });
