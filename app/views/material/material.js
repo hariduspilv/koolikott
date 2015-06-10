fb2152fbe24f573c2fdfb4b64e29d9174d20c576
Mart Laus <mart.laus@netgroup.ee>
2015-06-10 16:06:00 +0300

    #95226490 connects to back end

--- app/views/material/material.js
+++ app/views/material/material.js
@@ -1,6 +1,24 @@
 define(['app'], function(app)
 {
-    app.controller('materialController', ['$scope', function($scope) {
-            
+    app.controller('materialController', ['$scope', "serverCallService", "$filter", '$route',
+    		 function($scope, serverCallService, $filter, $route) {
+    	console.log($route.current.params);
+    	var materialId = $route.current.params.materialId;
+        var params = {};
+    	serverCallService.makeGet("rest/material/find?materialId=" + materialId, params, getAllMaterialSuccess, getAllMaterialFail);
+
+    	function getAllMaterialSuccess(data) {
+
+            if (isEmpty(data)) {
+                log('No data returned by session search.');
+                } else {
+                        $scope.material = data;
+                        console.log(data);
+                }
+    	}
+    	
+    	function getAllMaterialFail(data, status) {
+            console.log('Session search failed.')
+    	}
     }]);
 });
