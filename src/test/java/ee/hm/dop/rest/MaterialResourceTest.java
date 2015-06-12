8f3a3fb28ba8f340dbb47ecc7a032bacc60846c0
Mart Laus <mart.laus@netgroup.ee>
2015-06-12 16:06:02 +0300

    #96865462 added the language variable to the material class.

--- src/test/java/ee/hm/dop/rest/MaterialResourceTest.java
+++ src/test/java/ee/hm/dop/rest/MaterialResourceTest.java
@@ -1,20 +1,19 @@
 package ee.hm.dop.rest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import ee.hm.dop.common.test.ResourceIntegrationTestBase;
+import ee.hm.dop.model.Author;
+import ee.hm.dop.model.IssueDate;
+import ee.hm.dop.model.LanguageString;
+import ee.hm.dop.model.Material;
+import org.junit.Test;
 
 import java.util.List;
 
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
-import org.junit.Test;
-
-import ee.hm.dop.common.test.ResourceIntegrationTestBase;
-import ee.hm.dop.model.Author;
-import ee.hm.dop.model.IssueDate;
-import ee.hm.dop.model.LanguageString;
-import ee.hm.dop.model.Material;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MaterialResourceTest extends ResourceIntegrationTestBase {
 
@@ -61,7 +60,7 @@ public class MaterialResourceTest extends ResourceIntegrationTestBase {
         assertEquals(2, descriptions.size());
         for (LanguageString languageString : descriptions) {
             if (languageString.getId() == 1) {
-                assertEquals("est", languageString.getLanguage());
+                assertEquals("est", languageString.getLanguage().getCode());
                 assertEquals("test description in estonian", languageString.getText());
             } else if (languageString.getId() == 2) {
                 assertEquals("est", languageString.getLanguage());
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -12,7 +12,7 @@ insert into IssueDate(id, month, year) values(7, 3, 1991);
 
 insert into Material(id, title, lang, issueDate) values(1, 'Matemaatika õpik üheksandale klassile', 'est', 1);
 insert into Material(id, title, lang, issueDate) values(2, '?????????? ??????? ??? 8-?? ??????', 'rus', 2);
-insert into Material(id, title, lang, issueDate) values(3, '8???????? ????? ????????  ???? 6', 'ara', 3);
+insert into Material(id, title, lang, issueDate) values(3, '8???????? ????? ???????? ???? 6', 'ara', 3);
 insert into Material(id, title, lang, issueDate) values(4, 'Mathematics textbook for 6th grade', 'eng', 4);
 insert into Material(id, title, lang,issueDate) values(5, 'Mathematics textbook for 5th grade', 'eng', 5);
 insert into Material(id, title) values(6, 'Mathematics textbook for 4th grade');
@@ -42,6 +42,8 @@ insert into Material_Author(material, author) values(8, 2);
 insert into LanguageTable(id, name, code) values (1, 'Estonian', 'est');
 insert into LanguageTable(id, name, code) values (2, 'Russian', 'rus');
 insert into LanguageTable(id, name, code) values (3, 'Portuguese', 'por');
+insert into LanguageTable(id, name, code) values (4, 'Arabic', 'ara');
+insert into LanguageTable(id, name, code) values (5, 'English', 'ara');
 
 -- LanguageKeyCodes
 
@@ -52,8 +54,8 @@ insert into LanguageKeyCodes(lang, code) values (3, 'pt-br');
 
 -- LanguageDescription
 
-insert into LanguageString(id, lang, textValue, material) values (1, 'est', 'test description in estonian', 1);
-insert into LanguageString(id, lang, textValue, material) values (2, 'rus', 'test description in russian', 2);
-insert into LanguageString(id, lang, textValue, material) values (3, 'rus', 'test description in russian', 1);
-insert into LanguageString(id, lang, textValue, material) values (4, 'eng', 'test description in english', 3);
-insert into LanguageString(id, lang, textValue, material) values (5, 'ara', 'test description in arabic', 3);
+insert into LanguageString(id, lang, textValue, material) values (1, 1, 'test description in estonian', 1);
+insert into LanguageString(id, lang, textValue, material) values (2, 2, 'test description in russian', 2);
+insert into LanguageString(id, lang, textValue, material) values (3, 2, 'test description in russian', 1);
+insert into LanguageString(id, lang, textValue, material) values (4, 5, 'test description in english', 3);
+insert into LanguageString(id, lang, textValue, material) values (5, 4, 'test description in arabic', 3);
