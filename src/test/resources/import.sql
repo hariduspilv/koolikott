ab411b709ee249b6aaa4c947996681c63f4c1d03
Mart Laus <mart.laus@netgroup.ee>
2015-06-16 12:09:57 +0300

    #96982358 updated the translationgroup language id in import

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -76,7 +76,7 @@ insert into ResourceType(id, material, resourceType) values (5, 4, 'role play');
 
 insert into TranslationGroup(id, lang) values (1, 1);
 insert into TranslationGroup(id, lang) values (2, 2);
-insert into TranslationGroup(id, lang) values (3, 5);
+insert into TranslationGroup(id, lang) values (3, 3);
 
 -- Translation
 
