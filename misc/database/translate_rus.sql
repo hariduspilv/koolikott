6dc46ba85500d0a61d0bd83583a7da9ab2e46b71
silvermuru <silx99@gmail.com>
2015-06-16 11:44:44 +0300

    #96982200 Russian translations added (SQL)

---
+++ misc/database/translate_rus.sql
@@ -0,0 +1,17 @@
+-- Russian translations
+insert into Translation(translationGroup, translationKey, translation) values (2, 'Login', 'Войти');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'SUBJECT', 'Тема');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'TYPE', 'Тип');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'EDUCATIONAL_CONTEXT', 'Возрастные группы');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'UPDATED', 'Новый');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'ADDED', 'Добавленной');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'CREATED', 'Созданный');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'REPOSITORY_NUMBER', 'Количество хранилище');
+insert into Translation(translationGroup, translationKey, translation) values (2, 'ALL_RIGHTS_RESERVED', 'Все права защищены');
+
+
+
+
+
+
+
--- misc/database/translationGroups.sql
+++ misc/database/translationGroups.sql
@@ -1,1 +1,2 @@
 insert into TranslationGroup(id, lang) values (1, 1);
+insert into TranslationGroup(id, lang) values (2, 2);
