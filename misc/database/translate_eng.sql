da51b76db36a2048895f847b439dfa08675aed23
Mart Laus <mart.laus@netgroup.ee>
2015-06-16 10:01:16 +0300

    #96982358  Added the english translation file and english to the translation groups.

---
+++ misc/database/translate_eng.sql
@@ -0,0 +1,10 @@
+-- English translations
+insert into Translation(translationGroup, translationKey, translation) values (3, 'Login', 'Login');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'SUBJECT', 'Subject');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'TYPE', 'Type');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'EDUCATIONAL_CONTEXT', 'Educational context');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'UPDATED', 'Updated');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'ADDED', 'Added');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'CREATED', 'Created');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'REPOSITORY_NUMBER', 'Repository number');
+insert into Translation(translationGroup, translationKey, translation) values (3, 'ALL_RIGHTS_RESERVED', 'All rights reserved');
--- misc/database/translationGroups.sql
+++ misc/database/translationGroups.sql
@@ -1,1 +1,2 @@
 insert into TranslationGroup(id, lang) values (1, 1);
+insert into TranslationGroup(id, lang) values (3, 5);
