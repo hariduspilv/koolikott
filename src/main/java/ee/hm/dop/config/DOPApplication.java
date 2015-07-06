47a8c3cdd3daee215bdb25833b57844481f7c1b1
jordan <jordan@netgroup.ee>
2015-07-06 18:35:30 +0300

    #95300932 - Rollback changes to fix problem at Jenkins.

--- src/main/java/ee/hm/dop/config/DOPApplication.java
+++ src/main/java/ee/hm/dop/config/DOPApplication.java
@@ -3,11 +3,13 @@ package ee.hm.dop.config;
 import javax.inject.Inject;
 
 import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 
 import ee.hm.dop.guice.GuiceInjector;
+import ee.hm.dop.guice.provider.ObjectMapperProvider;
 
 public class DOPApplication extends ResourceConfig {
 
@@ -20,5 +22,8 @@ public class DOPApplication extends ResourceConfig {
 
         GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
         guiceBridge.bridgeGuiceInjector(GuiceInjector.getInjector());
+
+        register(JacksonFeature.class);
+        register(ObjectMapperProvider.class);
     }
 }
