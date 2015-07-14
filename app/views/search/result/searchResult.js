be8927548e6b51fec5aad66d690fc378716e6aea
jordan <jordan@netgroup.ee>
2015-07-14 11:35:38 +0300

    #95300932 - Searching when user gets to search result page

--- app/views/search/result/searchResult.js
+++ app/views/search/result/searchResult.js
@@ -1,9 +1,13 @@
 define(['app'], function(app)
 {
-    app.controller('searchResultController', ['$scope', "serverCallService", 'translationService',
-             function($scope, serverCallService, translationService) {
-    	var params = {};
-    	serverCallService.makeGet("rest/material/getAll", params, getAllMaterialSuccess, getAllMaterialFail);
+    app.controller('searchResultController', ['$scope', "serverCallService", 'translationService', '$location',
+             function($scope, serverCallService, translationService, $location) {
+    	var searchObject = $location.search();
+    	
+    	if (searchObject.q) {
+	    	var params = {'q': searchObject.q};
+	    	serverCallService.makeGet("rest/search", params, getAllMaterialSuccess, getAllMaterialFail);
+    	}
     	
     	function getAllMaterialSuccess(data) {
 
