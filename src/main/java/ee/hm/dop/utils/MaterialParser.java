b6989fab4935630229507495c2570f76d111c5cf
Mart Laus <mart.laus@netgroup.ee>
2015-07-15 11:48:54 +0300

    #97184746  Added material parser class

---
+++ src/main/java/ee/hm/dop/utils/MaterialParser.java
@@ -0,0 +1,16 @@
+package ee.hm.dop.utils;
+
+import org.w3c.dom.Document;
+
+import ee.hm.dop.model.Material;
+
+/**
+ * Created by mart.laus on 14.07.2015.
+ */
+public class MaterialParser {
+
+    public Material parseXMLtoMaterial(Document doc) {
+
+        return new Material();
+    }
+}
