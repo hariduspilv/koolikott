9992e2809f2005966170dc219a276265d71263ed
Rasmus Lelumees <rasmus.lelumees@gmail.com>
2015-07-21 13:31:58 +0300

    #99017918 No result message for search

--- app/views/search/result/searchResult.js
+++ app/views/search/result/searchResult.js
@@ -3,23 +3,26 @@ define(['app'], function(app)
     app.controller('searchResultController', ['$scope', "serverCallService", 'translationService', '$location',
              function($scope, serverCallService, translationService, $location) {
     	var searchObject = $location.search();
-    	
+
     	if (searchObject.q) {
+            $scope.searching = true;
+            $scope.searchQuery = searchObject.q;
 	    	var params = {'q': searchObject.q};
 	    	serverCallService.makeGet("rest/search", params, getAllMaterialSuccess, getAllMaterialFail);
     	}
     	
     	function getAllMaterialSuccess(data) {
-
             if (isEmpty(data)) {
                 log('No data returned by session search.');
-                } else {
-                        $scope.materials = data;
-                }
+            } else {
+                $scope.materials = data;
+            }
+            $scope.searching = false;
     	}
     	
     	function getAllMaterialFail(data, status) {
             console.log('Session search failed.')
+            $scope.searching = false;
     	}
     }]);
 });
