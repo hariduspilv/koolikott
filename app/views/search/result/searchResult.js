95c0f79a043e3b9771ef47b89b2148a1d35e2bb8
oliver <oliver.eivak@gmail.com>
2015-07-23 15:46:51 +0300

    #95300904 - Add logic to pagination buttons to show correct numbers

--- app/views/search/result/searchResult.js
+++ app/views/search/result/searchResult.js
@@ -3,6 +3,7 @@ define(['app'], function(app)
     app.controller('searchResultController', ['$scope', "serverCallService", 'translationService', '$location', '$rootScope',
              function($scope, serverCallService, translationService, $location, $rootScope) {
     	var searchObject = $location.search();
+        $scope.paging = [];
 
     	if (searchObject.q) {
             $scope.searching = true;
@@ -18,7 +19,9 @@ define(['app'], function(app)
 	    	serverCallService.makeGet("rest/search", params, getAllMaterialSuccess, getAllMaterialFail);
 	    	serverCallService.makeGet("rest/search/countResults", params, getResultCountSuccess, getResultCountFail);
 	    	$rootScope.searchFields.searchQuery = searchObject.q;
-    	}
+    	} else {
+            $location.url('/');
+        }
     	
     	function getAllMaterialSuccess(data) {
             if (isEmpty(data)) {
@@ -39,6 +42,7 @@ define(['app'], function(app)
                 log('No result count returned.');
             } else {
                 $scope.resultCount = data;
+                $scope.calculatePaging();
             }
     	}
     	
@@ -56,6 +60,73 @@ define(['app'], function(app)
             }
 
             return $scope.materials.length;
+        }
+
+        function pushPageNumbers(targetArray, from, to) {
+            for (i = from; i < to; i++) {
+                targetArray.push(i);
+            }
+            return to - from;
+        }
+
+        $scope.calculatePaging = function() {
+            $scope.paging = [];
+            $scope.paging.before = [];
+            $scope.paging.thisPage = 1;
+            $scope.paging.after = [];
+            $scope.paging.last = [];
+
+            if (!$scope.resultCount) {
+                return;
+            }
+
+            var RESULTS_PER_PAGE = 24;
+            var pageCount = Math.ceil($scope.resultCount / RESULTS_PER_PAGE);
+
+            var start = 0;
+            if (searchObject.start) {
+                start = searchObject.start;
+            }
+            var thisPage = (start / RESULTS_PER_PAGE) + 1;
+            if (thisPage > pageCount) {
+                thisPage = pageCount;
+            }
+            $scope.paging.thisPage = thisPage;
+
+            var MAX_PAGES_BEFORE_DOTS = 6;
+            var MAX_PAGES_AFTER_DOTS = 3;
+            var OPTIMAL_PAGES_BEFORE_THIS_PAGE = 2;
+            var MAX_PAGES = MAX_PAGES_BEFORE_DOTS + MAX_PAGES_AFTER_DOTS + 1;
+
+            var pagesBeforeDots = 0;
+
+            if (pageCount <= MAX_PAGES) {
+                // Display all page numbers
+                pushPageNumbers($scope.paging.before, 1, thisPage);
+                pushPageNumbers($scope.paging.after, thisPage + 1, pageCount + 1);
+
+            } else if (pageCount - (thisPage - OPTIMAL_PAGES_BEFORE_THIS_PAGE) < MAX_PAGES) {
+                // Display the last MAX_PAGES amount of pages
+                var pagesBeforeThisPage = MAX_PAGES - (pageCount - thisPage) - 1;
+                var pagesAfterThisPage = MAX_PAGES - pagesBeforeThisPage - 1;
+
+                pushPageNumbers($scope.paging.before, thisPage - pagesBeforeThisPage, thisPage);
+                pushPageNumbers($scope.paging.after, thisPage + 1, thisPage + 1 + pagesAfterThisPage);
+
+            } else {
+                // Display pages, dots and more pages
+                if (thisPage <= OPTIMAL_PAGES_BEFORE_THIS_PAGE) {
+                    pagesBeforeDots += pushPageNumbers($scope.paging.before, 1, thisPage);
+                } else {
+                    pagesBeforeDots += pushPageNumbers($scope.paging.before, thisPage - OPTIMAL_PAGES_BEFORE_THIS_PAGE, thisPage);
+                }
+
+                var pagesAfterThisPage = MAX_PAGES_BEFORE_DOTS - (pagesBeforeDots + 1);
+
+                pushPageNumbers($scope.paging.after, thisPage + 1, thisPage + 1 + pagesAfterThisPage);
+                pushPageNumbers($scope.paging.last,  pageCount + 1 - MAX_PAGES_AFTER_DOTS, pageCount + 1);
+
+            }
         }
     	
     	$scope.$on("$destroy", function() {
