93984db24c1a1e2006f0afe8a3260049cc49db61
Mart Laus <mart.laus@netgroup.ee>
2015-08-06 11:58:17 +0300

    #100498378 added logic to parse plus sign in search

--- app/services/searchService.js
+++ app/services/searchService.js
@@ -3,30 +3,38 @@ define(['app'], function(app) {
 	app.factory('searchService',['$location', '$rootScope', function($location, $rootScope) {
 		var searchURLbase = "search/result?q=";
 		var searchQuery = "";
-		var searchURL = "";
 		
 		return {
 			
 			setSearch : function(query) {
 				searchQuery = query;
-				searchURL = searchURLbase + query;
 				$rootScope.searchFields.searchQuery = query;
 			},
 			
 	        getURL : function() {
-	        	return searchURL;
+	        	var query = searchQuery
+	        	//replace backslashes
+                query = query.replace(/\\/g, "\\\\");
+
+                //make plus signs into \+
+                query = query.replace(/\+/g, "\\+");
+	        	return searchURLbase + query;
 	        },
 
 	        getQuery : function() {
-	        	if(searchQuery === ""){
-	        		var searchObject = $location.search();
-                 	if (searchObject.q) {
-                    	searchQuery = searchObject.q;
-                 	}
-	        	}
+				if(searchQuery === ""){
+					var searchObject = $location.search();
+					if (searchObject.q) {
+						//get back + sign
+               			query = searchObject.q.replace(/\\ /g, "+");
+                		//make backslashes singular
+                		query = query.replace(/\\\\/g, "\\");
+						searchQuery = query;
+					}
+				}
 
-	        	return searchQuery;
-	        },
+				return searchQuery;
+			},
 
             getPage : function() {
                 var searchObject = $location.search();
