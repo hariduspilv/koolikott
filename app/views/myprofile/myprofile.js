fa37e81dcd2f1df24ea907468a9d61355e9e13e8
oliver <oliver.eivak@gmail.com>
2015-08-13 16:05:13 +0300

    #96062580 - Remove unnecessary methods from profile

--- app/views/myprofile/myprofile.js
+++ app/views/myprofile/myprofile.js
@@ -1,23 +1,9 @@
 define(['app'], function(app)
 {
-    app.controller('myprofileController', ['$scope', 'serverCallService', '$route', 'translationService',
-        function($scope, serverCallService, $route, translationService) {
+    app.controller('myprofileController', ['$scope', 
+        function($scope) {
         
-        console.log("username: " + $route.current.params.username);
+        // username: $route.current.params.username
 
-        var params = {};
-        //serverCallService.makeGet("rest/material?materialId=" + materialId, params, getMaterialSuccess, getMaterialFail); 
-    	
-        function getMaterialSuccess(material) {
-            if (isEmpty(material)) {
-                log('No data returned by getting material');
-                } else {
-                    $scope.material = material;
-                }
-    	}
-    	
-    	function getMaterialFail(material, status) {
-            log('Getting materials failed.');
-    	}
     }]);
 });
