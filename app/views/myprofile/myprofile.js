1501babda247f545fc015922efc3a566d3302004
oliver <oliver.eivak@gmail.com>
2015-08-18 12:29:07 +0300

    #96062958 - Show user's full name on My profile page

--- app/views/myprofile/myprofile.js
+++ app/views/myprofile/myprofile.js
@@ -1,9 +1,19 @@
 define(['app'], function(app)
 {
-    app.controller('myprofileController', ['$scope', 
-        function($scope) {
-        
-        // username: $route.current.params.username
+    app.controller('myprofileController', ['$scope', '$route', 'loginService', 
+        function($scope, $route, loginService) {
+
+    	function getUser() {
+    		if (loginService.isAuthenticated()) {
+    			var user = loginService.getUser()
+
+    			if ($route.current.params.username === user.username) {
+    				$scope.user = user;
+    			}
+    		}
+    	}
+
+    	getUser();
 
     }]);
 });
