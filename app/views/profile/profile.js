b65f826b1673bdeda5ecc143d3429207d90c00fa
oliver <oliver.eivak@gmail.com>
2015-08-18 14:40:55 +0300

    #98696638 - Show other user's full name on profile page

--- app/views/profile/profile.js
+++ app/views/profile/profile.js
@@ -1,18 +1,43 @@
 define(['app'], function(app)
 {
-    app.controller('profileController', ['$scope', '$route', 'loginService', 
-        function($scope, $route, loginService) {
+    app.controller('profileController', ['$scope', '$route', 'loginService', 'serverCallService', '$location', 
+        function($scope, $route, loginService, serverCallService, $location) {
 
     	function getUser() {
     		if (loginService.isAuthenticated()) {
     			var user = loginService.getUser()
 
-    			if ($route.current.params.username === user.username) {
+    			if (user && $route.current.params.username === user.username) {
     				$scope.user = user;
     				$scope.myProfile = true;
     			}
     		}
+
+            if (!$scope.user) {
+                getUserFromRest();
+            }
     	}
+
+        function getUserFromRest() {
+            var params = {
+                'username': $route.current.params.username
+            };
+            var url = "rest/user";
+            serverCallService.makeGet(url, params, getUserSuccess, getUserFail);
+        }
+
+        function getUserSuccess(user) {
+            if (isEmpty(user)) {
+                console.log('No user returned.');
+                $location.url('/');
+            } else {
+                $scope.user = user;
+            }
+        }
+        
+        function getUserFail(data, status) {
+            console.log('Getting user failed.')
+        }
 
     	getUser();
 
