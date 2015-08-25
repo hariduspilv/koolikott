7c0a487ccb0d180c7789e40de372e2c594067ef4
silvermuru <silx99@gmail.com>
2015-08-25 12:08:00 +0300

    #101873998 Choose login method translation added

--- misc/database/translate_est.sql
+++ misc/database/translate_est.sql
@@ -1,6 +1,7 @@
 -- Estonian translations
 insert into Translation(translationGroup, translationKey, translation) values (1, 'LOGIN', 'Logi sisse');
 insert into Translation(translationGroup, translationKey, translation) values (1, 'LOGOUT', 'Log välja');
+insert into Translation(translationGroup, translationKey, translation) values (1, 'CHOOSE_LOGIN_METHOD', 'Vali sisenemise moodus');
 insert into Translation(translationGroup, translationKey, translation) values (1, 'MATERIAL_VIEW_SUBJECT', 'Teema');
 insert into Translation(translationGroup, translationKey, translation) values (1, 'MATERIAL_VIEW_RESOURCE_TYPE', 'Tüüp');
 insert into Translation(translationGroup, translationKey, translation) values (1, 'MATERIAL_VIEW_EDUCATIONAL_CONTEXT', 'Vanuserühmadele');
