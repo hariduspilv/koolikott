721ef2a04d47963ebe96fdfa4744685a16c94c12
jordan <jordan@netgroup.ee>
2015-08-31 18:11:16 +0300

    #102153426 - Disabling login when user has already initiated a login.

--- app/services/authenticationService.js
+++ app/services/authenticationService.js
@@ -1,12 +1,14 @@
 define(['app'], function(app) {
 	var instance;
+	var isAuthenticationInProgress;
 	
 	app.factory('authenticationService',['$location', '$rootScope', 'serverCallService', 'authenticatedUserService',
 	       function($location, $rootScope, serverCallService, authenticatedUserService) {
 
         function loginSuccess(authenticatedUser) {
             if (isEmpty(authenticatedUser)) {
-                log('No data returned by logging in');
+            	log('No data returned by logging in');
+            	enableLogin();
             } else {
                 authenticatedUserService.setAuthenticatedUser(authenticatedUser);
                 
@@ -18,15 +20,24 @@ define(['app'], function(app) {
         
         function loginFail(data, status) {
             log('Logging in failed.');
+            enableLogin();
         };
 
         function logoutSuccess(data) {
-            //ignore
+        	enableLogin();
         };
 
         function logoutFail(data, status) {
             //ignore
         };
+        
+        function disableLogin() {
+        	isAuthenticationInProgress = true;
+        }
+        
+        function enableLogin() {
+        	isAuthenticationInProgress = false;
+        }
         
         return {
 
@@ -39,6 +50,11 @@ define(['app'], function(app) {
             },
 
             loginWithIdCard : function() {
+            	if (isAuthenticationInProgress) {
+            		return;
+            	}
+            	
+            	disableLogin();
             	serverCallService.makeGet("rest/login/idCard", {}, loginSuccess, loginFail);
             }
 	    };
