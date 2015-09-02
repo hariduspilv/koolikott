340745aef6e88ae4e80eeb11007e496bc0a4d0df
silvermuru <silx99@gmail.com>
2015-09-02 13:29:28 +0300

    #102151890 error message show

--- app/directives/alert/alert.js
+++ app/directives/alert/alert.js
@@ -2,12 +2,12 @@ define(['app'], function(app)
 {
     
     app.directive('dopAlert', ['translationService', '$rootScope', 'authenticationService', 'authenticatedUserService',
-     function(translationService, $rootScope, authenticationService, authenticatedUserService) {
+    function($location, translationService, $rootScope, authenticationService, authenticatedUserService) {
         return {
             scope: true,
             templateUrl: 'app/directives/alert/alert.html',
             controller: function ($scope, $rootScope, authenticationService, authenticatedUserService) {
-                
+               
             }
         };
     }]);
