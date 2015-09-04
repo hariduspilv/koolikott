575d01dbbd42f166a96fce807322813b24844bb8
jordan <jordan@netgroup.ee>
2015-09-04 17:58:47 +0300

    #99598846 - Fixing Unit Test

--- src/test/java/ee/hm/dop/security/MetadataUtilsTest.java
+++ src/test/java/ee/hm/dop/security/MetadataUtilsTest.java
@@ -2,14 +2,20 @@ package ee.hm.dop.security;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opensaml.DefaultBootstrap;
+import org.opensaml.xml.ConfigurationException;
 import org.opensaml.xml.security.x509.X509Credential;
 
 public class MetadataUtilsTest {
 
+    @BeforeClass
+    public static void initOpenSAML() throws ConfigurationException {
+        DefaultBootstrap.bootstrap();
+    }
+
     @Test
-    @Ignore
     public void getCredential() throws Exception {
         X509Credential credential = MetadataUtils.getCerdential("reos_metadata.xml");
         assertNotNull(credential);
