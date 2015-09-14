f14b6e72ba235f3a11ae087d62c4bf3a026c33ea
jordan <jordan@netgroup.ee>
2015-09-14 16:47:36 +0300

    #96119420 - Getting StudyPlan id from URL and displaying the title

--- app/views/studyPlan/studyPlan.js
+++ app/views/studyPlan/studyPlan.js
@@ -1,7 +1,22 @@
 define(['app'], function(app)
 {
-    app.controller('studyPlanController', ['$scope', 'serverCallService', '$route', 'translationService', '$rootScope', 'searchService', '$location', 'alertService', 
-        function($scope, serverCallService, $route, translationService, $rootScope) {
-    	
-    }]);
+    app.controller('studyPlanController', ['$scope', 'translationService', 'serverCallService', '$route', 
+        function($scope, translationService, serverCallService, $route) {
+
+	    	var studyPlanId = $route.current.params.id;
+	        serverCallService.makeGet("rest/studyPlan?id=" + studyPlanId, {}, getStudyPlanSuccess, getStudyPlanFail);
+	        
+	        function getStudyPlanSuccess(studyPlan) {
+	            if (isEmpty(studyPlan)) {
+	            	getStudyPlanFail();
+	            } else {
+	                $scope.studyPlan = studyPlan;
+	            }
+	    	}
+	    	
+	    	function getStudyPlanFail() {
+	            log('No data returned by getting studyPlan.');
+	    	}
+    	}
+    ]);
 });
