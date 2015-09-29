9e5037fe3025a3b9d14498f6ee4d33747a3b0bf6
oliver <oliver.eivak@gmail.com>
2015-09-29 11:42:58 +0300

    #104296382 - Add +372 to mobile number if no calling code was added

--- app/directives/login-bar/login-bar.js
+++ app/directives/login-bar/login-bar.js
@@ -7,6 +7,8 @@ define(['app'], function(app)
             templateUrl: 'app/directives/login-bar/login-bar.html',
             controller: function ($scope) {
 
+                var ESTONIAN_CALLING_CODE = "+372";
+
                 $scope.mobileId = [];
 
                 $scope.idCardAuth = function() {
@@ -19,6 +21,12 @@ define(['app'], function(app)
 
                 $scope.mobileIdAuth = function() {
                     language = translationService.getLanguage();
+
+                    // Check if user did not enter a calling code
+                    if ($scope.mobileId.phoneNumber.indexOf("+") !== 0) {
+                        $scope.mobileId.phoneNumber = ESTONIAN_CALLING_CODE + $scope.mobileId.phoneNumber;
+                    }
+
                     authenticationService.loginWithMobileId($scope.mobileId.phoneNumber, $scope.mobileId.idCode, language, 
                         mobileIdSuccess, mobileIdFail, mobileIdReceiveChallenge);
                 };
