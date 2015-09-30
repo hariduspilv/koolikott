26d94341934758bbce9603c6f303795c0167851e
silvermuru <silx99@gmail.com>
2015-09-30 17:00:18 +0300

    #98695086 Mobil number input control to enter only numbers and + mark

--- app/directives/value-integer.js
+++ app/directives/value-integer.js
@@ -3,33 +3,44 @@ define(['app'], function(app)
     app.directive('vaInteger', [
         function() {
             return {
-                require : '?ngModel',
-                scope : {
-                    
-                },
-                link : function(scope, element, attrs, ngModelCtrl) {
-                    if (!ngModelCtrl) {
-                        return;
-                    }
+              require: '?ngModel',
+              scope: {
+                "inputPattern": '@'
+              },
+              link: function(scope, element, attrs, ngModelCtrl) {
 
-                    ngModelCtrl.$parsers.push(function(val) {
-                        var clean = val.replace(/[^0-9]/g, '');
+                var regexp = null;
 
-                        if (val !== clean) {
-                            ngModelCtrl.$setViewValue(clean);
-                            ngModelCtrl.$render();
-                        }
-
-                        return clean;
-                    });
-
-                    element.bind('keypress', function(event) {
-                        if (event.keyCode === 32 || event.charCode === 32) {
-                            return false;
-                        }
-                    });
+                if (scope.inputPattern !== undefined) {
+                  regexp = new RegExp(scope.inputPattern, "g");
                 }
-            };
+
+                if(!ngModelCtrl) {
+                  return;
+                }
+
+                ngModelCtrl.$parsers.push(function(val) {
+                  if (regexp) {
+                    var clean = val.replace(regexp, '');
+                    if (val !== clean) {
+                      ngModelCtrl.$setViewValue(clean);
+                      ngModelCtrl.$render();
+                    }
+                    return clean;
+                  }
+                  else {
+                    return val;
+                  }
+
+                });
+
+                element.bind('keypress', function(event) {
+                    if (event.keyCode === 32 || event.charCode === 32) {
+                        return false;
+                    }
+                });
+              }
+            }
         }
     ]);
 });
