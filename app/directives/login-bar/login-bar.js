2d0ab3c1c0c1f65153f816737e702a66a452bd0d
oliver <oliver.eivak@gmail.com>
2015-09-30 12:00:19 +0300

    #104296382 - Remove automatic adding of calling code to mobile number (move to back-end)

--- app/directives/login-bar/login-bar.js
+++ app/directives/login-bar/login-bar.js
@@ -7,8 +7,6 @@ define(['app'], function(app)
             templateUrl: 'app/directives/login-bar/login-bar.html',
             controller: function ($scope) {
 
-                var ESTONIAN_CALLING_CODE = "+372";
-
                 $scope.mobileId = [];
 
                 $scope.idCardAuth = function() {
@@ -21,11 +19,6 @@ define(['app'], function(app)
 
                 $scope.mobileIdAuth = function() {
                     language = translationService.getLanguage();
-
-                    // Check if user did not enter a calling code
-                    if ($scope.mobileId.phoneNumber.indexOf("+") !== 0) {
-                        $scope.mobileId.phoneNumber = ESTONIAN_CALLING_CODE + $scope.mobileId.phoneNumber;
-                    }
 
                     if (validIdCode($scope.mobileId.idCode)) {
                         authenticationService.loginWithMobileId($scope.mobileId.phoneNumber, $scope.mobileId.idCode, language, 
