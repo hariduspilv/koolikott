334eadb3f810040a2e35d6ff47e8c70f62e9b646
silvermuru <silx99@gmail.com>
2015-10-27 16:21:46 +0200

    #106742250 adding back detail search html

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -3,6 +3,9 @@ require.config({
     paths: {
         'dop': '../assets/js/dop.min',
         'modernizr': '../assets/js/modernizr.min',
+        'ngAnimate': '../bower_components/angular-animate/angular-animate.min',
+        'ngAria': '../bower_components/angular-aria/angular-aria.min',
+        'ngMaterial': '../bower_components/angular-material/angular-material.min',
         'authenticatedUserService': 'services/authenticatedUserService',
         'serverCallService': 'services/serverCallService',
         'authenticationService': 'services/authenticationService',
@@ -23,6 +26,11 @@ require.config({
         'app': {
             deps: ['dop', 'modernizr', 'translateUrlLoader','screenfull', 'angularScreenfull']
         },
+        'ngAnimate': ['dop'],
+        'ngAria': ['dop'],
+        'ngMaterial': {
+             deps: ['ngAnimate', 'ngAria']
+         },
         'screenfull': {
             deps: ['dop']
         },
@@ -35,7 +43,7 @@ require.config({
     }
 });
 
-require(['app', 'translationService', 'authenticatedUserService', 'serverCallService', 'authenticationService', 'searchService', 'dopHeader', 'dopFooter', 'dopLoginBar', 'dopAlert', 'alertService', 'inputValueControl', 'angularScreenfull', 'dopDetailedSearch'],
- function(app, translationService, authenticatedUserService, serverCallService, authenticationService, searchService, dopHeader, dopAlert, alertService, inputValueControl, angularScreenfull, dopDetailedSearch) {
+require(['app', 'ngMaterial', 'translationService', 'authenticatedUserService', 'serverCallService', 'authenticationService', 'searchService', 'dopHeader', 'dopFooter', 'dopLoginBar', 'dopAlert', 'alertService', 'inputValueControl', 'angularScreenfull', 'dopDetailedSearch'],
+ function(app, ngMaterial, translationService, authenticatedUserService, serverCallService, authenticationService, searchService, dopHeader, dopAlert, alertService, inputValueControl, angularScreenfull, dopDetailedSearch) {
     angular.bootstrap(document, ['app']);
 });
