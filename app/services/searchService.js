66f4d5bae9900f0bca1a80bd74bd1c56278b3d9c
oliver <oliver.eivak@gmail.com>
2015-10-28 11:49:50 +0200

    #106728690 - Removed filters from the right side of search results

--- app/services/searchService.js
+++ app/services/searchService.js
@@ -2,18 +2,12 @@ define(['app'], function(app) {
 
     app.factory('searchService',['$location', function($location) {
         var searchURLbase = "search/result?q=";
-        var subjectURL = "&subject=";
-        var resourceTypeURL = "&resource_type=";
         var educationalContextURL = "&educational_context=";
-        var licenseTypeURL = "&license_type=";
         var paidURL = "&paid=";
         var typeURL = "&type="
 
         var searchQuery = "";
-        var searchSubject = "";
-        var searchResourceType = "";
         var searchEducationalContext = "";
-        var searchLicenseType = "";
         var searchPaid = "";
         var searchType = "";
 
@@ -43,20 +37,8 @@ define(['app'], function(app) {
                 searchQuery = query;
             },
             
-            setSubject : function(subject) {
-                searchSubject = subject;
-            },
-
-            setResourceType : function(resourceType) {
-                searchResourceType = resourceType;
-            },
-
             setEducationalContext : function(educationalContext) {
                 searchEducationalContext = educationalContext;
-            },
-
-            setLicenseType : function(licenseType) {
-                searchLicenseType = licenseType;
             },
 
             setPaid : function(paid) {
@@ -75,17 +57,8 @@ define(['app'], function(app) {
                     searchURL = searchURLbase;
                 }
 
-                if (searchSubject) {
-                    searchURL += subjectURL + searchSubject;
-                } 
-                if (searchResourceType) {
-                    searchURL += resourceTypeURL + searchResourceType;
-                }
                 if (searchEducationalContext) {
                     searchURL += educationalContextURL + searchEducationalContext;
-                }
-                if (searchLicenseType) {
-                    searchURL += licenseTypeURL + searchLicenseType;
                 }
                 if (searchPaid && searchPaid === 'false') {
                     searchURL += paidURL + searchPaid;
@@ -99,8 +72,7 @@ define(['app'], function(app) {
 
             queryExists : function() {
                 var searchObject = $location.search();
-                if (searchObject.q || searchObject.subject || searchObject.resource_type || searchObject.educational_context || 
-                    searchObject.license_type|| (searchObject.paid && searchObject.paid === 'false') || 
+                if (searchObject.q || searchObject.educational_context || (searchObject.paid && searchObject.paid === 'false') || 
                     (searchObject.type && this.isValidType(searchObject.type))) {
                     return true;
                 } else {
@@ -119,28 +91,6 @@ define(['app'], function(app) {
                 return searchQuery;
             },
 
-            getSubject : function() {
-                if (searchSubject === "") {
-                    var searchObject = $location.search();
-                    if (searchObject.subject) {
-                        return searchObject.subject;
-                    }
-                }
-
-                return searchSubject;
-            },
-
-            getResourceType : function() {
-                if (searchResourceType === "") {
-                    var searchObject = $location.search();
-                    if (searchObject.resource_type) {
-                        return searchObject.resource_type;
-                    }
-                }
-
-                return searchResourceType;
-            },
-
             getEducationalContext: function() {
                 if (searchEducationalContext === "") {
                     var searchObject = $location.search();
@@ -150,17 +100,6 @@ define(['app'], function(app) {
                 }
 
                 return searchEducationalContext;
-            },
-
-            getLicenseType : function() {
-                if (searchLicenseType === "") {
-                    var searchObject = $location.search();
-                    if (searchObject.license_type) {
-                        return searchObject.license_type;
-                    }
-                }
-
-                return searchLicenseType;
             },
 
             isPaid : function() {
@@ -201,19 +140,10 @@ define(['app'], function(app) {
                 return 1;
             }, 
 
-            buildURL : function(query, page, subject, resourceType, educationalContext, licenseType, paid, type) {
+            buildURL : function(query, page, educationalContext, paid, type) {
                 var searchURL = "#/" + searchURLbase + encodeURI(escapeQuery(query)) + "&page=" + page;
-                if (subject) {
-                    searchURL += subjectURL + subject.toLowerCase();
-                }
-                if (resourceType) {
-                    searchURL += resourceTypeURL + resourceType.toLowerCase();
-                }
                 if (educationalContext) {
                     searchURL += educationalContextURL + educationalContext.toLowerCase();
-                }
-                if (licenseType) {
-                    searchURL += licenseTypeURL + licenseType.toLowerCase();
                 }
                 if (paid && paid === 'false') {
                     searchURL += paidURL + paid;
@@ -230,17 +160,8 @@ define(['app'], function(app) {
                     'page': page
                 };
 
-                if (this.getSubject()) {
-                    params.subject = this.getSubject();
-                }
-                if (this.getResourceType()) {
-                    params.resource_type = this.getResourceType();
-                }
                 if (this.getEducationalContext()) {
                     params.educational_context = this.getEducationalContext();
-                }
-                if (this.getLicenseType()) {
-                    params.license_type = this.getLicenseType();
                 }
                 if (this.isPaid() && this.isPaid() === 'false') {
                     params.paid = this.isPaid();
@@ -253,6 +174,7 @@ define(['app'], function(app) {
             },
 
             clearFieldsNotInSimpleSearch : function() {
+                searchEducationalContext = '';
                 searchPaid = '';
                 searchType = '';
             },
--- app/views/search/result/searchResult.js
+++ app/views/search/result/searchResult.js
@@ -1,7 +1,7 @@
 define(['app'], function(app)
 {
-    app.controller('searchResultController', ['$scope', "serverCallService", 'translationService', '$location', 'searchService', '$filter', 
-       function($scope, serverCallService, translationService, $location, searchService, $filter) {
+    app.controller('searchResultController', ['$scope', "serverCallService", 'translationService', '$location', 'searchService', 
+       function($scope, serverCallService, translationService, $location, searchService) {
 
         // Pagination variables
         $scope.paging = [];
@@ -24,9 +24,6 @@ define(['app'], function(app)
             if (!searchService.queryExists()) {
                 $location.url('/');
             }
-
-            // Filters
-            $scope.filters = [];
 
             // Get search query and current page
             $scope.searchQuery = searchService.getQuery();
@@ -56,20 +53,8 @@ define(['app'], function(app)
                 'start': start
             };
 
-            if (searchService.getSubject()) {
-                params.subject = searchService.getSubject();
-            }
-
-            if (searchService.getResourceType()) {
-                params.resource_type = searchService.getResourceType();
-            }
-
             if (searchService.getEducationalContext()) {
                 params.educational_context = searchService.getEducationalContext();
-            }
-
-            if (searchService.getLicenseType()) {
-                params.license_type = searchService.getLicenseType();
             }
 
             if (searchService.isPaid() && searchService.isPaid() === 'false') {
@@ -156,232 +141,10 @@ define(['app'], function(app)
             addNumbersToArray($scope.paging.after, $scope.paging.thisPage + 1, $scope.paging.thisPage + 1 + PAGES_AFTER_THIS_PAGE);
         }
 
-        // Get all subjects
-        serverCallService.makeGet("rest/subject/getAll", {}, getAllSubjectsSuccess, getAllSubjectsFail);
-
-        function getAllSubjectsSuccess(data) {
-            if (isEmpty(data)) {
-                log('No subjects returned.');
-            } else {
-                $scope.subjects = data;
-
-                // Select current subject in filter box
-                for (i = 0; i < $scope.subjects.length; i++) {
-                    if ($scope.subjects[i].name.toLowerCase() == searchService.getSubject().toLowerCase()) {
-                        $scope.filters.subject = $scope.subjects[i];
-                        break;
-                    }
-                }
-            }          
-        }
-
-        function getAllSubjectsFail(data, status) { 
-            console.log('Failed to get all subjects.');
-        }
-
-        // Get all resourceTypes
-        serverCallService.makeGet("rest/resourceType/getAll", {}, getAllResourceTypesSuccess, getAllResourceTypesFail);
-
-        function getAllResourceTypesSuccess(data) {
-            if (isEmpty(data)) {
-                log('No resource types returned.');
-            } else {
-                $scope.resourceTypes = data;
-
-                // Select current resourceType in filter box
-                for (i = 0; i < $scope.resourceTypes.length; i++) {
-                    if ($scope.resourceTypes[i].name.toLowerCase() == searchService.getResourceType().toLowerCase()) {
-                        $scope.filters.resourceType = $scope.resourceTypes[i];
-                        break;
-                    }
-                }
-            }
-        }
-
-        function getAllResourceTypesFail(data, status) { 
-            console.log('Failed to get all resource types.');
-        }
-
-        // Get all educationalContexts
-        serverCallService.makeGet("rest/educationalContext/getAll", {}, getAlleducationalContextsSuccess, getAlleducationalContextsFail);
-
-        function getAlleducationalContextsSuccess(data) {
-            if (isEmpty(data)) {
-                log('No educational contexts returned.');
-            } else {
-                $scope.educationalContexts = data;
-
-                // Select current educationalContext in filter box
-                for (i = 0; i < $scope.educationalContexts.length; i++) {
-                    if ($scope.educationalContexts[i].name.toLowerCase() == searchService.getEducationalContext().toLowerCase()) {
-                        $scope.filters.educationalContext = $scope.educationalContexts[i];
-                        break;
-                    }
-                }
-            }
-        }
-
-        function getAlleducationalContextsFail() {
-            console.log("Getting educational contexts for filter failed.");
-        }
-
-        // Get all licenseTypes
-        serverCallService.makeGet("rest/licenseType/getAll", {}, getAllLicenseTypesSuccess, getAllLicenseTypesFail);
-
-        function getAllLicenseTypesSuccess(data) {
-            if (isEmpty(data)) {
-                log('No license types returned.');
-            } else {
-                $scope.licenseTypes = data;
-
-                // Select current licenseType in filter box
-                for (i = 0; i < $scope.licenseTypes.length; i++) {
-                    if ($scope.licenseTypes[i].name.toLowerCase() == searchService.getLicenseType().toLowerCase()) {
-                        $scope.filters.licenseType = $scope.licenseTypes[i];
-                        break;
-                    }
-                }
-            }
-        }
-
-        function getAllLicenseTypesFail(data, status) { 
-            console.log('Failed to get all license types.');
-        }
-
-        $scope.filter = function() {
-            searchService.setSearch(searchService.getQuery());
-
-            if ($scope.filters.subject) {
-                searchService.setSubject($scope.filters.subject.name.toLowerCase());
-            } else {
-                searchService.setSubject('');
-            }
-
-            if ($scope.filters.resourceType) {
-                searchService.setResourceType($scope.filters.resourceType.name.toLowerCase());
-            } else {
-                searchService.setResourceType('');
-            }
-
-            if ($scope.filters.educationalContext) {
-                searchService.setEducationalContext($scope.filters.educationalContext.name.toLowerCase());
-            } else {
-                searchService.setEducationalContext('');
-            }
-
-            if ($scope.filters.licenseType) {
-                searchService.setLicenseType($scope.filters.licenseType.name.toLowerCase());
-            } else {
-                searchService.setLicenseType('');
-            }
-
-            $location.url(searchService.getURL());
-        }
-
-        $scope.reset = function() {
-           $scope.filters.subject = null;
-           searchService.setSubject('');
-           $scope.filters.resourceType = null;
-           searchService.setResourceType('');
-           $scope.filters.educationalContext = null;
-           searchService.setEducationalContext('');
-           $scope.filters.licenseType = null;
-           searchService.setLicenseType('');
-        }
-
         $scope.buildPageURL = function(page) {
-            var subject = $scope.filters.subject ? $scope.filters.subject.name : null;
-            var resourceType = $scope.filters.resourceType ? $scope.filters.resourceType.name : null;
-            var educationalContext = $scope.filters.educationalContext ? $scope.filters.educationalContext.name : null;
-            var licenseType = $scope.filters.licenseType ? $scope.filters.licenseType.name : null;
-
-            return searchService.buildURL($scope.searchQuery, page, subject, resourceType, educationalContext, licenseType, 
-                searchService.isPaid(), searchService.getType());
+            return searchService.buildURL($scope.searchQuery, page, searchService.getEducationalContext(), searchService.isPaid(), searchService.getType());
         }
 
 
     }]);
-
-app.filter('translatableItemFilter', function($filter) {
-    return function(items, query, translationPrefix) {
-        var out = [];
-
-        if (angular.isArray(items) && query) {
-            items.forEach(function(item) {
-                // Get translation
-                var translatedItem = $filter('translate')(translationPrefix + item.name.toUpperCase());
-
-                if (translatedItem.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
-                    out.push(item);
-                }
-            });
-        } else {
-            out = items;
-        }
-
-        return out;
-    }
-});
-
-app.filter('orderByTranslation', function($filter) {
-    return function(items, translationPrefix) {
-
-        if (angular.isArray(items)) {
-            for (i = 0; i < items.length; i++) {
-                // Get translation
-                var translatedItem = $filter('translate')(translationPrefix + items[i].name.toUpperCase());
-
-                // Create temporary property
-                items[i].translation = translatedItem.toLowerCase();
-            }
-
-            // Sort alphabetically
-            items = $filter('orderBy')(items, '-translation', true);
-
-            // Remove translation property
-            for (i = 0; i < items.length; i++) {
-                items[i].translation = null;
-            }
-
-        } 
-
-        return items;
-    }
-});
-
-app.filter('subjectFilter', function($filter) {
-    return function(items, query) {
-        var translationPrefix = 'SUBJECT_';
-        items = $filter('translatableItemFilter')(items, query, translationPrefix);
-        items = $filter('orderByTranslation')(items, translationPrefix);
-        return items;
-    }
-});
-
-app.filter('resourceTypeFilter', function($filter) {
-    return function(items, query) {
-        var translationPrefix = '';
-        items = $filter('translatableItemFilter')(items, query, translationPrefix);
-        items = $filter('orderByTranslation')(items, translationPrefix);
-        return items;
-    }
-});
-
-app.filter('educationalContextFilter', function($filter) {
-    return function(items, query) {
-        var translationPrefix = '';
-        items = $filter('translatableItemFilter')(items, query, translationPrefix);
-        return items;
-    }
-});
-
-app.filter('licenseTypeFilter', function($filter) {
-    return function(items, query) {
-        var translationPrefix = 'LICENSETYPE_';
-        items = $filter('translatableItemFilter')(items, query, translationPrefix);
-        items = $filter('orderByTranslation')(items, translationPrefix);
-        return items;
-    }
-});
-
 });
