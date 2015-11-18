6f35345f7ec2866411bb118eb20114883d20f77f
oliver <oliver.eivak@gmail.com>
2015-11-18 10:27:43 +0200

    #105557862 - Remove extra method from getting domains as the information is already in educational contexts

--- src/test/java/ee/hm/dop/rest/LearningMaterialMetadataResourceTest.java
+++ src/test/java/ee/hm/dop/rest/LearningMaterialMetadataResourceTest.java
@@ -1,24 +1,19 @@
 package ee.hm.dop.rest;
 
-import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.ws.rs.core.GenericType;
 
 import org.junit.Test;
 
 import ee.hm.dop.common.test.ResourceIntegrationTestBase;
-import ee.hm.dop.model.Domain;
 import ee.hm.dop.model.EducationalContext;
 
 public class LearningMaterialMetadataResourceTest extends ResourceIntegrationTestBase {
 
     private static final String GET_EDUCATIONAL_CONTEXT_URL = "learningMaterialMetadata/educationalContext";
-    private static final String GET_DOMAIN_BY_EDUCATIONAL_CONTEXT_URL = "learningMaterialMetadata/domain?educationalContext=%s";
 
     @Test
     public void getEducationalContext() {
@@ -29,17 +24,4 @@ public class LearningMaterialMetadataResourceTest extends ResourceIntegrationTes
         assertEquals(9, educationalContexts.stream().distinct().count());
     }
 
-    @Test
-    public void getAllDomainsByEducationalContext() {
-        String educationalContext = "preschooleducation";
-        List<Domain> domains = doGet(format(GET_DOMAIN_BY_EDUCATIONAL_CONTEXT_URL, educationalContext),
-                new GenericType<List<Domain>>() {
-                });
-
-        assertEquals(2, domains.stream().distinct().count());
-
-        List<String> domainNames = domains.stream().map(d -> d.getName()).collect(Collectors.toList());
-        assertTrue(domainNames.contains("Mathematics"));
-        assertTrue(domainNames.contains("ForeignLanguage"));
-    }
 }
