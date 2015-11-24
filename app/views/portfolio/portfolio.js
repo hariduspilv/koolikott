a6e6c9587735c29f7c14b90f1b4660bd78d142d5
Aare-Arnold Äkke <aare.arnold.akke@gmail.com>
2015-11-24 13:52:27 +0200

    #99706298 comment box ui for portfolio

--- app/views/portfolio/portfolio.js
+++ app/views/portfolio/portfolio.js
@@ -2,6 +2,19 @@ define(['app'], function(app)
 {
     app.controller('portfolioController', ['$scope', 'translationService', 'serverCallService', '$route', '$location', 'alertService', '$rootScope',
         function($scope, translationService, serverCallService, $route, $location, alertService, $rootScope) {
+            // TEST DATA
+
+            $scope.comments = [{
+                author: "Mari Maasikas",
+                date: "24.11.2015 14:21",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae gravida velit, at aliquet neque. Duis at pellentesque leo"
+            }, {
+                author: "Ants Kaalikas",
+                date: "24.11.2015 10:02",
+                text: "Nunc pharetra eu velit ut scelerisque. Duis enim odio, consectetur ac accumsan id, mattis et justo."
+            }];
+
+            // END TEST DATA
 
         	if ($rootScope.savedPortfolio){
             	$scope.portfolio = $rootScope.savedPortfolio;
