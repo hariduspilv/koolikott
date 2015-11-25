dec790c7b34391a27d9d4e96fc3a487ff3aaf8a8
kristjansander <kristjan.sander@bitspinners.eu>
2015-11-25 17:23:34 +0200

    Commeting for portfolio UI
    https://www.pivotaltracker.com/story/show/99706238

--- app/views/portfolio/portfolio.js
+++ app/views/portfolio/portfolio.js
@@ -2,34 +2,27 @@ define(['app'], function(app)
 {
     app.controller('portfolioController', ['$scope', 'translationService', 'serverCallService', '$route', '$location', 'alertService', '$rootScope',
         function($scope, translationService, serverCallService, $route, $location, alertService, $rootScope) {
-            // TEST DATA
 
-            $scope.comments = [{
-                author: "Mari Maasikas",
-                date: "24.11.2015 14:21",
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae gravida velit, at aliquet neque. Duis at pellentesque leo"
-            }, {
-                author: "Ants Kaalikas",
-                date: "24.11.2015 10:02",
-                text: "Nunc pharetra eu velit ut scelerisque. Duis enim odio, consectetur ac accumsan id, mattis et justo."
-            }];
-
-            // END TEST DATA
-
-        	if ($rootScope.savedPortfolio){
-            	$scope.portfolio = $rootScope.savedPortfolio;
-            	init();
-        	} else {
-            	var portfolioId = $route.current.params.id;
-	        	serverCallService.makeGet("rest/portfolio?id=" + portfolioId, {}, getPortfolioSuccess, getPortfolioFail);
-	        }
+			function init() {
+				if ($rootScope.savedPortfolio){
+					$scope.portfolio = $rootScope.savedPortfolio;
+					increaseViewCount();
+				} else {
+					getPortfolio(getPortfolioSuccess, getPortfolioFail);
+				}
+			}
+			
+			function getPortfolio(success, fail) {
+				var portfolioId = $route.current.params.id;
+				serverCallService.makeGet("rest/portfolio?id=" + portfolioId, {}, success, fail);
+			}
 
 	        function getPortfolioSuccess(portfolio) {
 	            if (isEmpty(portfolio)) {
 	            	getPortfolioFail();
 	            } else {
 	                $scope.portfolio = portfolio;
-	                init();
+	                increaseViewCount();
 	            }
 	    	}
 
@@ -43,7 +36,7 @@ define(['app'], function(app)
 	    		return formatDateToDayMonthYear(date);
             }
 
-            function init() {
+            function increaseViewCount() {
             	var params = {
 					'type' : '.Portfolio',
                 	'id': $scope.portfolio.id
@@ -51,6 +44,34 @@ define(['app'], function(app)
 
             	serverCallService.makePost("rest/portfolio/increaseViewCount", params, function success(){}, function fail(){});
     		}
+			
+			$scope.addComment = function() {
+				var url = "rest/comment/portfolio";
+				var params = {
+					'comment': $scope.newComment,
+					'portfolio': {
+						'type' : '.Portfolio',
+						'id': $scope.portfolio.id
+					}
+				};
+				serverCallService.makePost(url, params, addCommentSuccess, addCommentFailed);
+			};
+						
+			function addCommentSuccess() {
+				$scope.newComment.text = "";
+				
+				getPortfolio(function(portfolio) {
+					$scope.portfolio = portfolio;
+				}, function() {
+					log("Comment success, but failed to reload portfolio.");
+				});
+			}
+			
+			function addCommentFailed(){
+				log('Adding comment failed.');
+			}
+			
+			init();
     	}
     ]);
 });
