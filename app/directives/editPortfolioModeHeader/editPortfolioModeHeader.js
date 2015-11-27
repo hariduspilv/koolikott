df9f1107f03e9db5be38b7afecc556b2b8e5af63
jordan <jordan@netgroup.ee>
2015-11-27 11:04:16 +0200

    #106474144 - Refactoring: Renaming portfolioHeader directive to editPortfolioModeHeader.

--- app/directives/editPortfolioModeHeader/editPortfolioModeHeader.js
+++ app/directives/editPortfolioModeHeader/editPortfolioModeHeader.js
@@ -1,10 +1,10 @@
 define(['app'], function(app)
 {
-    app.directive('portfolioHeader', ['translationService', '$location', '$mdSidenav', '$mdDialog',
+    app.directive('dopEditPortfolioModeHeader', ['translationService', '$location', '$mdSidenav', '$mdDialog',
      function(translationService, $location, $mdSidenav, $mdDialog) {
         return {
             scope: true,
-            templateUrl: 'directives/portfolioHeader/portfolioHeader.html',
+            templateUrl: 'directives/editPortfolioModeHeader/editPortfolioModeHeader.html',
             controller: function ($scope, $location) {
                 $scope.toggleSidenav = function() {
                     $mdSidenav('left').toggle();
