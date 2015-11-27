38ec489d253ca8aea98163301a2e4971ad0d56a4
Mart Laus <mart.laus@netgroup.ee>
2015-11-27 12:03:21 +0200

    #108936434 fixed imports from merge

--- src/test/java/ee/hm/dop/oaipmh/estcore/MaterialParserEstCoreTest.java
+++ src/test/java/ee/hm/dop/oaipmh/estcore/MaterialParserEstCoreTest.java
@@ -25,14 +25,14 @@ import org.junit.runner.RunWith;
 import org.w3c.dom.Document;
 
 import ee.hm.dop.model.Author;
-import ee.hm.dop.model.Domain;
-import ee.hm.dop.model.EducationalContext;
 import ee.hm.dop.model.Language;
 import ee.hm.dop.model.LanguageString;
 import ee.hm.dop.model.Material;
 import ee.hm.dop.model.ResourceType;
-import ee.hm.dop.model.Subject;
 import ee.hm.dop.model.Tag;
+import ee.hm.dop.model.taxon.Domain;
+import ee.hm.dop.model.taxon.EducationalContext;
+import ee.hm.dop.model.taxon.Subject;
 import ee.hm.dop.oaipmh.ParseException;
 import ee.hm.dop.service.AuthorService;
 import ee.hm.dop.service.LanguageService;
