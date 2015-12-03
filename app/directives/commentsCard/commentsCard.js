0f272843ce1f3478b0a803eb5a0c29d83c3eddc2
Aare-Arnold Äkke <aare.arnold.akke@gmail.com>
2015-12-03 16:53:31 +0200

    merged comments-card branch

--- app/directives/commentsCard/commentsCard.js
+++ app/directives/commentsCard/commentsCard.js
@@ -10,7 +10,33 @@ define(['app'], function(app)
                 },
                 templateUrl: 'directives/commentsCard/commentsCard.html',
                 controller: function ($scope) {
+                    var COMMENTS_PER_PAGE = 5;
+                    
                     $scope.authorized = authenticatedUserService.isAuthenticated();
+
+                    $scope.visibleCommentsCount = COMMENTS_PER_PAGE;
+                    
+                    $scope.getLoadMoreCommentsLabel = function() {
+                        var commentsLeft = getLeftCommentsCount();
+                                      
+                        if (commentsLeft <= COMMENTS_PER_PAGE)
+                            return '(' + commentsLeft + ')'
+                        
+                        return '(' + COMMENTS_PER_PAGE + '/' + commentsLeft + ')';
+                    }
+                    
+                    $scope.showMoreComments = function() {
+                        var commentsLeft = getLeftCommentsCount();
+                        
+                        if (commentsLeft - COMMENTS_PER_PAGE >= 0)
+                            $scope.visibleCommentsCount += COMMENTS_PER_PAGE
+                        else
+                            $scope.visibleCommentsCount = $scope.comments.length;
+                    }
+                    
+                    function getLeftCommentsCount() {
+                        return $scope.comments.length - $scope.visibleCommentsCount;
+                    }
                     
                     $scope.addComment = function() {
                         $scope.submitClick();
