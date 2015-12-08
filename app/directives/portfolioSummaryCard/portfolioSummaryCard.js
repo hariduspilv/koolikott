15e8cee03121c5d9d712fcfa85374c10883b3953
fpoobus <fpoobus@netgroup-ThinkPad-T450s>
2015-12-08 18:09:10 +0200

    #98725592 #99686916 - Edit portfolio button for portfolio owner. Dynamic behavior depending on edit/view page

--- app/directives/portfolioSummaryCard/portfolioSummaryCard.js
+++ app/directives/portfolioSummaryCard/portfolioSummaryCard.js
@@ -1,19 +1,47 @@
 define(['app'], function(app)
 {
-    app.directive('dopPortfolioSummaryCard', ['translationService', '$location', '$mdSidenav', '$mdDialog', '$rootScope',
-        function(translationService, $location, $mdSidenav, $mdDialog, $rootScope) {
+    app.directive('dopPortfolioSummaryCard', ['translationService', '$location', '$mdSidenav', '$mdDialog', '$rootScope', 'authenticatedUserService', '$route',
+        function(translationService, $location, $mdSidenav, $mdDialog, $rootScope, authenticatedUserService, $route) {
             return {
                 scope: true,
                 templateUrl: 'directives/portfolioSummaryCard/portfolioSummaryCard.html',
                 controller: function ($scope, $location) {
 
+                	 $scope.openMenu = function($mdOpenMenu) {
+                     	 if(isShowEditButton()) {
+                     		 $scope.showEditButton = true;
+                     	 }
+                     	 
+                         $mdOpenMenu();     
+                     };
+                     
                     $scope.showEditPortfolioDialog = function($event) {
-                        $mdDialog.show({
-                            controller: 'addPortfolioDialog',
-                            templateUrl: 'views/addPortfolioDialog/addPortfolioDialog.html',
-                            locals:{portfolio: $scope.portfolio}
-                        });
+
+                    	if(isShowEditButton() && $rootScope.isEditPortforlioMode && !$rootScope.isViewPortforlioPage) {
+	                        $mdDialog.show({
+	                            controller: 'addPortfolioDialog',
+	                            templateUrl: 'views/addPortfolioDialog/addPortfolioDialog.html',
+	                            locals:{portfolio: $scope.portfolio}
+	                        });
+                    	}
+                    	if(isShowEditButton() && $rootScope.isViewPortforlioPage) {
+                    		var portfolioId = $route.current.params.id;
+                    		$location.url("/portfolio/edit?id="+portfolioId);
+                    	}
+                    	
                     };
+                    
+                    function isShowEditButton() {
+                    	var portfolio = $scope.portfolio;
+                    	var user = authenticatedUserService.getUser();
+                    	var authenticated = authenticatedUserService.isAuthenticated();
+                    	var showEditButton = false;
+						if(authenticated && user.id == portfolio.creator.id) {
+							 showEditButton = true;
+						}
+	                	return showEditButton;
+                    }
+                    
                 }
             };
         }]);
