9106a504c643048cd8c7f4cee844d69923d47d8d
oliver <oliver.eivak@gmail.com>
2015-12-11 11:54:59 +0200

    #108061552 - Validate URL input field in edit chapter, so that it starts with http:// or https://

--- app/directives/chapter/addChapterMaterial/addChapterMaterial.js
+++ app/directives/chapter/addChapterMaterial/addChapterMaterial.js
@@ -12,16 +12,18 @@ define(['app'], function(app)
             	$scope.isEditable = $rootScope.isEditPortforlioMode;
             	
                 $scope.addMaterialFromPermalink = function() {
-                    var addMaterialScope = $scope.$new(true);
+                    if ($scope.resourcePermalinkForm.$valid) {
+                        var addMaterialScope = $scope.$new(true);
 
-                    addMaterialScope.material ={};
-                    addMaterialScope.material.url = $scope.chapter.resourcePermalink;
-                    $mdDialog.show({
-                        controller: 'addMaterialDialog',
-                        templateUrl: 'views/addMaterialDialog/addMaterialDialog.html',
-                        scope: addMaterialScope
-                    });
-                }
+                        addMaterialScope.material ={};
+                        addMaterialScope.material.url = $scope.chapter.resourcePermalink;
+                        $mdDialog.show({
+                            controller: 'addMaterialDialog',
+                            templateUrl: 'views/addMaterialDialog/addMaterialDialog.html',
+                            scope: addMaterialScope
+                        });
+                    }
+                };
             }
         };
     }]);
