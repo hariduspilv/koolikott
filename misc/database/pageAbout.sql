23427e2b531d97b6ca8a533445b1b1781414a061
Aare-Arnold Äkke <aare.arnold.akke@gmail.com>
2015-12-15 14:18:01 +0200

    #110139006 about page text updates (only in estonian)

--- misc/database/pageAbout.sql
+++ misc/database/pageAbout.sql
@@ -1,46 +1,54 @@
 -- Page
 
 -- Estonian
-insert into Page(id, name, content, language) VALUES (1, 'About', '<h1>Mis on eKoolikott?</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque congue tempus urna. 
-Morbi non diam ac dui molestie auctor sed at erat. Suspendisse potenti. Integer volutpat tellus 
-eu mauris porta dapibus id ac dolor. Etiam interdum ut lorem in gravida. Morbi scelerisque augue 
-non quam faucibus, eget imperdiet neque pretium. Duis porttitor rutrum urna, quis scelerisque dui 
-egestas sit amet. Maecenas nunc neque, efficitur ac ullamcorper ac, porttitor ac magna. Nunc 
-porttitor mauris et ipsum accumsan, eget interdum arcu pretium. Phasellus sed orci id ligula 
-aliquam vestibulum. 
+insert into Page(id, name, content, language) VALUES (1, 'About', '<h1>Mis on E-koolikott?</h1>
+<p>E-koolikott on veebikeskkond, mis koondab kokku erinevates keskkondades asuvad digitaalsed õppematerjalid ning 
+teeb need kasutajatele kättesaadavaks. Õppematerjalide hulk ei ole sealjuures piiratud – E-koolikott sisaldab hõlmab
+nii õpetajate kui õpilaste poolt koostatud õppematerjale kui ka kirjastuste poolt avaldatud digiformaadis õpikuid ja õppematerjale.</p>
 
-<p>Nulla imperdiet velit quis quam fringilla, eget placerat ipsum sodales. 
-Aenean rhoncus eget enim sed iaculis. Vestibulum ante ipsum primis in faucibus orci luctus 
-et ultrices posuere cubilia Curae; Etiam ut nibh ornare, mollis sem eleifend, commodo metus.
-Vestibulum pharetra facilisis imperdiet. 
+<h2>Kellele on E-koolikott mõeldud?</h2>
+<p>E-koolikott on suunatud kõigile, kes tegelevad õppimise või õpetamisega. Õpilastel on võimalik tutvuda nii 
+tasuta kui tasuliste õppematerjalidega, samuti luua endale huvipakkuvatest õppematerjalidest kogumikke. 
+Kogumike loomise võimalus on ka õpetajatel, koondades kindla teemaga seotud õppematerjalid ning loodud kogumikke on võimalik õpilastega hiljem jagada.</p>
 
-<h2>Teise taseme pealkiri</h2>
-<p>Nunc molestie nulla odio, non viverra leo ullamcorper 
-eu. Sed nec eros odio. Phasellus quis blandit orci, sed laoreet turpis. Integer ac lectus sem. 
-Sed facilisis aliquam sapien at suscipit. Aliquam in augue et quam pellentesque rutrum. 
-Suspendisse eget feugiat purus. Mauris porttitor arcu eget nisi hendrerit placerat. Mauris 
-eget magna a libero iaculis porta. Quisque pharetra purus nec orci placerat, ac sodales enim 
-bibendum. Aenean et efficitur sem.
+<h2>Kuidas tekivad õppematerjalid E-koolikotti?</h2>
+<p>Hetkeseisuga on E-koolikotiga seotud järgmised keskkonnad:</p>
 
-Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur vitae mi eu urna facilisis 
-faucibus. Proin ac suscipit elit, dapibus pellentesque libero. Aenean luctus lobortis ante, vel 
-consequat leo faucibus sed. Praesent vitae dolor lacinia, suscipit ligula nec, cursus elit. 
-
-In nec euismod lacus: 
-Donec eget placerat nunc. 
 <ul>
-<li>Donec placerat volutpat nisl at mollis. </li>
-<li>Curabitur consequat lacus ac aliquam lobortis. Fusce ultricies eros non mi commodo vulputate. </li>
-<li>Suspendisse potenti. Etiam ultrices nulla enim, id tincidunt dui ullamcorper quis. 
-Praesent vitae dolor lacinia, suscipit ligula nec, cursus elit. </li>
-<li>Aliquam in augue et quam pellentesque rutrum. </li>
+<li>Waramu (koolielu.ee portaal)</li>
+<li>Kirjastuste repositooriumid</li>
 </ul>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque congue tempus urna. 
-Morbi non diam ac dui molestie auctor sed at erat. Suspendisse potenti. Integer volutpat tellus 
-eu mauris porta dapibus id ac dolor. 
-', 1);
+<h2>Mida E-koolikott veel pakub?</h2>
+<p>E-koolikott võimaldab kasutajate omavahelist suhtlust – kõiki keskkonnas sisalduvaid õppematerjale ja kogumikke on 
+võimalik kommenteerida. Samuti saab iga kasutaja anda nii õppematerjalidele kui ja kogumikele oma hinnangu 
+meeldimise/mittemeeldimise märke lisamise teel. Õppematerjale ja kogumikke aitavad omavahel siduda lisatud märksõnad ning 
+vajalike materjalide ja kogumike leidmiseks on võimalik kasutada nii lihtotsingut kui ka detailotsingut.</p>
+ 
+<h2>Otsing</h2>
+<p>Õppematerjalide otsinguvormid asuvad veebikeskkonna päises. Lihtotsing on alati nähtaval, detailotsing on avatav lihtotsingu kõrval asuva nupu abil.</p>
+
+<h2>Materjalide ja kogumike sirvimine</h2>
+<p>Õppematerjalide ja kogumike sirvimiseks tuleb esmalt teostada otsing. Otsingutulemustes kuvatakse otsinguparameetritele 
+vastavad materjalid ja/või kogumikud. Materjali või kogumiku avamisel kuvatakse esmalt nende põhiandmed (pealkiri, kirjeldus, 
+taksonoomiline kuuluvus, autor jne). Õppematerjali avamiseks tuleb klikkida selle pildil või pildi puudumisel ikoonil.</p>
+
+<h2>Kommenteerimine, meeldimine, püsilingid, ebasobivaks märkimine</h2>
+<p>Kogumiku või materjali avamisel on võimalik lehe päises asuva menüü kaudu teavitada E-koolikoti administraatorit ebasobivast materjalist 
+või katkisest lingist. Meeldivaks/mittemeeldivaks märkimida saab materjali või kogumikku põhiandmete all asuvate ikoonidel klikkimise teel. 
+Kommentaaride lisamine ja vaatamine on samuti võimalik materjali või kogumiku põhiandmete all. Kommentaaride lisamise õigus on ainult autenditud kasutajatel.</p>
+
+<h2>Kogumike loomine</h2>
+<p>Autenditud kasutajatel on võimalik luua õpematerjalidest kogumikke. Selleks tuleb vajutada ekraani paremal allääres asuvast “+” nupule ning 
+klikkida ikoonil “Lisa kogumik”. Avanevas aknas tuleb esmalt sisestada kogumiku põhiandmestik, mille järel avatakse kogumiku põhivaade. 
+Vasakul asub sisukord, kuhu on võimalik lisada peatükke ja alampeatükke. Igasse peatükki ja alampeatükki on võimalik lisada soovitud õppematerjale. 
+Selleks tuleb esmalt teostada otsing, mille järel on võimalik otsingutulemustes märkida ära sobivad materjalid. 
+Õppematerjalide märkimise järel tuleb hiirega liikuda sisukorras sobivale peatükile või alampeatükile ning vajutada nuppu “Lisa”.</p>
+
+<h2>Materjalide loomine</h2>
+<p>Kogumiku loomisel on võimalik lisada E-koolikoti keskkonda uus õppematerjal. Selleks tuleb sisestada õppematerjali link vastava peatüki 
+või alapeatüki kirjelduse järel asuvasse tekstivälja ning vajutada nuppu “Lisa”. Seejärel tuleb avanevas aknas sisestada õppematerjali põhiandmestik, 
+mille järel lisatakse materjal süsteemi ning ühtlasi ka käesoleva kogumiku peatükki või alapeatükki.</p>', 1);
 
 -- Russian
 insert into Page(id, name, content, language) VALUES (2, 'About', '<h1>Что eKoolikott?</h1>
