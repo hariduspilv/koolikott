69dc0e9a36d9886810345d9decf44cb2135d74e5
mare hunt <mare.hunt@netgroup.ee>
2015-12-17 15:54:52 +0200

    violet header design

--- app/directives/pageStructure/columnLayout/columnLayout.js
+++ app/directives/pageStructure/columnLayout/columnLayout.js
@@ -5,7 +5,10 @@ define(['app'], function(app)
         return {
             scope: true,
             templateUrl: 'directives/pageStructure/columnLayout/columnLayout.html',
-            controller: function ($scope, $rootScope) {
+            controller: function ($scope, $rootScope, $mdSidenav) {
+              $scope.toggleSidenav = function() {
+                  $mdSidenav('left').toggle();
+              };
             	$scope.$watch(function() {
             		return $rootScope.savedPortfolio;
             	}, function(newPortfolio, oldPortfolio) {
@@ -13,7 +16,7 @@ define(['app'], function(app)
             			$scope.portfolio = newPortfolio;
             		}
                 });
-            	
+
             	$scope.portfolio = $rootScope.savedPortfolio;
             }
         };
