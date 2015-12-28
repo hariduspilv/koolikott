e3dd8d0f21e951b129067d20ffa6952efe611f1c
fpoobus <fpoobus>
2015-12-28 16:41:09 +0200

    #110750602 - Watch authentication for changes. When state changes to logged in, close loginDialog.

--- app/views/loginDialog/loginDialog.js
+++ app/views/loginDialog/loginDialog.js
@@ -1,12 +1,18 @@
 define(['app'], function(app)
 {
-    app.controller('loginController', ['$scope', '$mdDialog', 'authenticationService', '$location', 'translationService', '$rootScope',
-        function($scope, $mdDialog, authenticationService, $location, translationService, $rootScope) {
+    app.controller('loginController', ['$scope', '$mdDialog', 'authenticationService', '$location', 'translationService', '$rootScope', 'authenticatedUserService',
+        function($scope, $mdDialog, authenticationService, $location, translationService, $rootScope, authenticatedUserService) {
 			$scope.mobileId = {};
 			$scope.validation = {
 				error: {}
 			};
 
+			$scope.$watch(function() { return authenticatedUserService.isAuthenticated }, function(newValue, oldValue) {
+				if(newValue == true) {
+					$mdDialog.hide();
+				}
+            }, false);
+			
 			$rootScope.$on('$routeChangeSuccess', function() {
 				$mdDialog.hide();
             });
