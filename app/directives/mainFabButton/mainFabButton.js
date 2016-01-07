de1bcbe19f371421cdf9407e380f7d20c9e21f88
Aare-Arnold Äkke <aare.arnold.akke@gmail.com>
2016-01-07 13:14:17 +0200

    #110055222 fab button improvements

--- app/directives/mainFabButton/mainFabButton.js
+++ app/directives/mainFabButton/mainFabButton.js
@@ -1,11 +1,10 @@
-define(['app'], function(app)
-{
+define(['app'], function(app) {
     app.directive('dopMainFabButton', ['$rootScope', 'serverCallService', '$route',
         function($rootScope, serverCallService, $route) {
             return {
                 scope: true,
                 templateUrl: 'directives/mainFabButton/mainFabButton.html',
-                controller: function ($scope, $mdDialog, $location, authenticatedUserService) {
+                controller: function($scope, $mdDialog, $location, authenticatedUserService) {
                     $scope.isOpen = false;
 
                     $scope.showAddPortfolioDialog = function() {
@@ -13,7 +12,9 @@ define(['app'], function(app)
                         $mdDialog.show({
                             controller: 'addPortfolioDialog',
                             templateUrl: 'views/addPortfolioDialog/addPortfolioDialog.html',
-                            locals:{portfolio: portfolio}
+                            locals: {
+                                portfolio: portfolio
+                            }
                         });
                     };
 
@@ -23,27 +24,27 @@ define(['app'], function(app)
                             templateUrl: 'views/addMaterialDialog/addMaterialDialog.html'
                         });
                     };
-                    
+
                     $scope.copyPortfolio = function() {
-                    	var url = "rest/portfolio/copy";
-                    	var portfolio = createPortfolio($route.current.params.id);
-        				serverCallService.makePost(url, portfolio, createPortfolioSuccess, createPortfolioFailed);
+                        var url = "rest/portfolio/copy";
+                        var portfolio = createPortfolio($route.current.params.id);
+                        serverCallService.makePost(url, portfolio, createPortfolioSuccess, createPortfolioFailed);
                     };
 
                     function createPortfolioSuccess(portfolio) {
-                    	if (isEmpty(portfolio)) {
-                    		createPortfolioFailed();
-        	            } else {
-        	            	$rootScope.savedPortfolio = portfolio;
-        	            	$rootScope.openMetadataDialog = true;
-        	            	$mdDialog.hide();
-        	                $location.url('/portfolio/edit?id=' + portfolio.id);
-        	            }
-        			}
-        			
-        			function createPortfolioFailed() {
-        				log('Creating copy of portfolio failed.');
-        			}
+                        if (isEmpty(portfolio)) {
+                            createPortfolioFailed();
+                        } else {
+                            $rootScope.savedPortfolio = portfolio;
+                            $rootScope.openMetadataDialog = true;
+                            $mdDialog.hide();
+                            $location.url('/portfolio/edit?id=' + portfolio.id);
+                        }
+                    }
+
+                    function createPortfolioFailed() {
+                        log('Creating copy of portfolio failed.');
+                    }
 
                     $scope.hasPermission = function() {
                         return authenticatedUserService.getUser() &&
