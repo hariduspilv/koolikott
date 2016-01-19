86d242712be51f65f825f6aff9c075da85434c87
fpoobus <fpoobus@netgroup-ThinkPad-T450s>
2016-01-19 16:02:04 +0200

    #111961263 - Popular materials frontend implementation

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -4,7 +4,7 @@ define(['app'], function(app)
     $scope.showHints = true;
 
     var params = {};
-    serverCallService.makeGet("rest/material/getNewestMaterials?numberOfMaterials=8", params, getNewestMaterialsSuccess, getNewestMaterialsFail);
+    serverCallService.makeGet("rest/material/getNewestMaterials?numberOfMaterials=8", params, getNewestMaterialsSuccess, requestFailed);
 
     function getNewestMaterialsSuccess(data) {
       if (isEmpty(data)) {
@@ -13,9 +13,21 @@ define(['app'], function(app)
         $scope.materials = data;
       }
     }
+    
+    
+    serverCallService.makeGet("rest/material/getPopularMaterials?numberOfMaterials=8", params, getPopularMaterialsSuccess, requestFailed);
 
-    function getNewestMaterialsFail(data, status) {
+    function getPopularMaterialsSuccess(data) {
+      if (isEmpty(data)) {
+        log('No data returned by session search.');
+      } else {
+        $scope.popularMaterials = data;
+      }
+    }
+
+    function requestFailed(data, status) {
       console.log('Session search failed.')
     }
+    
   }]);
 });
