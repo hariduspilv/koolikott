bb617b04a09f66f75a9abb628d94f5f5980bc772
fpoobus <fpoobus@netgroup-ThinkPad-T450s>
2016-01-20 13:25:51 +0200

    #98695722 - Frontend implementation to recommend material
    #98695732 - Frontend implementation to remove recommendation from
    material

--- app/directives/recommend/recommend.js
+++ app/directives/recommend/recommend.js
@@ -1,9 +1,49 @@
 define(['app'], function(app)
 {
-    app.directive('dopRecommend', ['translationService',
-     function(translationService) {
+    app.directive('dopRecommend', ['translationService', 'authenticatedUserService', 'serverCallService',
+     function(translationService, authenticatedUserService, serverCallService) {
         return {
-            templateUrl: 'directives/recommend/recommend.html'
+        	scope: {
+                material: '=',
+            },
+            templateUrl: 'directives/recommend/recommend.html',
+        	controller: function ($scope, $location) {
+        		
+            	$scope.recommend = function() {
+            		var url = "rest/material/recommend";
+            		if($scope.material && $scope.isAdmin()) {
+                        serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
+            		}
+            	}
+            	
+            	$scope.removeRecommendation = function() {
+            		var url = "rest/material/removeRecommendation";
+            		if($scope.material && $scope.isAdmin()) {
+                        serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
+            		}
+            	}
+                
+            	function querySuccess(data) {
+            		$scope.material.recommended = !$scope.material.recommended ;
+            		$scope.isRecommended = !$scope.isRecommended;
+            	}
+            	
+            	function queryFail() {
+            		log("Request failed");
+            	}
+            	
+            	$scope.isAdmin = function() {
+                    return authenticatedUserService.getUser() && authenticatedUserService.getUser().role === 'ADMIN';
+                };
+                
+                $scope.$watch("material", function(newValue, oldValue) {
+                	if(newValue && newValue.recommended != null) {
+	                	$scope.material = newValue;
+	                	$scope.isRecommended = $scope.material.recommended;
+                	}
+                });
+                
+            }
         };
     }]);
 
