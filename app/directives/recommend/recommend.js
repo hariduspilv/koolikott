b87d49476b7626bea383af461e7ea67fcc3e314f
fpoobus <fpoobus@netgroup-ThinkPad-T450s>
2016-01-21 12:39:46 +0200

    #104163054 - Frontend impl. to recommend portfolio
    #104163054 - Frontend impl. to remove recommendation from portfolio

--- app/directives/recommend/recommend.js
+++ app/directives/recommend/recommend.js
@@ -5,27 +5,42 @@ define(['app'], function(app)
         return {
         	scope: {
                 material: '=',
+                portfolio: '='
             },
             templateUrl: 'directives/recommend/recommend.html',
         	controller: function ($scope, $location) {
         		
             	$scope.recommend = function() {
-            		var url = "rest/material/recommend";
             		if($scope.material && $scope.isAdmin()) {
+            			var url = "rest/material/recommend";
                         serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
+            		}
+            		if($scope.portfolio && $scope.isAdmin()) {
+            			var url = "rest/portfolio/recommend";
+                        serverCallService.makePost(url, $scope.portfolio, querySuccess, queryFail);
             		}
             	}
             	
-            	$scope.removeRecommendation = function() {
-            		var url = "rest/material/removeRecommendation";
+            	$scope.removeRecommendation = function() {	
             		if($scope.material && $scope.isAdmin()) {
+            			var url = "rest/material/removeRecommendation";
                         serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
+            		}
+            		if($scope.portfolio && $scope.isAdmin()) {
+            			var url = "rest/portfolio/removeRecommendation";
+                        serverCallService.makePost(url, $scope.portfolio, querySuccess, queryFail);
             		}
             	}
                 
             	function querySuccess(data) {
-            		$scope.material.recommended = !$scope.material.recommended ;
-            		$scope.isRecommended = !$scope.isRecommended;
+            		if($scope.material && $scope.material.recommended != null) {
+	            		$scope.material.recommended = !$scope.material.recommended ;
+	            		$scope.isRecommended = !$scope.isRecommended;
+            		}
+            		if($scope.portfolio && $scope.portfolio.recommended != null) {
+	            		$scope.portfolio.recommended = !$scope.portfolio.recommended ;
+	            		$scope.isRecommended = !$scope.isRecommended;
+            		}
             	}
             	
             	function queryFail() {
@@ -40,6 +55,14 @@ define(['app'], function(app)
                 	if(newValue && newValue.recommended != null) {
 	                	$scope.material = newValue;
 	                	$scope.isRecommended = $scope.material.recommended;
+                	}
+                });
+                
+                $scope.$watch("portfolio", function(newValue, oldValue) {
+                	log(newValue);
+                	if(newValue && newValue.recommended != null) {
+	                	$scope.portfolio = newValue;
+	                	$scope.isRecommended = $scope.portfolio.recommended;
                 	}
                 });
                 
