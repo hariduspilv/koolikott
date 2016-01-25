074125b2513dfed8130ca67062a12698a64218e6
Rasmus Listra <rasmus.listra@netgroup.ee>
2016-01-25 16:30:20 +0200

    Adding new Admin for testing

--- misc/database/testData.sql
+++ misc/database/testData.sql
@@ -32,7 +32,7 @@ insert into User(id, userName, name, surName, idCode, role) values (6, 'mart.dev
 insert into User(id, userName, name, surName, idCode, role) values (7, 'ester.tester', 'Ester', 'Tester', '38202020234', 'USER');
 insert into User(id, userName, name, surName, idCode, role) values (8, 'admin.admin', 'Admin', 'Admin', '89898989898', 'ADMIN');
 insert into User(id, userName, name, surName, idCode, role) values (9, 'smallPublisher', 'Publisher', 'Publisher', '12345678900', 'PUBLISHER');
-
+insert into User(id, userName, name, surName, idCode, role) values (10, 'second.admin', 'Second', 'Admin', '89898989899', 'ADMIN');
 
 -- Materials
 
