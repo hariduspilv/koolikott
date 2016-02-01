bca6222efd0e33ca519bf5bfe870c9ffc7a28443
oliver <oliver.eivak@gmail.com>
2016-02-01 09:14:06 +0200

    #112403775 - Refactoring recommendations

--- app/directives/recommend/recommend.js
+++ app/directives/recommend/recommend.js
@@ -14,59 +14,44 @@ define([
             controller: function($scope, $location) {
 
                 $scope.recommend = function() {
-                    if ($scope.material && $scope.isAdmin()) {
-                        var url = "rest/material/recommend";
-                        serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
-                    }
-                    if ($scope.portfolio && $scope.isAdmin()) {
-                        var url = "rest/portfolio/recommend";
-                        serverCallService.makePost(url, $scope.portfolio, querySuccess, queryFail);
+                    if (authenticatedUserService.isAdmin())
+                        if ($scope.material) {
+                            var url = "rest/material/recommend";
+                            serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
+                        } else if ($scope.portfolio) {
+                            var url = "rest/portfolio/recommend";
+                            serverCallService.makePost(url, $scope.portfolio, querySuccess, queryFail);
+                        }
                     }
                 }
 
                 $scope.removeRecommendation = function() {
-                    if ($scope.material && $scope.isAdmin()) {
-                        var url = "rest/material/removeRecommendation";
-                        serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
-                    }
-                    if ($scope.portfolio && $scope.isAdmin()) {
-                        var url = "rest/portfolio/removeRecommendation";
-                        serverCallService.makePost(url, $scope.portfolio, querySuccess, queryFail);
+                    if (authenticatedUserService.isAdmin())
+                        if ($scope.material) {
+                            var url = "rest/material/removeRecommendation";
+                            serverCallService.makePost(url, $scope.material, querySuccess, queryFail);
+                        } else if ($scope.portfolio) {
+                            var url = "rest/portfolio/removeRecommendation";
+                            serverCallService.makePost(url, $scope.portfolio, querySuccess, queryFail);
+                        }
                     }
                 }
 
-                function querySuccess(data) {
-                    if ($scope.material && $scope.material.recommended != null) {
-                        $scope.material.recommended = !$scope.material.recommended;
-                        $scope.isRecommended = !$scope.isRecommended;
+                function querySuccess(recommendation) {
+                    if (isEmpty(recommendation)) {
+                        recommendation = null;
                     }
-                    if ($scope.portfolio && $scope.portfolio.recommended != null) {
-                        $scope.portfolio.recommended = !$scope.portfolio.recommended;
-                        $scope.isRecommended = !$scope.isRecommended;
+
+                    if ($scope.material) {
+                        $scope.material.recommendation = recommendation;
+                    } else if ($scope.portfolio) {
+                        $scope.portfolio.recommendation = recommendation;
                     }
                 }
 
                 function queryFail() {
                     log("Request failed");
                 }
-
-                $scope.isAdmin = function() {
-                    return authenticatedUserService.getUser() && authenticatedUserService.getUser().role === 'ADMIN';
-                };
-
-                $scope.$watch("material", function(newValue, oldValue) {
-                    if (newValue && newValue.recommended != null) {
-                        $scope.material = newValue;
-                        $scope.isRecommended = $scope.material.recommended;
-                    }
-                });
-
-                $scope.$watch("portfolio", function(newValue, oldValue) {
-                    if (newValue && newValue.recommended != null) {
-                        $scope.portfolio = newValue;
-                        $scope.isRecommended = $scope.portfolio.recommended;
-                    }
-                });
 
             }
         };
