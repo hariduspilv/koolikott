ca335484b92bf3713cc3365ac313bce3491c2c72
oliver <oliver.eivak@gmail.com>
2016-02-09 16:48:27 +0200

    #112736987 - Fixing authentication timeout (419) handling

--- app/services/serverCallService.js
+++ app/services/serverCallService.js
@@ -4,76 +4,86 @@ define(['angularAMD'], function(angularAMD) {
     angularAMD.factory('serverCallService', ['$http', '$location', 'authenticatedUserService', "Upload",
         function($http, $location, authenticatedUserService, Upload) {
 
-	    	function makeCall(url, method, params, includeAuthentication, successCallback, errorCallback, finallyCallback, transformRequest) {
-	    		headers = {};
-	    		
-	    		if (includeAuthentication) {
-	    			setAuthorization(headers);
-	    		}
-	
-	            var config = {
-	                method: method,
-	                url: url,
-	                headers: headers
-	            };
-	            
-	            if (method === 'POST' || method === 'PUT') {
-	            	config.data = params;
-	            } else {
-	            	config.params = params;
-	            }
-	            
-	            if (transformRequest) {
-	                config.transformRequest = transformRequest
-	            }
-	            
-	            $http(config).
-	            success(function(data) {
-	                successCallback(data);
-	            }).
-	            error(function(data, status, headers, config) {
-	                if (status == '419') {
-	                    authenticatedUserService.removeAuthenticatedUser();
-	                    instance.makePost(url, data, successCallback, errorCallback);
-	                } else {
-	                    errorCallback(data, status);
-	                }
-	            }).finally(finallyCallback);
-	        }
-	    	
-	    	function setAuthorization(headers) {
-	            if (authenticatedUserService.isAuthenticated()) {
-	            	var user = authenticatedUserService.getUser();
-	                headers.Authentication = authenticatedUserService.getToken();
-	                headers.Username = user.username;
-	            }
-	    	}
-	    	
+            function makeCall(url, method, params, includeAuthentication, successCallback, errorCallback, finallyCallback, transformRequest) {
+                headers = {};
+                
+                if (includeAuthentication) {
+                    setAuthorization(headers);
+                }
+    
+                var config = {
+                    method: method,
+                    url: url,
+                    headers: headers
+                };
+                
+                if (method === 'POST' || method === 'PUT') {
+                    config.data = params;
+                } else {
+                    config.params = params;
+                }
+                
+                if (transformRequest) {
+                    config.transformRequest = transformRequest
+                }
+                
+                $http(config).
+                success(function(data) {
+                    successCallback(data);
+                }).
+                error(function(data, status, headers, config) {
+                    if (status == '419') {
+                        authenticatedUserService.removeAuthenticatedUser();
+
+                        config.headers = {};
+
+                        $http(config).
+                        success(function(data) {
+                            successCallback(data);
+                        }).
+                        error(function(data, status, headers, config) {
+                            errorCallback(data, status);
+                        }).finally(finallyCallback);
+
+                    } else {
+                        errorCallback(data, status);
+                    }
+                }).finally(finallyCallback);
+            }
+            
+            function setAuthorization(headers) {
+                if (authenticatedUserService.isAuthenticated()) {
+                    var user = authenticatedUserService.getUser();
+                    headers.Authentication = authenticatedUserService.getToken();
+                    headers.Username = user.username;
+                }
+            }
+            
             instance = {
                 makePost: function(url, data, successCallback, errorCallback, finallyCallback) {
-                	makeCall(url, 'POST', data, true, successCallback, errorCallback, finallyCallback);
+                    makeCall(url, 'POST', data, true, successCallback, errorCallback, finallyCallback);
                 },
 
                 makeGet: function(url, params, successCallback, errorCallback, finallyCallback) {
-                	makeCall(url, 'GET', params, true, successCallback, errorCallback, finallyCallback);
+                    makeCall(url, 'GET', params, true, successCallback, errorCallback, finallyCallback);
                 },
 
-				makePut: function(url, data, successCallback, errorCallback, finallyCallback) {
-					makeCall(url, 'PUT', data, true, successCallback, errorCallback, finallyCallback);
-				},
-				makeDelete: function(url, data, successCallback, errorCallback, finallyCallback) {
-					makeCall(url, 'DELETE', data, true, successCallback, errorCallback, finallyCallback);
-				},
+                makePut: function(url, data, successCallback, errorCallback, finallyCallback) {
+                    makeCall(url, 'PUT', data, true, successCallback, errorCallback, finallyCallback);
+                },
+                makeDelete: function(url, data, successCallback, errorCallback, finallyCallback) {
+                    makeCall(url, 'DELETE', data, true, successCallback, errorCallback, finallyCallback);
+                },
 
                 makeJsonp: function(url, params, successCallback, errorCallback, finallyCallback) {
-                	makeCall(url, 'JSONP', params, false, successCallback, errorCallback, finallyCallback);
+                    makeCall(url, 'JSONP', params, false, successCallback, errorCallback, finallyCallback);
                 },
                 
                 upload: function(url, data, successCallback, errorCallback, finallyCallback) {
-                	var headers = {};
-                	setAuthorization(headers);
-                	
-                	Upload.upload({
+                    var headers = {};
+                    setAuthorization(headers);
+                    
+                    Upload.upload({
                         url: url,
                         data: data,
                         headers: headers
