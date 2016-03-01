cb507f495a3ab54a7c910c25ff8ebc4ab2a35e82
martlausNG <mart.laus@netgroup.ee>
2016-03-01 15:09:37 +0200

    #114488019 added translations

--- src/main/resources/db/migration/V2.0__Release_migration_1_0_1.sql
+++ src/main/resources/db/migration/V2.0__Release_migration_1_0_1.sql
@@ -37,4 +37,16 @@ INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'ADD_TO_NEW_PORTFOLIO','Lisa materjalid uude kogumikku');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'ADD_TO_NEW_PORTFOLIO','Добавить материалы в новый портфель');
 
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'ADD_MATERIAL_TO_EXISTING_PORTFOLIO','Add materials to new portfolio');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'ADD_MATERIAL_TO_EXISTING_PORTFOLIO','Lisa materjalid uude kogumikku');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'ADD_MATERIAL_TO_EXISTING_PORTFOLIO','Добавить материалы в новый портфель');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'CHOOSE_PORTFOLIO','Add materials to new portfolio');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'CHOOSE_PORTFOLIO','Lisa materjalid uude kogumikku');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'CHOOSE_PORTFOLIO','Добавить материалы в новый портфель');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'CHOOSE_PORTFOLIO_CHAPTER','Add materials to new portfolio');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'CHOOSE_PORTFOLIO_CHAPTER','Lisa materjalid uude kogumikku');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'CHOOSE_PORTFOLIO_CHAPTER','Добавить материалы в новый портфель');
+
 SET foreign_key_checks = 1;
