1dc0d6a5c80434ccf53ae80725416da143c749b4
martlausNG <mart.laus@netgroup.ee>
2016-03-01 15:43:44 +0200

    #114488093 showing list of users portfolios

--- app/views/addMaterialToExistingPortfolio/addMaterialToExistingPortfolio.js
+++ app/views/addMaterialToExistingPortfolio/addMaterialToExistingPortfolio.js
@@ -9,6 +9,7 @@ define([
         function ($scope, $mdDialog, serverCallService, translationService, metadataService, $rootScope, authenticatedUserService) {
             $scope.step = {};
             $scope.step.currentStep = 0;
+            getUsersPortfolios();
 
             $scope.cancel = function () {
                 $mdDialog.hide();
@@ -17,5 +18,30 @@ define([
             $scope.step.previousStep = function () {
                 $scope.step.currentStep -= 1;
             };
+
+            $scope.chooseChapter = function (selectedPortfolio) {
+                $scope.step.currentStep = 1;
+            };
+
+            function getUsersPortfolios() {
+                var user = authenticatedUserService.getUser();
+                var params = {
+                    'username': user.username
+                };
+                var url = "rest/portfolio/getByCreator";
+                serverCallService.makeGet(url, params, getUsersPortfoliosSuccess, getUsersPortfoliosFail);
+            }
+
+            function getUsersPortfoliosSuccess(data) {
+                if (isEmpty(data)) {
+                    getUsersPortfoliosFail();
+                } else {
+                    $scope.portfolios = data;
+                }
+            }
+
+            function getUsersPortfoliosFail() {
+                console.log('Failed to get portfolios.');
+            }
         }];
 });
