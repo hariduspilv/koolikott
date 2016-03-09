7673185cd1e49d4a72ed9caedd79121344ba9aee
oliver <oliver.eivak@gmail.com>
2016-03-09 15:46:47 +0200

    #113722195 - Add tooltip for reporting improper tags

---
+++ src/main/resources/db/migration/V2.2.0000__Release_migration_1_0_2.sql
@@ -0,0 +1,3 @@
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TAG_TOOLTIP_REPORT_AS_IMPROPER','Notify of improper tag');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TAG_TOOLTIP_REPORT_AS_IMPROPER','Teavita ebasobivast võtmesõnast');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TAG_TOOLTIP_REPORT_AS_IMPROPER','Уведомить неправильного ключевого слова');
