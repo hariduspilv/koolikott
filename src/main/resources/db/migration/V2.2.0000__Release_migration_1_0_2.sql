63c8ea1531d4dcc38e65ada5a89e31bfd91d2823
oliver <oliver.eivak@gmail.com>
2016-03-18 09:37:28 +0200

    #115868467 - Add translations for "log in to rate" tooltip

--- src/main/resources/db/migration/V2.2.0000__Release_migration_1_0_2.sql
+++ src/main/resources/db/migration/V2.2.0000__Release_migration_1_0_2.sql
@@ -6,3 +6,6 @@ INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'COPY_PERMALINK_SUCCESS','Link kopeeritud!');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'COPY_PERMALINK_SUCCESS','Ссылка скопирована!');
 
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TOOLTIP_LOG_IN_TO_RATE','Log in to rate');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TOOLTIP_LOG_IN_TO_RATE','Logi sisse, et hinnata');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TOOLTIP_LOG_IN_TO_RATE','Войти, чтобы оценить');
