57544c442d655257f5a5838661a6f31852edee9b
martlausNG <mart.laus@netgroup.ee>
2016-03-18 09:25:12 +0200

    #115868955 added translations

--- src/main/resources/db/migration/V2.2.0000__Release_migration_1_0_2.sql
+++ src/main/resources/db/migration/V2.2.0000__Release_migration_1_0_2.sql
@@ -1,3 +1,8 @@
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TAG_TOOLTIP_REPORT_AS_IMPROPER','Notify of improper tag');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TAG_TOOLTIP_REPORT_AS_IMPROPER','Teavita ebasobivast võtmesõnast');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TAG_TOOLTIP_REPORT_AS_IMPROPER','Уведомить неправильного ключевого слова');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'COPY_PERMALINK_SUCCESS','Link copied!');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'COPY_PERMALINK_SUCCESS','Link kopeeritud!');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'COPY_PERMALINK_SUCCESS','Ссылка скопирована!');
+
