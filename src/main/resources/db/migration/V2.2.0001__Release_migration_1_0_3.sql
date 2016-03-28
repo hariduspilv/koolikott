905bbac19256386d406fb56335d880ac5254b6ca
oliver <oliver.eivak@gmail.com>
2016-03-28 12:31:11 +0300

    #114192739 - Add translation for notifying of improper tag

---
+++ src/main/resources/db/migration/V2.2.0001__Release_migration_1_0_3.sql
@@ -0,0 +1,3 @@
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'REASON_IMPROPER_TAG','Reason: improper tag');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'REASON_IMPROPER_TAG','Põhjus: ebasobiv võtmesõna');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'REASON_IMPROPER_TAG','Причина: неправильное ключевое слово');
