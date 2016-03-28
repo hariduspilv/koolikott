0ff64ced0a47decd97f1c90962eb3fda26e42b80
oliver <oliver.eivak@gmail.com>
2016-03-28 14:09:31 +0300

    #113722417 - Add translation for removing tag upvote

--- src/main/resources/db/migration/V2.2.0001__Release_migration_1_0_3.sql
+++ src/main/resources/db/migration/V2.2.0001__Release_migration_1_0_3.sql
@@ -1,3 +1,7 @@
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'REASON_IMPROPER_TAG','Reason: improper tag');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'REASON_IMPROPER_TAG','Põhjus: ebasobiv võtmesõna');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'REASON_IMPROPER_TAG','Причина: неправильное ключевое слово');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TAG_TOOLTIP_REMOVE_UPVOTE','Remove upvote');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TAG_TOOLTIP_REMOVE_UPVOTE','Eemalda +1');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TAG_TOOLTIP_REMOVE_UPVOTE','Удалить +1');
