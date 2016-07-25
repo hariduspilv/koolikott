267aec1c4081e97d4d016c253c87aab817381873
Aleks Tatter <alekstatter@gmail.com>
2016-07-25 14:19:29 +0300

    #17 Added tooltip translations

---
+++ back-end/src/main/resources/db/migration/V5.2.1__Release_migration_1_0_8.sql
@@ -0,0 +1,19 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TOOLTIP_OPEN_CHAPTER','Ava kogumiku peatükk');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TOOLTIP_OPEN_CHAPTER','Open chapter');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TOOLTIP_OPEN_CHAPTER','Открыть раздел');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TOOLTIP_CLOSE_CHAPTER','Sulge kogumiku peatükk');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TOOLTIP_CLOSE_CHAPTER','Close chapter');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TOOLTIP_CLOSE_CHAPTER','Закрыть раздел');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TOOLTIP_OPEN_SUBCHAPTER','Ava kogumiku alampeatükk');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TOOLTIP_OPEN_SUBCHAPTER','Open subchapter');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TOOLTIP_OPEN_SUBCHAPTER','Открыть подраздел');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'TOOLTIP_CLOSE_SUBCHAPTER','Sulge kogumiku alampeatükk');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'TOOLTIP_CLOSE_SUBCHAPTER','Close subchapter');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'TOOLTIP_CLOSE_SUBCHAPTER','Закрыть подраздел');
+
+SET foreign_key_checks = 1;
