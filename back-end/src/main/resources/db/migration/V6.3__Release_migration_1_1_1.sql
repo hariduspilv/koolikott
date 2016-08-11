b8511745ab082fa5f10a70d3704fe5aa0d8fc25e
martlausNG <mart.laus@netgroup.ee>
2016-08-11 10:05:08 +0300

    #33 Add translations

---
+++ back-end/src/main/resources/db/migration/V6.3__Release_migration_1_1_1.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'UPDATE_FILE','Uuenda faili');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'UPDATE_FILE','Update file');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'UPDATE_FILE','Обновление файла');
+
+SET foreign_key_checks = 1;
