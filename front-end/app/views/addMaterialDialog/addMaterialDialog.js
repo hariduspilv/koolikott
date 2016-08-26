8229a0d1f893210467867fcdc88f74e8091c5310
martlausNG <mart.laus@netgroup.ee>
2016-08-26 10:59:41 +0300

    #23 Add publisher null check

--- front-end/app/views/addMaterialDialog/addMaterialDialog.js
+++ front-end/app/views/addMaterialDialog/addMaterialDialog.js
@@ -116,7 +116,7 @@ define([
                         $scope.material.uploadedFile = null;
                     }
 
-                    if (!$scope.material.publishers[0].name) {
+                    if ($scope.material.publishers[0] && !$scope.material.publishers[0].name) {
                         $scope.material.publishers[0] = null;
                     }
 
