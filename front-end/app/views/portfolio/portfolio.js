49e11af121347b9a9e9e7672af6f5c93bd417fa0
martlausNG <mart.laus@netgroup.ee>
2016-09-12 14:53:36 +0300

    Fix clicking on reccomended portfolio in view portfolio

--- front-end/app/views/portfolio/portfolio.js
+++ front-end/app/views/portfolio/portfolio.js
@@ -16,97 +16,106 @@ define([
 ], function (app) {
     return ['$scope', 'translationService', 'serverCallService', '$route', '$location', 'alertService', '$rootScope', 'authenticatedUserService', '$timeout',
         function ($scope, translationService, serverCallService, $route, $location, alertService, $rootScope, authenticatedUserService, $timeout) {
-        var increaseViewCountPromise;
+            var increaseViewCountPromise;
 
-        function init() {
-            if ($rootScope.savedPortfolio) {
-                setPortfolio($rootScope.savedPortfolio);
-                increaseViewCount();
-            } else {
-                getPortfolio(getPortfolioSuccess, getPortfolioFail);
-            }
-
-            $scope.newComment = {};
-        }
-
-        function getPortfolio(success, fail) {
-            var portfolioId = $route.current.params.id;
-            serverCallService.makeGet("rest/portfolio?id=" + portfolioId, {}, success, fail);
-        }
-
-        function getPortfolioSuccess(portfolio) {
-            if (isEmpty(portfolio)) {
-                getPortfolioFail();
-            } else {
-                setPortfolio(portfolio);
-                increaseViewCount();
-
-            }
-        }
-
-        function getPortfolioFail() {
-            log('No data returned by getting portfolio.');
-            alertService.setErrorAlert('ERROR_PORTFOLIO_NOT_FOUND');
-            $location.url("/");
-        }
-
-        function increaseViewCount() {
-            /*
-             *  It is needed to have it in a timeout because of double call caused by using two different page structure.
-             *  So we cancel it in case the page is destroyed so the new one that will be create makes the valid call.
-             */
-            increaseViewCountPromise = $timeout(function () {
-                if ($scope.portfolio) {
-                    var portfolio = createPortfolio($scope.portfolio.id);
-                    serverCallService.makePost("rest/portfolio/increaseViewCount", portfolio, function success() {
-                    }, function fail() {
-                    });
+            function init() {
+                if ($rootScope.savedPortfolio) {
+                    setPortfolio($rootScope.savedPortfolio);
+                    increaseViewCount();
+                } else {
+                    getPortfolio(getPortfolioSuccess, getPortfolioFail);
                 }
-            }, 1000);
-        }
 
-        $scope.addComment = function () {
-            var url = "rest/comment/portfolio";
+                $scope.newComment = {};
+            }
 
-            var portfolio = createPortfolio($scope.portfolio.id);
-            var params = {
-                'comment': $scope.newComment,
-                'portfolio': portfolio
+            function getPortfolio(success, fail) {
+                var portfolioId = $route.current.params.id;
+                serverCallService.makeGet("rest/portfolio?id=" + portfolioId, {}, success, fail);
+            }
+
+            function getPortfolioSuccess(portfolio) {
+                if (isEmpty(portfolio)) {
+                    getPortfolioFail();
+                } else {
+                    setPortfolio(portfolio);
+                    increaseViewCount();
+
+                }
+            }
+
+            function getPortfolioFail() {
+                log('No data returned by getting portfolio.');
+                alertService.setErrorAlert('ERROR_PORTFOLIO_NOT_FOUND');
+                $location.url("/");
+            }
+
+            function increaseViewCount() {
+                /*
+                 *  It is needed to have it in a timeout because of double call caused by using two different page structure.
+                 *  So we cancel it in case the page is destroyed so the new one that will be create makes the valid call.
+                 */
+                increaseViewCountPromise = $timeout(function () {
+                    if ($scope.portfolio) {
+                        var portfolio = createPortfolio($scope.portfolio.id);
+                        serverCallService.makePost("rest/portfolio/increaseViewCount", portfolio, function success() {
+                        }, function fail() {
+                        });
+                    }
+                }, 1000);
+            }
+
+            $scope.addComment = function () {
+                var url = "rest/comment/portfolio";
+
+                var portfolio = createPortfolio($scope.portfolio.id);
+                var params = {
+                    'comment': $scope.newComment,
+                    'portfolio': portfolio
+                };
+                serverCallService.makePost(url, params, addCommentSuccess, addCommentFailed);
             };
-            serverCallService.makePost(url, params, addCommentSuccess, addCommentFailed);
-        };
 
-        function addCommentSuccess() {
-            $scope.newComment.text = "";
+            function addCommentSuccess() {
+                $scope.newComment.text = "";
 
-            getPortfolio(function (portfolio) {
+                getPortfolio(function (portfolio) {
+                    $scope.portfolio = portfolio;
+                }, function () {
+                    log("Comment success, but failed to reload portfolio.");
+                });
+            }
+
+            function addCommentFailed() {
+                log('Adding comment failed.');
+            }
+
+            function setPortfolio(portfolio) {
                 $scope.portfolio = portfolio;
-            }, function () {
-                log("Comment success, but failed to reload portfolio.");
+                $rootScope.savedPortfolio = portfolio;
+            }
+
+
+            $scope.$watch(function () {
+                return $location.url();
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    $route.reload()
+                }
+            }, true);
+
+            $scope.$on('$routeChangeStart', function () {
+                if (!$location.url().startsWith("/portfolio/edit?id=")) {
+                    setPortfolio(null);
+                }
             });
-        }
 
-        function addCommentFailed() {
-            log('Adding comment failed.');
-        }
+            $scope.$on('$destroy', function () {
+                if (increaseViewCountPromise) {
+                    $timeout.cancel(increaseViewCountPromise);
+                }
+            });
 
-        function setPortfolio(portfolio) {
-            $scope.portfolio = portfolio;
-            $rootScope.savedPortfolio = portfolio;
-        }
-
-        $scope.$on('$routeChangeStart', function () {
-            if (!$location.url().startsWith("/portfolio/edit?id=")) {
-                setPortfolio(null);
-            }
-        });
-
-        $scope.$on('$destroy', function () {
-            if (increaseViewCountPromise) {
-                $timeout.cancel(increaseViewCountPromise);
-            }
-        });
-
-        init();
-    }];
+            init();
+        }];
 });
