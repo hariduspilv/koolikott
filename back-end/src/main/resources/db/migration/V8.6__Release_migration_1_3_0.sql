3afcfb9e3711b4ec0534a437b5f067041e746628
Rannar Allorg <rannar.allorg@netgroup.ee>
2016-10-06 16:21:53 +0300

    #2 Add database migrations

---
+++ back-end/src/main/resources/db/migration/V8.6__Release_migration_1_3_0.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1,'MATERIAL_ALREADY_EXISTS','Selline materjal on juba olemas');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3,'MATERIAL_ALREADY_EXISTS','This material already exists');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2,'MATERIAL_ALREADY_EXISTS','Такой материал уже присутствует');
+
+SET foreign_key_checks = 1;
