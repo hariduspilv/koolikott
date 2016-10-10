976a84a7d123312b826dd578893673494b500d2f
martlausNG <mart.laus@netgroup.ee>
2016-10-10 19:09:54 +0300

    #74 Add moving cards up and down

--- front-end/app/directives/embeddedMaterial/embeddedMaterial.js
+++ front-end/app/directives/embeddedMaterial/embeddedMaterial.js
@@ -4,63 +4,84 @@ define([
     'services/iconService',
     'services/embedService'
 ], function (app) {
-    app.directive('dopEmbeddedMaterial', ['translationService', 'iconService', 'embedService', function (translationService, iconService, embedService) {
-        return {
-            scope: {
-                material: '=',
-                chapter: '='
-            },
-            templateUrl: 'directives/embeddedMaterial/embeddedMaterial.html',
-            controller: function ($scope) {
-                init();
+    app.directive('dopEmbeddedMaterial', ['translationService', 'iconService', 'embedService', '$rootScope',
+        function (translationService, iconService, embedService, $rootScope) {
+            return {
+                scope: {
+                    material: '=',
+                    chapter: '=',
+                    index: '='
+                },
+                templateUrl: 'directives/embeddedMaterial/embeddedMaterial.html',
+                controller: function ($scope, $rootScope) {
+                    init();
 
-                function init() {
-                    if ($scope.material) {
-                        embedService.getEmbed(getSource($scope.material), embedCallback);
-                        $scope.materialType = getType();
-                    }
-                }
+                    function init() {
+                        $scope.isEditPortfolioPage = $rootScope.isEditPortfolioPage;
+                        $scope.isEditPortfolioMode = $rootScope.isEditPortfolioMode;
 
-                $scope.removeMaterial = function ($event, material) {
-                    $event.preventDefault();
-                    $event.stopPropagation();
-
-                    var index = $scope.chapter.materials.indexOf(material);
-                    $scope.chapter.materials.splice(index, 1);
-                };
-
-                $scope.getCorrectLanguageTitle = function (material) {
-                    if (material) {
-                        return getCorrectLanguageString(material.titles, material.language);
-                    }
-                };
-
-                function getType() {
-                    if ($scope.material === undefined || $scope.material === null) return '';
-
-                    return iconService.getMaterialIcon($scope.material.resourceTypes);
-                }
-
-                function getCorrectLanguageString(languageStringList, materialLanguage) {
-                    if (languageStringList) {
-                        return getUserDefinedLanguageString(languageStringList, translationService.getLanguage(), materialLanguage);
-                    }
-                }
-
-                function embedCallback(res) {
-                    if (res && res.data.html) {
-                        $scope.embeddedDataIframe = null;
-                        $scope.embeddedData = null;
-
-                        if (res.data.html.contains("<iframe")) {
-                            $scope.embeddedDataIframe = res.data.html.replace("http:", "");
-                        } else {
-                            $scope.embeddedData = res.data.html.replace("http:", "");
+                        if ($scope.material) {
+                            embedService.getEmbed(getSource($scope.material), embedCallback);
+                            $scope.materialType = getType();
                         }
                     }
-                }
 
-            }
-        };
-    }]);
+                    $scope.removeMaterial = function ($event, material) {
+                        $event.preventDefault();
+                        $event.stopPropagation();
+
+                        var index = $scope.chapter.materials.indexOf(material);
+                        $scope.chapter.materials.splice(index, 1);
+                    };
+
+                    $scope.getCorrectLanguageTitle = function (material) {
+                        if (material) {
+                            return getCorrectLanguageString(material.titles, material.language);
+                        }
+                    };
+
+                    $scope.moveItem = function (origin, destination) {
+                        console.log(origin, destination)
+                        var temp = $scope.chapter.materials[destination];
+                        $scope.chapter.materials[destination] = $scope.chapter.materials[origin];
+                        $scope.chapter.materials[origin] = temp;
+                    };
+
+                    $scope.listItemUp = function (itemIndex) {
+                        $scope.moveItem(itemIndex, itemIndex - 1);
+                    };
+
+                    $scope.listItemDown = function (itemIndex) {
+                        $scope.moveItem(itemIndex, itemIndex + 1);
+
+                    };
+
+                    function getType() {
+                        if ($scope.material === undefined || $scope.material === null) return '';
+
+                        return iconService.getMaterialIcon($scope.material.resourceTypes);
+                    }
+
+                    function getCorrectLanguageString(languageStringList, materialLanguage) {
+                        if (languageStringList) {
+                            return getUserDefinedLanguageString(languageStringList, translationService.getLanguage(), materialLanguage);
+                        }
+                    }
+
+                    function embedCallback(res) {
+                        if (res && res.data.html) {
+                            $scope.embeddedDataIframe = null;
+                            $scope.embeddedData = null;
+
+                            if (res.data.html.contains("<iframe")) {
+                                $scope.embeddedDataIframe = res.data.html.replace("http:", "");
+                            } else {
+                                $scope.embeddedData = res.data.html.replace("http:", "");
+                            }
+                        }
+                    }
+
+                }
+            };
+        }]);
 });
