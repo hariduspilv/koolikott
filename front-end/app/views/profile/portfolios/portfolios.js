faac36ab4b2cfb27390793a3847256bffc4aef36
martlausNG <mart.laus@netgroup.ee>
2016-10-21 10:57:17 +0300

    #82 Show user specific translations

--- front-end/app/views/profile/portfolios/portfolios.js
+++ front-end/app/views/profile/portfolios/portfolios.js
@@ -5,27 +5,37 @@ define([
 ], function (app) {
     return ['$scope', '$route', 'authenticatedUserService', 'serverCallService',
         function ($scope, $route, authenticatedUserService, serverCallService) {
-        function init() {
-            getUserPortfolios();
-        }
-
-        function getUserPortfolios() {
-            var params = {
-                'username': authenticatedUserService.getUser().username
+            function init() {
+                getUserPortfolios();
+                isMyProfilePage();
             }
-            serverCallService.makeGet("rest/portfolio/getByCreator", params, getPortfoliosSuccess, getDataFailed)
-        }
 
-        function getPortfoliosSuccess(data) {
-            if(data) {
-                $scope.portfolios = data
+            function getUserPortfolios() {
+                var params = {
+                    'username': authenticatedUserService.getUser().username
+                };
+                serverCallService.makeGet("rest/portfolio/getByCreator", params, getPortfoliosSuccess, getDataFailed)
             }
-        }
 
-        function getDataFailed() {
-            console.log("Failed to get data")
-        }
+            function getPortfoliosSuccess(data) {
+                if (data) {
+                    $scope.portfolios = data
+                }
+            }
 
-        init();
-    }];
+            function getDataFailed() {
+                console.log("Failed to get data")
+            }
+
+            function isMyProfilePage() {
+                if (authenticatedUserService.isAuthenticated()) {
+                    var user = authenticatedUserService.getUser()
+                    if (user && $route.current.params.username === user.username) {
+                        $scope.myProfile = true;
+                    }
+                }
+            }
+
+            init();
+        }];
 });
