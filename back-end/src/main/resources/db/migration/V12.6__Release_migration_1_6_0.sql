7b3643416d75f8cb0ca377d715799b2ca0ece85f
martlausNG <mart.laus@netgroup.ee>
2016-12-05 13:50:58 +0200

    #49 Readd translations

---
+++ back-end/src/main/resources/db/migration/V12.6__Release_migration_1_6_0.sql
@@ -0,0 +1,11 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'USER_UPDATED', 'Kasutaja uuendatud');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'USER_UPDATED', 'User updated');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'USER_UPDATED', 'Пользователь обновлен');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'BUTTON_SAVE', 'Salvesta');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'BUTTON_SAVE', 'Save');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'BUTTON_SAVE', 'Сохранить');
+
+SET foreign_key_checks = 1;
