ca6eb878b3d9c03be4c3e65f2c97ec04e95b10b6
mare hunt <mare.hunt@netgroup.ee>
2016-12-06 20:04:54 +0200

    #121 Basic toolbar design

---
+++ back-end/src/main/resources/db/migration/V12.8__Release_migration_1_6_0.sql
@@ -0,0 +1,6 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'MATERIAL', 'Materjal');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SUBCHAPTER', 'Alampeatükk');
+
+SET foreign_key_checks = 1;
