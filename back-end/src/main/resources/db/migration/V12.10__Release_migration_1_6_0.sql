07a312c356c2aab97c8540090469da467efd195b
gert <gglukmann@gmail.com>
2016-12-07 09:10:00 +0200

    #125 Different translations for delete buttons

---
+++ back-end/src/main/resources/db/migration/V12.10__Release_migration_1_6_0.sql
@@ -0,0 +1,15 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'DELETE_MATERIAL', 'Eemalda materjal');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'DELETE_MATERIAL', 'Remove material');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'DELETE_MATERIAL', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'DELETE_SUBCHAPTER', 'Kustuta alampeatükk');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'DELETE_SUBCHAPTER', 'Delete subchapter');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'DELETE_SUBCHAPTER', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'DELETE_CHAPTER', 'Kustuta peatükk');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'DELETE_CHAPTER', 'Delete chapter');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'DELETE_CHAPTER', '');
+
+SET foreign_key_checks = 1;
