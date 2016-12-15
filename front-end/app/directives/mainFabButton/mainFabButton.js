936cf9f0d0c8459c17bdc709a55255a527e280d2
Aleks Tatter <alekstatter@gmail.com>
2016-12-15 12:39:12 +0200

    #137 FAB update

--- front-end/app/directives/mainFabButton/mainFabButton.js
+++ front-end/app/directives/mainFabButton/mainFabButton.js
@@ -6,91 +6,102 @@ define([
 ], function(angularAMD) {
     angularAMD.directive('dopMainFabButton', ['$rootScope', 'serverCallService', '$route', 'storageService', '$filter',  'toastService',
         function($rootScope, serverCallService, $route, storageService, $filter, toastService) {
-        return {
-            scope: true,
-            templateUrl: 'directives/mainFabButton/mainFabButton.html',
-            controller: function($scope, $mdDialog, $location, authenticatedUserService, $rootScope) {
-                $scope.isOpen = false;
-                $scope.userHasSelectedMaterials = false;
+            return {
+                scope: true,
+                templateUrl: 'directives/mainFabButton/mainFabButton.html',
+                controller: function($scope, $mdDialog, $location, authenticatedUserService, $rootScope) {
+                    $scope.isOpen = false;
+                    $scope.userHasSelectedMaterials = false;
 
-                $rootScope.$watch('selectedMaterials.length', function (newValue) {
-                    $scope.userHasSelectedMaterials = newValue > 0;
-                },false);
+                    $rootScope.$watch('selectedMaterials.length', function (newValue) {
+                        $scope.userHasSelectedMaterials = newValue > 0;
+                    },false);
 
-                $rootScope.$watch('selectedSingleMaterial', function (newValue) {
+                    $rootScope.$watch('selectedSingleMaterial', function (newValue) {
                         $scope.userHasSelectedMaterials = newValue !== null;
-                },false);
+                    },false);
 
-                $scope.showAddPortfolioDialog = function() {
-                    var emptyPortfolio = createPortfolio();
+                    $scope.showAddPortfolioDialog = function(e) {
+                        e.preventDefault();
+                        var emptyPortfolio = createPortfolio();
 
-                    if($scope.userHasSelectedMaterials || $rootScope.selectedSingleMaterial) {
-                        emptyPortfolio.chapters = [];
+                        if($scope.userHasSelectedMaterials || $rootScope.selectedSingleMaterial) {
+                            emptyPortfolio.chapters = [];
 
-                        emptyPortfolio.chapters.push({
-                            title: $filter('translate')('PORTFOLIO_DEFAULT_NEW_CHAPTER_TITLE'),
-                            subchapters: [],
-                            materials: []
-                        });
+                            emptyPortfolio.chapters.push({
+                                title: $filter('translate')('PORTFOLIO_DEFAULT_NEW_CHAPTER_TITLE'),
+                                subchapters: [],
+                                materials: []
+                            });
 
-                        if ($rootScope.selectedMaterials && $rootScope.selectedMaterials.length > 0) {
-                            for (var i = 0; i < $rootScope.selectedMaterials.length; i++) {
-                                var selectedMaterial = $rootScope.selectedMaterials[i];
-                                emptyPortfolio.chapters[0].materials.push(selectedMaterial);
+                            if ($rootScope.selectedMaterials && $rootScope.selectedMaterials.length > 0) {
+                                for (var i = 0; i < $rootScope.selectedMaterials.length; i++) {
+                                    var selectedMaterial = $rootScope.selectedMaterials[i];
+                                    emptyPortfolio.chapters[0].materials.push(selectedMaterial);
+                                }
+                            } else if($rootScope.selectedSingleMaterial != null) {
+                                emptyPortfolio.chapters[0].materials.push($rootScope.selectedSingleMaterial);
                             }
-                        } else if($rootScope.selectedSingleMaterial != null) {
-                            emptyPortfolio.chapters[0].materials.push($rootScope.selectedSingleMaterial);
+                            toastService.showOnRouteChange('PORTFOLIO_ADD_MATERIAL_SUCCESS');
                         }
-                        toastService.showOnRouteChange('PORTFOLIO_ADD_MATERIAL_SUCCESS');
+
+                        storageService.setPortfolio(emptyPortfolio);
+
+                        $mdDialog.show(angularAMD.route({
+                            templateUrl: 'views/addPortfolioDialog/addPortfolioDialog.html',
+                            controllerUrl: 'views/addPortfolioDialog/addPortfolioDialog'
+                        }));
+                    };
+
+                    $scope.showAddMaterialsToPortfolioDialog = function() {
+                        $mdDialog.show(angularAMD.route({
+                            templateUrl: 'views/addMaterialToExistingPortfolio/addMaterialToExistingPortfolio.html',
+                            controllerUrl: 'views/addMaterialToExistingPortfolio/addMaterialToExistingPortfolio'
+                        }));
+                    };
+
+                    $scope.showAddMaterialDialog = function() {
+                        $mdDialog.show(angularAMD.route({
+                            templateUrl: 'addMaterialDialog.html',
+                            controllerUrl: 'views/addMaterialDialog/addMaterialDialog'
+                        }))
+                    };
+
+                    $scope.copyPortfolio = function() {
+                        var url = "rest/portfolio/copy";
+                        var portfolio = createPortfolio($route.current.params.id);
+                        serverCallService.makePost(url, portfolio, createPortfolioSuccess, createPortfolioFailed);
+                    };
+
+                    function createPortfolioSuccess(portfolio) {
+                        if (isEmpty(portfolio)) {
+                            createPortfolioFailed();
+                        } else {
+                            $rootScope.savedPortfolio = portfolio;
+                            $rootScope.openMetadataDialog = true;
+                            $mdDialog.hide();
+                            $location.url('/portfolio/edit?id=' + portfolio.id);
+                        }
                     }
 
-                    storageService.setPortfolio(emptyPortfolio);
+                    function createPortfolioFailed() {
+                        log('Creating copy of portfolio failed.');
+                    }
 
-                    $mdDialog.show(angularAMD.route({
-                        templateUrl: 'views/addPortfolioDialog/addPortfolioDialog.html',
-                        controllerUrl: 'views/addPortfolioDialog/addPortfolioDialog'
-                    }));
-                };
+                    $scope.hasPermission = function() {
+                        return authenticatedUserService.getUser() && !authenticatedUserService.isRestricted();
+                    };
 
-                $scope.showAddMaterialsToPortfolioDialog = function() {
-                    $mdDialog.show(angularAMD.route({
-                        templateUrl: 'views/addMaterialToExistingPortfolio/addMaterialToExistingPortfolio.html',
-                        controllerUrl: 'views/addMaterialToExistingPortfolio/addMaterialToExistingPortfolio'
-                    }));
-                };
+                    $scope.setFabState = function(state) {
+                        if(!isTouchDevice()) {
+                            $scope.isOpen = state;
+                        }
+                    };
 
-                $scope.showAddMaterialDialog = function() {
-                    $mdDialog.show(angularAMD.route({
-                        templateUrl: 'addMaterialDialog.html',
-                        controllerUrl: 'views/addMaterialDialog/addMaterialDialog'
-                    }))
-                };
-
-                $scope.copyPortfolio = function() {
-                    var url = "rest/portfolio/copy";
-                    var portfolio = createPortfolio($route.current.params.id);
-                    serverCallService.makePost(url, portfolio, createPortfolioSuccess, createPortfolioFailed);
-                };
-
-                function createPortfolioSuccess(portfolio) {
-                    if (isEmpty(portfolio)) {
-                        createPortfolioFailed();
-                    } else {
-                        $rootScope.savedPortfolio = portfolio;
-                        $rootScope.openMetadataDialog = true;
-                        $mdDialog.hide();
-                        $location.url('/portfolio/edit?id=' + portfolio.id);
+                    function isTouchDevice() {
+                        return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
                     }
                 }
-
-                function createPortfolioFailed() {
-                    log('Creating copy of portfolio failed.');
-                }
-
-                $scope.hasPermission = function() {
-                    return authenticatedUserService.getUser() && !authenticatedUserService.isRestricted();
-                };
-            }
-        };
-    }]);
+            };
+        }]);
 });
