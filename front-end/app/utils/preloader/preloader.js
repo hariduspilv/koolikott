178771fc2a2ac83ebd243b32f1cae11a68a7bf19
martlausNG <mart.laus@netgroup.ee>
2016-12-21 15:46:10 +0200

    #142 Reduce preloader size

--- front-end/app/utils/preloader/preloader.js
+++ front-end/app/utils/preloader/preloader.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var element = document.getElementById("preloader");
-    element.parentNode.removeChild(element);
+    setTimeout(function () {
+        element.parentNode.removeChild(element);
+    }, 3000)
 });
