72ffd08364d8e7fc81ba9027e736aca27558d29f
Rannar Allorg <rannar.allorg@netgroup.ee>
2016-12-27 11:28:31 +0200

    #152  Refactor portfolio visibility dialog

--- front-end/app/views/editPortfolio/editPortfolio.js
+++ front-end/app/views/editPortfolio/editPortfolio.js
@@ -1,129 +1,108 @@
 'use strict'
 
 angular.module('koolikottApp')
-.controller('editPortfolioController',
-[
-    '$scope', 'translationService', 'serverCallService', '$route', '$location', 'alertService', '$rootScope', 'authenticatedUserService', 'dialogService', 'toastService', '$mdDialog', '$interval', '$filter', '$timeout', '$document', 'storageService',
-    function ($scope, translationService, serverCallService, $route, $location, alertService, $rootScope, authenticatedUserService, dialogService, toastService, $mdDialog, $interval, $filter, $timeout, $document, storageService) {
-        var isAutoSaving = false;
-        var autoSaveInterval;
+    .controller('editPortfolioController',
+        [
+            '$scope', 'translationService', 'serverCallService', '$route', '$location', 'alertService', '$rootScope', 'authenticatedUserService', 'dialogService', 'toastService', '$mdDialog', '$interval', '$filter', '$timeout', '$document', 'storageService',
+            function ($scope, translationService, serverCallService, $route, $location, alertService, $rootScope, authenticatedUserService, dialogService, toastService, $mdDialog, $interval, $filter, $timeout, $document, storageService) {
+                var isAutoSaving = false;
+                var autoSaveInterval;
 
-        function init() {
-            if (storageService.getPortfolio()) {
-                if (checkAuthorized(storageService.getPortfolio())) {
-                    setPortfolio(storageService.getPortfolio());
-                    checkPortfolioVisibility(storageService.getPortfolio());
-                }
-            } else {
-                getPortfolio(getPortfolioSuccess, getPortfolioFail);
-            }
-
-            if ($rootScope.newPortfolioCreated) {
-                $scope.showFirstMessage = true;
-                $rootScope.newPortfolioCreated = false;
-            }
-
-            $rootScope.savedChapter = null;
-            startAutosave();
-        }
-
-        function checkPortfolioVisibility(portfolio) {
-            if (portfolio.visibility === 'PRIVATE') return;
-
-            showWarning();
-        }
-
-        function getPortfolio(success, fail) {
-            var portfolioId = $route.current.params.id;
-            serverCallService.makeGet("rest/portfolio?id=" + portfolioId, {}, success, fail);
-        }
-
-        function getPortfolioFail() {
-            log('No data returned by getting portfolio.');
-            alertService.setErrorAlert('ERROR_PORTFOLIO_NOT_FOUND');
-            $location.url("/");
-        }
-
-        function getPortfolioSuccess(portfolio) {
-            if (isEmpty(portfolio)) {
-                getPortfolioFail();
-            } else if (checkAuthorized(portfolio)) {
-                setPortfolio(portfolio);
-                checkPortfolioVisibility(portfolio);
-            }
-        }
-
-        $scope.toggleSidenav = function (menuId) {
-            $mdSidenav(menuId).toggle();
-        };
-
-        $scope.closeSidenav = function (menuId) {
-            $mdSidenav(menuId).close();
-        };
-
-        $scope.onDeleteChapter = function (chapter) {
-            var deleteChapter = function () {
-                $scope.portfolio.chapters.splice($scope.portfolio.chapters.indexOf(chapter), 1);
-            };
-
-            dialogService.showDeleteConfirmationDialog(
-                'PORTFOLIO_DELETE_CHAPTER_CONFIRM_TITLE',
-                'PORTFOLIO_DELETE_CHAPTER_CONFIRM_MESSAGE',
-                deleteChapter);
-            };
-
-            function updatePortfolio() {
-                var url = "rest/portfolio/update";
-                serverCallService.makePost(url, $scope.portfolio, updatePortfolioSuccess, updatePortfolioFailed);
-            }
-
-            function updatePortfolioSuccess(portfolio) {
-                if (isEmpty(portfolio)) {
-                    createPortfolioFailed();
-                } else {
-                    if (!isAutoSaving) setPortfolio(portfolio);
-
-                    var message = isAutoSaving ? 'PORTFOLIO_AUTOSAVED' : 'PORTFOLIO_SAVED';
-                    toastService.show(message);
-                }
-            }
-
-            function updatePortfolioFailed() {
-                log('Updating portfolio failed.');
-            }
-
-            function setPortfolio(portfolio) {
-                $scope.portfolio = portfolio;
-
-                if ($scope.portfolio.chapters) {
-                    $scope.portfolio.chapters.forEach(function (chapter) {
-                        if(chapter.contentRows) {
-                            chapter.contentRows.forEach(function (contentRow) {
-                                contentRow.learningObjects.forEach(function(learningObject){
-                                    learningObject.source = getSource(learningObject);
-                                })
-                            })
+                function init() {
+                    if (storageService.getPortfolio()) {
+                        if (checkAuthorized(storageService.getPortfolio())) {
+                            setPortfolio(storageService.getPortfolio());
                         }
-                    });
+                    } else {
+                        getPortfolio(getPortfolioSuccess, getPortfolioFail);
+                    }
+
+                    if ($rootScope.newPortfolioCreated) {
+                        $scope.showFirstMessage = true;
+                        $rootScope.newPortfolioCreated = false;
+                    }
+
+                    $rootScope.savedChapter = null;
+                    startAutosave();
                 }
 
-                storageService.setPortfolio(portfolio);
-                $rootScope.isPlaceholderVisible = false;
-            }
+                function getPortfolio(success, fail) {
+                    var portfolioId = $route.current.params.id;
+                    serverCallService.makeGet("rest/portfolio?id=" + portfolioId, {}, success, fail);
+                }
 
-            function showWarning() {
-                var setPrivate = function () {
-                    storageService.getPortfolio().visibility = 'PRIVATE';
-                    updatePortfolio();
+                function getPortfolioFail() {
+                    log('No data returned by getting portfolio.');
+                    alertService.setErrorAlert('ERROR_PORTFOLIO_NOT_FOUND');
+                    $location.url("/");
+                }
+
+                function getPortfolioSuccess(portfolio) {
+                    if (isEmpty(portfolio)) {
+                        getPortfolioFail();
+                    } else if (checkAuthorized(portfolio)) {
+                        setPortfolio(portfolio);
+                    }
+                }
+
+                $scope.toggleSidenav = function (menuId) {
+                    $mdSidenav(menuId).toggle();
                 };
 
-                dialogService.showConfirmationDialog(
-                    "{{'PORTFOLIO_MAKE_PRIVATE' | translate}}",
-                    "{{'PORTFOLIO_WARNING' | translate}}",
-                    "{{'PORTFOLIO_YES' | translate}}",
-                    "{{'PORTFOLIO_NO' | translate}}",
-                    setPrivate);
+                $scope.closeSidenav = function (menuId) {
+                    $mdSidenav(menuId).close();
+                };
+
+                $scope.onDeleteChapter = function (chapter) {
+                    var deleteChapter = function () {
+                        $scope.portfolio.chapters.splice($scope.portfolio.chapters.indexOf(chapter), 1);
+                    };
+
+                    dialogService.showDeleteConfirmationDialog(
+                        'PORTFOLIO_DELETE_CHAPTER_CONFIRM_TITLE',
+                        'PORTFOLIO_DELETE_CHAPTER_CONFIRM_MESSAGE',
+                        deleteChapter);
+                };
+
+                function updatePortfolio() {
+                    var url = "rest/portfolio/update";
+                    serverCallService.makePost(url, $scope.portfolio, updatePortfolioSuccess, updatePortfolioFailed);
                 }
+
+                function updatePortfolioSuccess(portfolio) {
+                    if (isEmpty(portfolio)) {
+                        createPortfolioFailed();
+                    } else {
+                        if (!isAutoSaving) setPortfolio(portfolio);
+
+                        var message = isAutoSaving ? 'PORTFOLIO_AUTOSAVED' : 'PORTFOLIO_SAVED';
+                        toastService.show(message);
+                    }
+                }
+
+                function updatePortfolioFailed() {
+                    log('Updating portfolio failed.');
+                }
+
+                function setPortfolio(portfolio) {
+                    $scope.portfolio = portfolio;
+
+                    if ($scope.portfolio.chapters) {
+                        $scope.portfolio.chapters.forEach(function (chapter) {
+                            if (chapter.contentRows) {
+                                chapter.contentRows.forEach(function (contentRow) {
+                                    contentRow.learningObjects.forEach(function (learningObject) {
+                                        learningObject.source = getSource(learningObject);
+                                    })
+                                })
+                            }
+                        });
+                    }
+
+                    storageService.setPortfolio(portfolio);
+                    $rootScope.isPlaceholderVisible = false;
+                }
+
 
                 function checkAuthorized(portfolio) {
                     var user = authenticatedUserService.getUser();
