e5d2c9a8a9cbe37fb73018736c18f20300e27373
mare hunt <mare.hunt@netgroup.ee>
2016-12-29 11:34:20 +0200

    #114 adjusting icons layout and image preloader position in portfolio modal

---
+++ back-end/src/main/resources/db/migration/V14.3__Release_migration_1_8_0.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'LOAD_IMAGE', 'Lohista või lae pilt. ');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'LOAD_IMAGE', ' ');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'LOAD_IMAGE', ' ');
+
+SET foreign_key_checks = 1;
