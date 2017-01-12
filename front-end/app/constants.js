e90581485f4fbe6f5ce8ddf9678c7ea4eba38247
Rannar Allorg <rannar.allorg@netgroup.ee>
2017-01-12 13:46:20 +0200

    Add new YOUTUBE_API_KEY

--- front-end/app/constants.js
+++ front-end/app/constants.js
@@ -1,4 +1,4 @@
 angular.module('DOPconstants', [])
     .constant('APP_VERSION', 'DEVELOPER')
     .constant('FB_APP_ID', '225966171178748')
-    .constant('YOUTUBE_API_KEY', 'AIzaSyDiAfuCVBNwz2ApCht3CZrgft-CeO6yI2A');
+    .constant('YOUTUBE_API_KEY', 'AIzaSyDj2NUAQo5prRJOYzjtdmUzhoQcdtytizE');
