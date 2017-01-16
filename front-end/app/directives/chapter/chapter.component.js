2a392c4ceb561b69426dacdef38153ff42aa9b05
Gert Glükmann <gglukmann@gmail.com>
2017-01-16 12:17:54 +0200

    Refactor chapter to component

--- front-end/app/directives/chapter/chapter.component.js
+++ front-end/app/directives/chapter/chapter.component.js
@@ -18,47 +18,29 @@ function dopChapterController ($rootScope, translationService, dialogService) {
 
     vm.$onInit = () => {
         vm.isEditable = $rootScope.isEditPortfolioMode;
-        vm.isCollapsed = false;
+        vm.isCollapsed = vm.chapter.openCloseChapter ? true : false;
         vm.subisCollapsed = [];
 
-        angular.forEach(vm.chapter.subchapters, function (value, key) {
+        angular.forEach(vm.chapter.subchapters, (value, key) => {
             vm.subisCollapsed[value.$$hashKey] = false;
         }, log);
-
-        if (vm.chapter.openCloseChapter == true) {
-            vm.isCollapsed = false;
-        }
     }
 
     // Open/Close Chapter
-    function ocChapter () {
-        vm.isCollapsed = !vm.isCollapsed;
-    };
+    vm.ocChapter = () => vm.isCollapsed = !vm.isCollapsed;
 
     // Open/Close SubChapter
-    function ocSubChapter (subChapter) {
-        vm.subisCollapsed[subChapter.$$hashKey] = !vm.subisCollapsed[subChapter.$$hashKey];
-    };
+    vm.ocSubChapter = (subChapter) => vm.subisCollapsed[subChapter.$$hashKey] = !vm.subisCollapsed[subChapter.$$hashKey];
 
-    function onDeleteSubChapter (subChapter) {
-        var deleteSubChapter = function () {
-            vm.chapter.subchapters.splice(vm.chapter.subchapters.indexOf(subChapter), 1);
-        };
+    vm.onDeleteSubChapter = (subChapter) => {
+        var deleteSubChapter = () => vm.chapter.subchapters.splice(vm.chapter.subchapters.indexOf(subChapter), 1);
 
         dialogService.showDeleteConfirmationDialog(
             'PORTFOLIO_DELETE_SUB_CHAPTER_CONFIRM_TITLE',
             'PORTFOLIO_DELETE_SUB_CHAPTER_CONFIRM_MESSAGE',
             deleteSubChapter
         );
-
     };
 
-    function deleteChapter () {
-        vm.onDelete()(vm.chapter);
-    };
-
-    vm.ocChapter = ocChapter;
-    vm.ocSubChapter = ocSubChapter;
-    vm.onDeleteSubChapter = onDeleteSubChapter;
-    vm.deleteChapter = deleteChapter;
+    vm.deleteChapter = () => vm.onDelete()(vm.chapter);
 };
