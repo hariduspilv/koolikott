dcf934cbd6773a1717bf96ce3546ddbe686be666
Joonas Lume <joonaslume@gmail.com>
2017-01-20 17:37:21 +0200

    Remove shadowed card background from materials that only have links

--- front-end/app/directives/embeddedMaterial/embeddedMaterial.js
+++ front-end/app/directives/embeddedMaterial/embeddedMaterial.js
@@ -113,6 +113,11 @@ angular.module('koolikottApp').directive('dopEmbeddedMaterial', [
                     });
                 };
 
+                $scope.fallbackToLink = function () {
+                    console.log($scope.isEditPortfolioMode || !$scope.sourceType || $scope.sourceType === 'LINK');
+                    return $scope.isEditPortfolioMode || !$scope.sourceType || $scope.sourceType === 'LINK';
+                };
+
                 $scope.$watch(function () {
                     return getSource($scope.material);
                 }, function (newValue, oldValue) {
@@ -226,6 +231,8 @@ angular.module('koolikottApp').directive('dopEmbeddedMaterial', [
                         return getUserDefinedLanguageString(languageStringList, translationService.getLanguage(), materialLanguage);
                     }
                 }
+
+
 
                 function embedCallback(res) {
                     if (res && res.data.html) {
