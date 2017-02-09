f2bfab230bcfcd5956ad8917348cf59f970a39c2
Gert Glükmann <gglukmann@gmail.com>
2017-02-09 15:32:22 +0200

    #54 Add translations to tour

---
+++ back-end/src/main/resources/db/migration/V15.1__Release_migration_1_9_0.sql
@@ -0,0 +1,31 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TOUR_SIDENAV', 'Õppevarades saad otsida vastava taksonoomia all olevaid materjale');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TOUR_SIDENAV', '');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TOUR_SIDENAV', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TOUR_DETAILEDSEARCH', 'Siit saad avada lisaks lihtotsingule detailsema otsingu');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TOUR_DETAILEDSEARCH', '');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TOUR_DETAILEDSEARCH', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TOUR_HELP', 'Küsimärgi alt saad sama tuuri uuesti avada ning anda tagasisidet e-koolikoti kohta ning vastavalt sisse- või väljalogida');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TOUR_HELP', '');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TOUR_HELP', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TOUR_CANCELLED_HELP', 'Küsimärgi alt saad tuuri uuesti avada');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TOUR_CANCELLED_HELP', '');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TOUR_CANCELLED_HELP', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TOUR_EDIT_SIDENAV', 'Siia tekivad kõik Sinu loodud peatükid ning alampeatükid ning neile vajutades viiakse sind kogumiku sees kohe õigesse kohta');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TOUR_EDIT_SIDENAV', '');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TOUR_EDIT_SIDENAV', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TOUR_EDIT_SEARCH', 'Siit saad otsida materjali ning lisada neid hetkel avatud kogumikku');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TOUR_EDIT_SEARCH', '');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TOUR_EDIT_SEARCH', '');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TOUR_EDIT_CHANGE_VISIBLITY', 'Siit saad muuta kogumiku staatust ning kogumikku salvestada');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TOUR_EDIT_CHANGE_VISIBLITY', '');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TOUR_EDIT_CHANGE_VISIBLITY', '');
+
+SET foreign_key_checks = 1;
