9216f9e8fcb93ccbb3cce10912400f7b7c1b1ef5
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-09-15 12:58:09 +0300

    fix compilation error

--- back-end/src/main/java/ee/hm/dop/config/guice/module/ProviderModule.java
+++ back-end/src/main/java/ee/hm/dop/config/guice/module/ProviderModule.java
@@ -1,24 +1,18 @@
 package ee.hm.dop.config.guice.module;
 
+import com.google.inject.AbstractModule;
+import ee.hm.dop.config.db.DatabaseMigrator;
+import ee.hm.dop.config.db.FlywayDbMigrator;
+import ee.hm.dop.config.guice.GuiceInjector.Module;
+import ee.hm.dop.config.guice.provider.*;
+import ee.hm.dop.service.solr.SolrEngineService;
+import org.apache.commons.configuration.Configuration;
+import org.opensaml.xml.signature.SignatureValidator;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.client.Client;
 import javax.xml.soap.SOAPConnection;
-
-import com.google.inject.AbstractModule;
-import ee.hm.dop.db.DatabaseMigrator;
-import ee.hm.dop.db.FlywayDbMigrator;
-import ee.hm.dop.config.guice.GuiceInjector.Module;
-import ee.hm.dop.config.guice.provider.ConfigurationProvider;
-import ee.hm.dop.config.guice.provider.EntityManagerFactoryProvider;
-import ee.hm.dop.config.guice.provider.EntityManagerProvider;
-import ee.hm.dop.config.guice.provider.HttpClientProvider;
-import ee.hm.dop.config.guice.provider.SOAPConnectionProvider;
-import ee.hm.dop.config.guice.provider.SearchEngineServiceProvider;
-import ee.hm.dop.config.guice.provider.SignatureValidatorProvider;
-import ee.hm.dop.service.solr.SolrEngineService;
-import org.apache.commons.configuration.Configuration;
-import org.opensaml.xml.signature.SignatureValidator;
 
 @Module
 public class ProviderModule extends AbstractModule {
