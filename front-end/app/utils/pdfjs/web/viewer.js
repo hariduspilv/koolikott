4749a96f974d705bf48e47bcca88ea657036f32d
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-09-22 13:24:24 +0300

    #182 Disable pdf.js origin check
    (cherry picked from commit a459476)

--- front-end/app/utils/pdfjs/web/viewer.js
+++ front-end/app/utils/pdfjs/web/viewer.js
@@ -6546,10 +6546,10 @@ var pdfjsWebLibs;
      if (HOSTED_VIEWER_ORIGINS.indexOf(viewerOrigin) >= 0) {
       return;
      }
-     var fileOrigin = new URL(file, window.location.href).origin;
-     if (fileOrigin !== viewerOrigin) {
-      throw new Error('file origin does not match viewer\'s');
-     }
+     // var fileOrigin = new URL(file, window.location.href).origin;
+     // if (fileOrigin !== viewerOrigin) {
+     //  throw new Error('file origin does not match viewer\'s');
+     // }
     } catch (e) {
      var message = e && e.message;
      var loadingErrorMessage = mozL10n.get('loading_error', null, 'An error occurred while loading the PDF.');
