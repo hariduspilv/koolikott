d18b46b1b2dce9b3233c6b1ef8b262416793846f
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-09-26 18:49:15 +0300

    #182 External pdf embeddeding fix
    (cherry picked from commit 25eca17)

--- back-end/src/main/java/ee/hm/dop/service/content/MaterialProxy.java
+++ back-end/src/main/java/ee/hm/dop/service/content/MaterialProxy.java
@@ -33,7 +33,7 @@ public class MaterialProxy {
             client.executeMethod(get);
         } catch (UnknownHostException | IllegalArgumentException e) {
             logger.info("Could not contact host, returning empty response: " + e.getMessage());
-            return Response.noContent().build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 
         String attachmentLocation = attachmentLocation(get, PDF_EXTENSION, PDF_MIME_TYPE);
@@ -47,7 +47,7 @@ public class MaterialProxy {
             String contentDisposition = format("Inline; filename=\"%s\"", fileName);
             return buildContentTypeResponse(get.getResponseBody(), contentDisposition);
         }
-        return Response.noContent().build();
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
     private Response buildContentTypeResponse(byte[] responseBody, String contentDisposition) throws IOException {
