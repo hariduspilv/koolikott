a85d37cc419b5da973d42fa6889602899b83571b
ips <ips@divedivedive.org>
2017-09-28 11:44:21 +0300

    #186 bugfixes: dashboard users listing

---
+++ back-end/src/main/resources/db/migration/V16.5__Release_migration_1_10_0.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'ERROR_USERS_NOT_FOUND', 'Ei leidnud kasutajat');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'ERROR_USERS_NOT_FOUND', 'No users found');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'ERROR_USERS_NOT_FOUND', '');
+
+SET foreign_key_checks = 1;
