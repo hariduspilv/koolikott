c85ec1b68c49879a3aab247e064157fd86f318ae
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-09-29 13:01:47 +0300

    #188 BrokenContent fixes

--- back-end/src/main/java/ee/hm/dop/dao/BrokenContentDao.java
+++ back-end/src/main/java/ee/hm/dop/dao/BrokenContentDao.java
@@ -1,36 +1,34 @@
 package ee.hm.dop.dao;
 
-import java.math.BigInteger;
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-
 import ee.hm.dop.model.BrokenContent;
 import ee.hm.dop.model.User;
 import org.joda.time.DateTime;
 
+import javax.persistence.Query;
+import java.math.BigInteger;
+import java.util.List;
+
 public class BrokenContentDao extends AbstractDao<BrokenContent> {
 
-    public BrokenContent update(BrokenContent brokenontent) {
-        if (brokenontent.getId() == null) {
-            brokenontent.setAdded(DateTime.now());
+    public BrokenContent update(BrokenContent brokenContent) {
+        if (brokenContent.getId() == null) {
+            brokenContent.setAdded(DateTime.now());
         }
-        return createOrUpdate(brokenontent);
+        return createOrUpdate(brokenContent);
     }
 
-    public void deleteBrokenMaterials(Long id) {
-        Query query = entityManager.createQuery("UPDATE BrokenContent b set b.deleted = true WHERE b.material.id = :id");
-        query.setParameter("id", id);
+    public void deleteBrokenMaterials(Long materialId) {
+        Query query = getEntityManager().createQuery("UPDATE BrokenContent b set b.deleted = true WHERE b.material.id = :id");
+        query.setParameter("id", materialId);
         query.executeUpdate();
     }
 
     public List<BrokenContent> getBrokenMaterials() {
-        return entityManager.createQuery("FROM BrokenContent b " +
-                "WHERE b.material IS NOT NULL AND b.deleted = false", entity())
+        return getEntityManager().createQuery("" +
+                "SELECT b FROM BrokenContent b " +
+                "INNER JOIN b.material m " +
+                "WHERE m.deleted = false " +
+                "AND b.deleted = false", entity())
                 .getResultList();
     }
 
@@ -38,38 +36,30 @@ public class BrokenContentDao extends AbstractDao<BrokenContent> {
         String queryString = "SELECT Count(b.id) FROM BrokenContent b " +
                 "INNER JOIN LearningObject lo ON b.material=lo.id " +
                 "WHERE lo.deleted = FALSE AND b.deleted = FALSE";
-        Query query = entityManager.createNativeQuery(queryString);
+        Query query = getEntityManager().createNativeQuery(queryString);
         return ((BigInteger) query.getSingleResult()).longValue();
     }
 
     public List<BrokenContent> findByMaterialAndUser(long materialId, User loggedInUser) {
-        TypedQuery<BrokenContent> findByData = entityManager.createQuery(
-                "FROM BrokenContent b WHERE b.material.id = :materialid AND b.creator = :user AND b.deleted = false",
-                entity());
+        return getEntityManager()
+                .createQuery("FROM BrokenContent b " +
+                        "WHERE b.material.id = :materialid " +
+                        "AND b.creator = :user " +
+                        "AND b.deleted = false", entity())
+                .setParameter("materialid", materialId)
+                .setParameter("user", loggedInUser)
+                .getResultList();
 
-        List<BrokenContent> brokenContents = null;
-        try {
-            brokenContents = findByData.setParameter("materialid", materialId).setParameter("user", loggedInUser)
-                    .getResultList();
-        } catch (NoResultException ex) {
-            // ignore
-        }
-
-        return brokenContents;
     }
 
     public List<BrokenContent> findByMaterial(long materialId) {
-        TypedQuery<BrokenContent> findByMaterial = entityManager.createQuery(
-                "FROM BrokenContent b WHERE b.material.id = :materialid AND b.deleted = false", BrokenContent.class);
+        return getEntityManager()
+                .createQuery("FROM BrokenContent b " +
+                        "WHERE b.material.id = :materialid " +
+                        "AND b.deleted = false", entity())
+                .setParameter("materialid", materialId)
+                .getResultList();
 
-        List<BrokenContent> brokenContents = null;
-        try {
-            brokenContents = findByMaterial.setParameter("materialid", materialId).getResultList();
-        } catch (NoResultException ex) {
-            // ignore
-        }
-
-        return brokenContents;
     }
 
 }
