e98fd59be5afbc796f93265abe7f7c93cd03fc7d
egle-riin.ruutel <egle-riin.ruutel@netgroup.ee>
2017-10-02 13:55:38 +0300

    Refactor portfolio 1
    (cherry picked from commit 14d08b9)

--- back-end/src/test/java/ee/hm/dop/rest/ImproperContentResourceTest.java
+++ back-end/src/test/java/ee/hm/dop/rest/ImproperContentResourceTest.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 import ee.hm.dop.common.test.ResourceIntegrationTestBase;
+import ee.hm.dop.common.test.TestConstants;
 import ee.hm.dop.model.ImproperContent;
 import ee.hm.dop.model.LearningObject;
 import ee.hm.dop.model.Portfolio;
@@ -23,7 +24,7 @@ public class ImproperContentResourceTest extends ResourceIntegrationTestBase {
     public static final String IMPROPER_MATERIALS_COUNT = "admin/improper/material/count";
     public static final String IMPROPER_PORTFOLIOS = "admin/improper/portfolio";
     public static final String IMPROPER_PORTFOLIOS_COUNT = "admin/improper/portfolio/count";
-    public static final long TEST_PORFOLIO_ID = 101L;
+    public static final long TEST_PORFOLIO_ID = TestConstants.PORTFOLIO_1;
     public static final long TEST_UNREVIEWED_PORFOLIO_ID = 108L;
     public static final String GET_IMPROPERS_BY_ID = "impropers/%s";
 
