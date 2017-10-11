aacc2cb930c1b319d8255607d95e8a9d3c126b48
egle-riin.ruutel <egle-riin.ruutel@netgroup.ee>
2017-10-11 12:27:11 +0300

    Add additional validation to AtomFeedResource test

--- back-end/src/test/java/ee/hm/dop/rest/AtomFeedResourceTest.java
+++ back-end/src/test/java/ee/hm/dop/rest/AtomFeedResourceTest.java
@@ -2,15 +2,22 @@ package ee.hm.dop.rest;
 
 import ee.hm.dop.common.test.ResourceIntegrationTestBase;
 import ee.hm.dop.model.enums.LanguageC;
-import org.apache.abdera.model.Feed;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
 
 import static java.lang.String.format;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AtomFeedResourceTest extends ResourceIntegrationTestBase {
 
@@ -22,6 +29,10 @@ public class AtomFeedResourceTest extends ResourceIntegrationTestBase {
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
         String result = response.readEntity(String.class);
         assertTrue(!result.isEmpty());
+
+        Document doc = parseFeed(result);
+        assertEquals("e-koolikott:et", doc.getElementsByTagName("id").item(0).getTextContent());
+        assertEquals("feed", doc.getDocumentElement().getTagName());
     }
 
     @Test
@@ -30,6 +41,10 @@ public class AtomFeedResourceTest extends ResourceIntegrationTestBase {
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
         String result = response.readEntity(String.class);
         assertTrue(!result.isEmpty());
+
+        Document doc = parseFeed(result);
+        assertEquals("e-koolikott:en", doc.getElementsByTagName("id").item(0).getTextContent());
+        assertEquals("feed", doc.getDocumentElement().getTagName());
     }
 
     @Test
@@ -38,9 +53,19 @@ public class AtomFeedResourceTest extends ResourceIntegrationTestBase {
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
         String result = response.readEntity(String.class);
         assertTrue(!result.isEmpty());
+
+        Document doc = parseFeed(result);
+        assertEquals("e-koolikott:ru", doc.getElementsByTagName("id").item(0).getTextContent());
+        assertEquals("feed", doc.getDocumentElement().getTagName());
     }
 
     private Response getFeed(String language) {
         return doGet(format(GET_FEED, language), MediaType.APPLICATION_ATOM_XML_TYPE);
+    }
+
+    private Document parseFeed(String result) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        return dBuilder.parse(new InputSource(new StringReader(result)));
     }
 }
