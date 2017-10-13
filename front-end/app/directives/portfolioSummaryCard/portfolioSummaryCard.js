8c722954bae7e8b0de3e5718ffc0e8c3e2506529
ips <ips@divedivedive.org>
2017-10-13 17:08:43 +0300

    #196: make improper banner appear immediately upon reporting it

--- front-end/app/directives/portfolioSummaryCard/portfolioSummaryCard.js
+++ front-end/app/directives/portfolioSummaryCard/portfolioSummaryCard.js
@@ -128,7 +128,7 @@ angular.module('koolikottApp')
 
             $scope.setNotImproper = () => {
                 if (($scope.isAdmin() || $scope.isModerator()) && $scope.portfolio) {
-                    var url = "rest/impropers?learningObject=" + $scope.portfolio.id;
+                    var url = "rest/admin/improper/setProper?learningObject=" + $scope.portfolio.id;
                     serverCallService.makeDelete(url, {}, setNotImproperSuccessful, setNotImproperFailed);
                 }
             };
