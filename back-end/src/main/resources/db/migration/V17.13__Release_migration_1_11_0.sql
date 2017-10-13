627d530aa8ddea2c68723a8f2de6b4a263a37fdf
ips <ips@divedivedive.org>
2017-10-13 12:03:37 +0300

    #202: Raporteerimist kinnitaval modaalil kuvada põhjuse valikuväli ning põhjuse täpsustamiseks tekstiväli

---
+++ back-end/src/main/resources/db/migration/V17.13__Release_migration_1_11_0.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'IMPROPER_REPORT_DESCRIPTION', 'Selgitus');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'IMPROPER_REPORT_DESCRIPTION', 'Explanation');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'IMPROPER_REPORT_DESCRIPTION', '');
+
+SET foreign_key_checks = 1;
