1efdb377b3e45c992d060803056751361803e676
ips <ips@divedivedive.org>
2017-10-30 09:58:35 +0200

    #210: Bugfix: include missing translations

--- back-end/src/main/resources/db/migration/V18.3__Release_migration_1_12_0.sql
+++ back-end/src/main/resources/db/migration/V18.3__Release_migration_1_12_0.sql
@@ -1,5 +1,9 @@
 SET foreign_key_checks = 0;
 
+UPDATE Translation SET translation = 'Muutunud õppevara' WHERE translationKey = 'DASHBOARD_CHANGED_LEARNING_OBJECTS' and translationGroup = 1;
+UPDATE Translation SET translation = 'Changed learning objects' WHERE translationKey = 'DASHBOARD_CHANGED_LEARNING_OBJECTS' and translationGroup = 3;
+UPDATE Translation SET translation = 'Muutunud õppevara' WHERE translationKey = 'DASHBOARD_CHANGED_LEARNING_OBJECTS' and translationGroup = 2;
+
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'TYPE', 'Tüüp');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'TYPE', 'Type');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'TYPE', 'Тип');
@@ -15,5 +19,13 @@ INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'CHANGED_AT', 'Muudetud');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'CHANGED_AT', 'Changed');
 INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'CHANGED_AT', 'Muudetud');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'NUM_CHANGERS', '%d muutjat');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'NUM_CHANGERS', '%d changers');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'NUM_CHANGERS', '%d muutjat');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'NUM_REPORTERS', '%d teatajat');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'NUM_REPORTERS', '%d reporters');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'NUM_REPORTERS', '%d teatajat');
 
 SET foreign_key_checks = 1;
