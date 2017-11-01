b203c0d73c5738c77100c3f7397fa97dd831885a
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-11-01 18:19:13 +0200

    Less noise for tests

--- back-end/src/main/java/ee/hm/dop/rest/errorhandling/GenericExceptionMapper.java
+++ back-end/src/main/java/ee/hm/dop/rest/errorhandling/GenericExceptionMapper.java
@@ -25,16 +25,14 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
     }
 
     private Response getResponse(Throwable error) {
-        Response response;
         if (error instanceof WebApplicationException) {
             WebApplicationException webEx = (WebApplicationException) error;
-            response = webEx.getResponse();
+            return webEx.getResponse();
         } else {
-            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Internal error")
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Internal error")
                     .type("text/plain").build();
         }
-
-        return response;
     }
 
     private void setTransactionRollbackOnly() {
