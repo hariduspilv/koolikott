ffe2eab2a3dd76ca1df153c89937128854fc8142
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-11-07 14:08:44 +0200

    merge scripts

--- back-end/src/main/resources/db/migration/V18.7__Release_migration_1_12_0.sql
+++ back-end/src/main/resources/db/migration/V18.7__Release_migration_1_12_0.sql
@@ -1,7 +1,18 @@
 SET foreign_key_checks = 0;
 
-ALTER TABLE  ImproperContent DROP COLUMN needs_rr;
+ALTER TABLE ImproperContent ADD needs_rr BOOLEAN DEFAULT 0;
 
-DROP TABLE IF EXISTS BrokenContent;
+INSERT INTO ImproperContent (creator, learningObject, createdAt, reviewed, reviewedAt, status, needs_rr)
+  SELECT bc.creator, bc.material, bc.added,
+    CASE WHEN bc.deleted=1 THEN 1 ELSE 0 END,
+    CASE WHEN bc.deleted=1 THEN CURRENT_TIMESTAMP ELSE NULL END,
+    CASE WHEN bc.deleted=1 THEN 'ACCEPTED' ELSE NULL END,
+    1
+  FROM BrokenContent bc;
+
+INSERT INTO ReportingReason (improperContent, reason)
+  SELECT ic.id, 'LO_FORM'
+  FROM ImproperContent ic
+  WHERE ic.needs_rr = 1;
 
 SET foreign_key_checks = 1;
