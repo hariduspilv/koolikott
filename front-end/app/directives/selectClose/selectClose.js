b566a2b21f35152c1e1583310c0c148b6c3189cb
Martin Nõukas <martinez966@gmail.com>
2017-11-08 17:13:10 +0200

    #229 Bugfix for vanus sulgemise nupp

--- front-end/app/directives/selectClose/selectClose.js
+++ front-end/app/directives/selectClose/selectClose.js
@@ -1,21 +1,14 @@
 'use strict'
 
-{
-class controller extends Controller {
-    $onInit() {
-        this.$scope.closeSelect = () =>
+directive('dopSelectClose', ['$compile', ($compile) => ({
+    restrict: 'A',
+    scope: true,
+    link($scope, $element) {
+        $scope.closeSelect = () =>
             angular.element(
                 document.querySelector('.md-select-backdrop')
             ).triggerHandler('click')
-    }
-}
-controller.$inject = ['$scope']
-directive('dopSelectClose', ['$compile', ($compile) => ({
-    restrict: 'A',
-    link($scope, $element) {
         const $closeBtn = $compile('<div data-ng-include="\'directives/selectClose/selectClose.html\'"></div>')($scope)
         $element.find('md-select-menu[multiple]').append($closeBtn)
     },
-    controller
 })])
-}
