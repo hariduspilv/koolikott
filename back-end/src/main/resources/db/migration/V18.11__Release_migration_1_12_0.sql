e7625f2692a21c010e1cffc468e9193daeca8faf
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-11-08 11:31:47 +0200

    fix script

---
+++ back-end/src/main/resources/db/migration/V18.11__Release_migration_1_12_0.sql
@@ -0,0 +1,8 @@
+SET foreign_key_checks = 0;
+
+ALTER TABLE ImproperContent
+  CHANGE COLUMN creator createdBy BIGINT NOT NULL,
+  ADD CONSTRAINT FK_IC_CreatedBy_User_id FOREIGN KEY (createdBy) REFERENCES User (id),
+  ADD CONSTRAINT FK_IC_ReviewedBy_User_id FOREIGN KEY (reviewedBy) REFERENCES User (id);
+
+SET foreign_key_checks = 1;
