4c0b5cbc85fc6bdc45700edae15f64e199a5c114
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-11-08 11:36:41 +0200

    fix script

--- back-end/src/main/resources/db/migration/V18.11__Release_migration_1_12_0.sql
+++ back-end/src/main/resources/db/migration/V18.11__Release_migration_1_12_0.sql
@@ -1,7 +1,7 @@
 SET foreign_key_checks = 0;
 
 ALTER TABLE ImproperContent
-  CHANGE COLUMN creator createdBy BIGINT NOT NULL,
+  CHANGE COLUMN creator createdBy BIGINT NULL,
   ADD CONSTRAINT FK_IC_CreatedBy_User_id FOREIGN KEY (createdBy) REFERENCES User (id),
   ADD CONSTRAINT FK_IC_ReviewedBy_User_id FOREIGN KEY (reviewedBy) REFERENCES User (id);
 
