1d790e29d8f7376b12231e6ac0ecf7b5e123658c
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-11-09 13:37:27 +0200

    #220 when change is reverted then tag is removed

--- back-end/src/main/java/ee/hm/dop/service/reviewmanagement/ReviewableChangeAdminService.java
+++ back-end/src/main/java/ee/hm/dop/service/reviewmanagement/ReviewableChangeAdminService.java
@@ -1,17 +1,23 @@
 package ee.hm.dop.service.reviewmanagement;
 
+import com.google.common.collect.Lists;
 import ee.hm.dop.dao.LearningObjectDao;
 import ee.hm.dop.dao.ReviewableChangeDao;
+import ee.hm.dop.dao.TranslationDAO;
 import ee.hm.dop.model.*;
 import ee.hm.dop.model.enums.ReviewStatus;
 import ee.hm.dop.model.enums.ReviewType;
 import ee.hm.dop.service.content.LearningObjectService;
 import ee.hm.dop.utils.UserUtil;
 import ee.hm.dop.utils.ValidatorUtil;
+import org.apache.commons.collections.CollectionUtils;
 import org.joda.time.DateTime;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static ee.hm.dop.service.metadata.TaxonService.TAXON_PREFIXES;
 
 public class ReviewableChangeAdminService {
 
@@ -23,6 +29,8 @@ public class ReviewableChangeAdminService {
     private LearningObjectService learningObjectService;
     @Inject
     private LearningObjectDao learningObjectDao;
+    @Inject
+    private TranslationDAO translationDAO;
 
     public List<AdminLearningObject> getUnReviewed(User user) {
         UserUtil.mustBeModeratorOrAdmin(user);
@@ -111,6 +119,31 @@ public class ReviewableChangeAdminService {
                     ((Material) learningObject).setSource(change.getMaterialSource());
                 }
             }
+
+            if (change.getMaterialSource() == null){
+                List<String> translationKeys = getTranslationKeys(learningObject, change);
+                if (CollectionUtils.isNotEmpty(translationKeys)){
+                    removeTagsByTranslation(learningObject, translationKeys);
+                }
+            }
         }
+    }
+
+    public List<String> getTranslationKeys(LearningObject learningObject, ReviewableChange change) {
+        if (change.getTaxon() != null) {
+            return TAXON_PREFIXES.stream().map(p -> p + change.getTaxon().getName()).collect(Collectors.toList());
+        } else if (change.getResourceType() != null) {
+            if (learningObject instanceof Material) {
+                return Lists.newArrayList(change.getResourceType().getName());
+            }
+        } else if (change.getTargetGroup() != null) {
+            return Lists.newArrayList(change.getTargetGroup().getName());
+        }
+        return null;
+    }
+
+    public void removeTagsByTranslation(LearningObject learningObject, List<String> translationKey) {
+        List<String> translations = translationDAO.getTranslationsForKey(translationKey);
+        learningObject.getTags().removeIf(t -> translations.contains(t.getName().toUpperCase()));
     }
 }
