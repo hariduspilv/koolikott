617d5845efc55701f66bf05d7aca8b187a3f0d9f
Martin Nõukas <martinez966@gmail.com>
2017-11-13 11:33:01 +0200

    Added a new word for links, started changing add material look

---
+++ back-end/src/main/resources/db/migration/V18.12__Release_migration_1_12_0.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'ADD_MATERIAL_LINK', 'Palun lisa link');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'ADD_MATERIAL_LINK', 'Please add a link');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'ADD_MATERIAL_LINK', 'Palun lisa link');
+
+SET foreign_key_checks = 1;
