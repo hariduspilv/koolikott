5bc31f012dba36aca425ee011c2e87841cff5d21
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-11-21 15:44:17 +0200

    Reduce logging

--- front-end/app/directives/embeddedMaterial/embeddedMaterial.js
+++ front-end/app/directives/embeddedMaterial/embeddedMaterial.js
@@ -203,8 +203,6 @@ class controller extends Controller {
                     this.$scope.sourceType = 'PDF'
                     this.$scope.fallbackType = 'LINK'
 
-                    console.log('proxy tree')
-
                     if (this.$scope.isProxySource) {
                         console.log('material source: ' +this.$scope.material.source)
 
@@ -237,7 +235,6 @@ class controller extends Controller {
     }
     probeContentSuccess({ headers }) {
         console.log('Content probing succeeded!')
-        console.log('Content probing succeeded! twice is the charm')
 
         const { 'content-disposition': contentDisposition } = headers()
 
@@ -251,23 +248,17 @@ class controller extends Controller {
         const filename = contentDisposition.match(/filename="(.+)"/)[1]
 
         if (this.getEmbedType({ source: filename }) === 'PDF') {
-            console.log('it is pdf baby')
-
             this.$scope.material.PDFLink = this.pdfjsLink(encodeURIComponent(this.proxyUrl))
-
             console.log('proxy pdf link' + this.$scope.material.PDFLink)
-
             const pdfContainer = document.querySelector('.embed-pdf-' + this.$scope.material.id)
 
             if (pdfContainer) {
                 console.log("proxy pdf element setup")
-
                 pdfContainer.innerHTML(this.iFrameLink(this.$scope.material.PDFLink))
             } else
                 this.$timeout(() => this.sourceTypeAndPdfSetup(true), 100)
         } else {
-            console.log('everything is very sad')
-
+            console.log('proxy is not possible')
             this.$scope.sourceType = this.$scope.fallbackType
         }
     }
