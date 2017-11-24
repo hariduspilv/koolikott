224a8a466cb6380146eba70c6e4d6fe52a9ec4a8
Martin Nõukas <martinez966@gmail.com>
2017-11-24 10:55:35 +0200

    Added missing sql words

---
+++ back-end/src/main/resources/db/migration/V19.3__Release_migration_1_13_0.sql
@@ -0,0 +1,15 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'SIZE_UNTIL', 'Suurus kuni');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'SIZE_UNTIL', 'Size until');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'SIZE_UNTIL', 'Suurus kuni');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'RECESSION_LINK', 'Retsensiooni link');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'RECESSION_LINK', 'Recession link');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'RECESSION_LINK', 'Retsensiooni link');
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'MATERIAL_TYPE', 'Materjali tüüp');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'MATERIAL_TYPE', 'Material type');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'MATERIAL_TYPE', 'Materjali tüüp');
+
+SET foreign_key_checks = 1;
