92a31953df839dace8b2d1553474b8f008276a14
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-12-11 18:17:52 +0200

    #242 Migration script to update chapters 3 levels

--- back-end/src/main/resources/db/migration/V20.1__Release_migration_1_14_0.sql
+++ back-end/src/main/resources/db/migration/V20.1__Release_migration_1_14_0.sql
@@ -3,20 +3,4 @@ SET foreign_key_checks = 0;
 ALTER TABLE Chapter
   ADD COLUMN deleted BOOLEAN NULL;
 
-UPDATE Chapter
-SET deleted = 1
-WHERE portfolio IS NULL AND parentChapter IS NULL;
-
-UPDATE Chapter c
-SET c.deleted = 1
-WHERE c.parentChapter IN (
-  SELECT parent.id
-  FROM (
-         SELECT *
-         FROM Chapter parent
-         WHERE parent.portfolio IS NULL AND parent.parentChapter IS NULL
-       ) parent
-);
-
-
 SET foreign_key_checks = 1;
