bd355532521e2e19b652713615b6b1461109e726
Ivar Oja <ips@ips.re>
2017-12-20 16:51:52 +0200

    #249: Bugfix: prevent displaying comma in publishers and authors list if there are empty names

--- front-end/app/directives/embedFooter/embedFooter.component.js
+++ front-end/app/directives/embedFooter/embedFooter.component.js
@@ -24,7 +24,7 @@ class controller extends Controller {
         this.$scope.icon = this.iconService.getMaterialIcon(resourceTypes)
         this.$scope.link = '/material?id=' + id
         this.$scope.title = this.getUserDefinedLanguageString(titles, this.currentLanguage, language)
-        this.$scope.publishersAndAuthors = publishers.map(p => p.name).concat(authors.map(a => a.name+' '+a.surname)).join(', ')
+        this.$scope.publishersAndAuthors = publishers.filter(p => p.name).map(p => p.name).concat(authors.filter(a => a.name || a.surname).map(a => a.name ? a.name+(a.surname ? ' '+a.surname : '') : a.surname)).join(', ')
         this.setMaterialSourceLink()
 
         const { name: licenseTypeName } = licenseType || {}
