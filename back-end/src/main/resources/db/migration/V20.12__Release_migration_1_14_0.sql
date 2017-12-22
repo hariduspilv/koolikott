db86465b886d56c6752efdba8c04fe51c0bb2b83
Ivar Oja <ips@ips.re>
2017-12-22 15:49:06 +0200

    #298: Bugfix: fix translation key LICENSETYPE_LONG_NAME_YOUTBE -> LICENSETYPE_LONG_NAME_YOUTUBE

---
+++ back-end/src/main/resources/db/migration/V20.12__Release_migration_1_14_0.sql
@@ -0,0 +1,5 @@
+SET foreign_key_checks = 0;
+
+UPDATE Translation SET translationKey = 'LICENSETYPE_LONG_NAME_YOUTUBE' WHERE translationKey = 'LICENSETYPE_LONG_NAME_YOUTBE';
+
+SET foreign_key_checks = 1;
