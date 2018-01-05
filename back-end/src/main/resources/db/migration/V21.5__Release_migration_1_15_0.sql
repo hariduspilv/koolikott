38538cbbfb8d2c8705029f9b6740d665b7167fbd
Gert Glükmann <gglukmann@gmail.com>
2018-01-05 13:40:29 +0200

    #284 add translation

---
+++ back-end/src/main/resources/db/migration/V21.5__Release_migration_1_15_0.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (1, 'MATERIAL_DELETED', 'See materjal on kustutatud');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (3, 'MATERIAL_DELETED', 'See materjal on kustutatud');
+INSERT INTO Translation(translationGroup, translationKey, translation) VALUES (2, 'MATERIAL_DELETED', 'See materjal on kustutatud');
+
+SET foreign_key_checks = 1;
