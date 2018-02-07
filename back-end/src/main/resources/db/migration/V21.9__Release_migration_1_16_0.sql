87ad15377d328293923a8f34fa6566e82cd50ad7
Ivar Oja <ips@ips.re>
2018-02-07 14:19:41 +0200

    #228: Add missing translations

---
+++ back-end/src/main/resources/db/migration/V21.9__Release_migration_1_16_0.sql
@@ -0,0 +1,11 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'CLEAR_FORM', 'Tühjenda');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'CLEAR_FORM', 'Clear');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'CLEAR_FORM', 'Tühjenda');
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'DO_FILTER', 'Filtreeri');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'DO_FILTER', 'Filter');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'DO_FILTER', 'Filtreeri');
+
+SET foreign_key_checks = 1;
