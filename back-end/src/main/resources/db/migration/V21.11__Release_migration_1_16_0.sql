5d111d6abac1f1fea8b9b1b7599b68ceed785f40
Ivar Oja <ips@ips.re>
2018-02-15 15:54:19 +0200

    #228: change DELETED translations

--- back-end/src/main/resources/db/migration/V21.11__Release_migration_1_16_0.sql
+++ back-end/src/main/resources/db/migration/V21.11__Release_migration_1_16_0.sql
@@ -12,4 +12,12 @@ INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'USER_DID_ADD', 'Added');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'USER_DID_ADD', 'Lisas');
 
+UPDATE Translation SET translation = 'Kustutatud' WHERE translationKey = 'DELETED' and translationGroup = 1;
+UPDATE Translation SET translation = 'Deleted' WHERE translationKey = 'DELETED' and translationGroup = 3;
+UPDATE Translation SET translation = 'Kustutatud' WHERE translationKey = 'DELETED' and translationGroup = 2;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'DELETED_LEARNING_OBJECTS', 'Kustutatud õppevara');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'DELETED_LEARNING_OBJECTS', 'Deleted learning objects');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'DELETED_LEARNING_OBJECTS', 'Kustutatud õppevara');
+
 SET foreign_key_checks = 1;
