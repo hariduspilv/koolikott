6b7d143fb2fa01859d9aa0447222eb0179f47865
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2018-05-22 13:06:49 +0300

    #322 new translations and unify login functionality

--- front-end/app/views/dev/login/login.js
+++ front-end/app/views/dev/login/login.js
@@ -3,21 +3,18 @@
 angular.module('koolikottApp')
 .controller('devLoginController',
 [
-    'serverCallService', '$route', 'authenticatedUserService', '$location', 'authenticationService',
-    function(serverCallService, $route, authenticatedUserService, $location, authenticationService) {
+    'serverCallService', '$rootScope', '$route', 'authenticatedUserService', '$location', 'authenticationService',
+    function(serverCallService, $rootScope, $route, authenticatedUserService, $location, authenticationService) {
         const idCode = $route.current.params.idCode;
-        serverCallService.makeGet("rest/dev/login/" + idCode, {}, loginSuccess, loginFail, finishLogin);
+        serverCallService.makeGet("rest/dev/login/" + idCode, {}, loginSuccess, loginFail);
 
         function loginSuccess(userStatus) {
+            $rootScope.afterAuthRedirectURL = "/portfolios";
             authenticationService.loginSuccess(userStatus);
         }
 
         function loginFail() {
             authenticationService.loginFail()
-        }
-
-        function finishLogin() {
-            $location.url('/' + authenticatedUserService.getAuthenticatedUser().user.username);
         }
     }
 ]);
