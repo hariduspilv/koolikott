ca5248789ef3dfc79f08a53f1e314495190bf59e
Jan Viilma <janviilma@hotmail.com>
2018-06-26 15:14:51 +0300

    #331 - update phrase search translations

--- back-end/src/main/resources/db/migration/V25.2__Release_migration_1_20_0.sql
+++ back-end/src/main/resources/db/migration/V25.2__Release_migration_1_20_0.sql
@@ -1,44 +1,44 @@
 SET foreign_key_checks = 0;
 
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_NO_RESULT_EXACT', 'Ühtegi täpset vastet ei leitud.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_NO_RESULT_EXACT', 'No exact results found.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_NO_RESULT_EXACT', 'Ühtegi täpset vastet ei leitud.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_NO_RESULT_EXACT', 'Ühtegi <strong>täpset</strong> vastet ei leitud.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_NO_RESULT_EXACT', 'No <strong>exact</strong> results found.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_NO_RESULT_EXACT', 'Ühtegi <strong>täpset</strong> vastet ei leitud.');
 
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_1_RESULT_PART_1_EXACT', 'Otsingule leidus');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_1_RESULT_PART_1_EXACT', 'Search returned');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_1_RESULT_PART_1_EXACT', 'Otsingule leidus');
 
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_1_RESULT_PART_2_EXACT', 'täpne vaste.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_1_RESULT_PART_2_EXACT', 'exact result.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_1_RESULT_PART_2_EXACT', 'täpne vaste.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_1_RESULT_PART_2_EXACT', '<strong>täpne</strong> vaste.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_1_RESULT_PART_2_EXACT', '<strong>exact</strong> result.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_1_RESULT_PART_2_EXACT', '<strong>täpne</strong> vaste.');
 
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_PART_1_EXACT', 'Otsingule leidus');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_PART_1_EXACT', 'Search returned');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_PART_1_EXACT', 'Otsingule leidus');
 
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_PART_2_EXACT', 'täpset vastet.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_PART_2_EXACT', 'exact results');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_PART_2_EXACT', 'täpset vastet.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_PART_2_EXACT', '<strong>täpset</strong> vastet.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_PART_2_EXACT', '<strong>exact</strong> results');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_PART_2_EXACT', '<strong>täpset</strong> vastet.');
 
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_NO_RESULT_SIMILAR', 'Ühtegi osalist vastet ei leitud');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_NO_RESULT_SIMILAR', 'No partial results found.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_NO_RESULT_SIMILAR', 'Ühtegi osalist vastet ei leitud.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_NO_RESULT_SIMILAR', 'Ühtegi <strong>osalist</strong> vastet ei leitud');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_NO_RESULT_SIMILAR', 'No <strong>partial</strong> results found.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_NO_RESULT_SIMILAR', 'Ühtegi <strong>osalist</strong> vastet ei leitud.');
 
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_1_RESULT_PART_1_SIMILAR', 'Otsingule leidus');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_1_RESULT_PART_1_SIMILAR', 'Search returned');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_1_RESULT_PART_1_SIMILAR', 'Otsingule leidus');
 
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_1_RESULT_PART_2_SIMILAR', 'osaline vaste.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_1_RESULT_PART_2_SIMILAR', 'partial result.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_1_RESULT_PART_2_SIMILAR', 'osaline vaste.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_1_RESULT_PART_2_SIMILAR', '<strong>osaline</strong> vaste.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_1_RESULT_PART_2_SIMILAR', '<strong>partial</strong> result.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_1_RESULT_PART_2_SIMILAR', '<strong>osaline</strong> vaste.');
 
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_PART_1_SIMILAR', 'Otsingule leidus');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_PART_1_SIMILAR', 'Search returned');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_PART_1_SIMILAR', 'Otsingule leidus');
 
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_PART_2_SIMILAR', 'osalist vastet.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_PART_2_SIMILAR', 'partial results.');
-INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_PART_2_SIMILAR', 'osalist vastet.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'SEARCH_RESULT_PART_2_SIMILAR', '<strong>osalist</strong> vastet.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'SEARCH_RESULT_PART_2_SIMILAR', '<strong>partial</strong> results.');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'SEARCH_RESULT_PART_2_SIMILAR', '<strong>osalist</strong> vastet.');
 
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'HEADER_DEFAULT_SEARCH_PLACEHOLDER', 'Otsi pealkirja, kirjelduse, võtmesõna, autori, väljaandja järgi');
 INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'HEADER_DEFAULT_SEARCH_PLACEHOLDER', 'Search by title, description, tag, author, publisher');
