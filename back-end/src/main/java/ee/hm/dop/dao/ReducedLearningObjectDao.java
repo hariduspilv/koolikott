88c4258898cd12f10bc6899b6c56622be8494a91
Jan Viilma <janviilma@hotmail.com>
2018-07-05 10:17:19 +0300

    #348 - change default order of user favourites submenu

--- back-end/src/main/java/ee/hm/dop/dao/ReducedLearningObjectDao.java
+++ back-end/src/main/java/ee/hm/dop/dao/ReducedLearningObjectDao.java
@@ -1,6 +1,7 @@
 package ee.hm.dop.dao;
 
 import ee.hm.dop.model.ReducedLearningObject;
+import ee.hm.dop.model.ReducedMaterial;
 import ee.hm.dop.model.ReducedPortfolio;
 import ee.hm.dop.model.User;
 import org.apache.commons.collections.CollectionUtils;
@@ -14,18 +15,19 @@ public class ReducedLearningObjectDao extends AbstractDao<ReducedLearningObject>
 
     public List<ReducedLearningObject> findMaterialByCreator(User creator, int start, int maxResults) {
         Query query = getEntityManager()
-                .createQuery("select rlo FROM ReducedMaterial rlo" +
-                        " left outer join rlo.userFavorites as uf on uf.creator.id=:creatorId" +
-                        " WHERE rlo.creator.id = :creatorId" +
-                        " AND rlo.deleted = false order by rlo.visibility asc, uf.creator.id desc, rlo.added desc");
+                .createQuery("select rlo FROM ReducedMaterial rlo " +
+                        "left outer join rlo.userFavorites as uf on uf.creator.id = :creatorId " +
+                        "WHERE rlo.creator.id = :creatorId AND rlo.deleted = false " +
+                        "order by rlo.visibility asc, uf.creator.id desc, rlo.added desc", ReducedMaterial.class);
         return findByCreator(query, creator, start, maxResults);
     }
 
     public List<ReducedLearningObject> findPortfolioByCreator(User creator, int start, int maxResults) {
         Query query = getEntityManager()
-                .createQuery("FROM ReducedPortfolio rlo " +
-                        "WHERE rlo.creator.id = :creatorId " +
-                        "AND rlo.deleted = false order by visibility asc, added desc", ReducedPortfolio.class);
+                .createQuery("select rlo FROM ReducedPortfolio rlo " +
+                        "left outer join rlo.userFavorites as uf on uf.creator.id = :creatorId " +
+                        "WHERE rlo.creator.id = :creatorId AND rlo.deleted = false " +
+                        "order by rlo.visibility asc, uf.creator.id desc, rlo.added desc", ReducedPortfolio.class);
         return findByCreator(query, creator, start, maxResults);
     }
 
