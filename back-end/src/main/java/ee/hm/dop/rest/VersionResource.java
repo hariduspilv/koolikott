e96a62953023d32b9eb0594cdcb20ac5596074a7
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2018-07-24 22:41:53 +0300

    1.21.0 version information

--- back-end/src/main/java/ee/hm/dop/rest/VersionResource.java
+++ back-end/src/main/java/ee/hm/dop/rest/VersionResource.java
@@ -11,6 +11,6 @@ public class VersionResource extends BaseResource{
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getVersion() {
-        return "1.20.2";
+        return "1.21.0";
     }
 }
