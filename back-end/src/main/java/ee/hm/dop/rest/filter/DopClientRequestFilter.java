7ff39dbff7809d03d4d15015206f9b5ff943b2b3
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2018-08-22 15:22:05 +0300

    #362 Requests logging

--- back-end/src/main/java/ee/hm/dop/rest/filter/DopClientRequestFilter.java
+++ back-end/src/main/java/ee/hm/dop/rest/filter/DopClientRequestFilter.java
@@ -1,7 +1,5 @@
 package ee.hm.dop.rest.filter;
 
-import ee.hm.dop.utils.ConfigurationProperties;
-import org.apache.commons.configuration2.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,23 +8,15 @@ import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
-import static ee.hm.dop.config.guice.GuiceInjector.getInjector;
-
 @Provider
 public class DopClientRequestFilter implements ClientRequestFilter {
     private static Logger logger = LoggerFactory.getLogger(DopClientRequestFilter.class);
 
     @Override
     public void filter(ClientRequestContext requestContext) throws IOException {
-        if (configuration().getBoolean(ConfigurationProperties.REQUEST_EXTRA_LOGGING)) {
-            logger.info(requestContext.getUri().toString());
-            if (requestContext.getEntity() != null) {
-                logger.info(requestContext.getEntity().toString());
-            }
+        logger.info(requestContext.getUri().toString());
+        if (requestContext.getEntity() != null) {
+            logger.info(requestContext.getEntity().toString());
         }
-    }
-
-    protected Configuration configuration() {
-        return getInjector().getInstance(Configuration.class);
     }
 }
