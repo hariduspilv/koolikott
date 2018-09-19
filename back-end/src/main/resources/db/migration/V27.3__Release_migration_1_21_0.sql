35b039659c20ea0943f9150fd3b7dd07100ef0c5
martin.kokser <martin.kokser@netgroup.ee>
2018-09-19 13:27:20 +0300

    #366 added vocationaleducation specific selection translations to taxonSelector + translations to database

---
+++ back-end/src/main/resources/db/migration/V27.3__Release_migration_1_21_0.sql
@@ -0,0 +1,13 @@
+SET foreign_key_checks = 0;
+
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'VOCATIONAL_EDUCATION_DOMAIN', 'Õppesuund');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'VOCATIONAL_EDUCATION_DOMAIN', 'Field of education');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'VOCATIONAL_EDUCATION_DOMAIN', 'Õppesuund');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'VOCATIONAL_EDUCATION_DOMAIN_REQUIRED', 'Õppesuund on kohustuslik');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'VOCATIONAL_EDUCATION_DOMAIN_REQUIRED', 'Õppesuund on kohustuslik');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'VOCATIONAL_EDUCATION_DOMAIN_REQUIRED', 'Õppesuund on kohustuslik');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (1, 'VOCATIONAL_EDUCATION_TOPIC', 'Õppekavarühm');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (3, 'VOCATIONAL_EDUCATION_TOPIC', 'Detailed field');
+INSERT INTO Translation (translationGroup, translationKey, translation) VALUES (2, 'VOCATIONAL_EDUCATION_TOPIC', 'Õppekavarühm');
+
+SET foreign_key_checks = 1;
