c52e128fa9b4088480926786f262e553627c68ec
martin.kokser <martin.kokser@netgroup.ee>
2018-09-26 14:47:24 +0300

    #357 qrDialog checking window resize and adjusting qr code size accordingly

--- front-end/app/directives/qrDialog/qrDialog.js
+++ front-end/app/directives/qrDialog/qrDialog.js
@@ -5,7 +5,7 @@
         constructor(...args) {
             super(...args)
 
-            debugger
+            this.checkResolution()
 
             const location = this.locals.location.origin + this.locals.location.pathname + this.locals.location.search
             this.$scope.location = (this.locals.slug) ? location + '#' + this.locals.slug : location
@@ -14,24 +14,39 @@
                 this.$mdDialog.hide();
             };
 
-            if (window.innerWidth < 1920)
-                this.$scope.size = 400
-            else
-                this.$scope.size = 800
+            this.$scope.$on('$destroy', () =>
+                this.$interval.cancel(this.resolutionCheckInterval)
+            )
         }
 
+        checkResolution() {
+            this.resolutionCheckInterval = this.$interval(() => {
+                let width = window.innerWidth
+                let qrCode = angular.element(document.querySelector('#qrContent'))
+                this.$scope.size = width >= 1920 ? 700 :
+                    (width < 1920 && width > 1280) ? 650 :
+                        (width < 1280 && width > 960) ? 600 :
+                            (width < 960 && width > 768) ? 500 :
+                                (width < 768) ? 250 : 200
+
+                this.$scope.qrStyle = width >= 1920 ? {'padding': '9px 75px 75px'} :
+                    (width < 1920 && width > 1280) ? {'padding': '6px 70px 70px'} :
+                        (width < 1280 && width > 960) ? {'padding': '0px 64px 64px'} :
+                            (width < 960 && width > 768) ? {'padding': '0px 54px 54px'} :
+                                (width < 768) ? {'padding': '0px 27px 27px'} : {'padding': '0px 27px 27px'}
+
+            }, 20)
+        }
     }
 
     controller.$inject = [
         '$scope',
-        '$translate',
-        'serverCallService',
-        'sortService',
         'dialogService',
         '$window',
         '$mdDialog',
         '$location',
-        'locals'
+        'locals',
+        '$interval',
     ]
     angular.module('koolikottApp').controller('qrDialogController', controller)
 }
