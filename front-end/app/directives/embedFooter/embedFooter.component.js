c0468ebd52614d60165d0ca71724a1303211f953
martin.kokser <martin.kokser@netgroup.ee>
2018-10-03 15:58:52 +0300

    #365 added name to url for embedded material in portfolio

--- front-end/app/directives/embedFooter/embedFooter.component.js
+++ front-end/app/directives/embedFooter/embedFooter.component.js
@@ -22,8 +22,8 @@ class controller extends Controller {
     }
     setMaterialFooterData({ id, publishers, authors, titles, source, uploadedFile, language, resourceTypes, licenseType, deleted }) {
         this.$scope.icon = this.iconService.getMaterialIcon(resourceTypes)
-        this.$scope.link = '/material?id=' + id
         this.$scope.title = this.getUserDefinedLanguageString(titles, this.currentLanguage, language)
+        this.$scope.link = '/material?name=' + this.replaceSpacesAndCharacters(this.$scope.title)+ '&id=' + id
         this.$scope.publishersAndAuthors = publishers.filter(p => p.name).map(p => p.name).concat(authors.filter(a => a.name || a.surname).map(a => a.name ? a.name+(a.surname ? ' '+a.surname : '') : a.surname)).join(', ')
         this.setMaterialSourceLink()
 
