eaea14f814c1fc1385e65abbc3cc18b835b1d779
martin.kokser <martin.kokser@netgroup.ee>
2018-12-13 15:48:08 +0200

    fixed migration script

--- back-end/src/main/resources/db/migration/V35.4__Release_migration_1_27_0.sql
+++ back-end/src/main/resources/db/migration/V35.4__Release_migration_1_27_0.sql
@@ -1,13 +1,13 @@
 SET foreign_key_checks = 0;
 
-update Translation set translation = 'Videojuhendid' where translationKey = 'USER_GUIDE_PAGE' and translationGroup = 1
-update Translation set translation = 'Videojuhendid' where translationKey = 'USER_GUIDE_PAGE' and translationGroup = 2
-update Translation set translation = 'Video guides' where translationKey = 'USER_GUIDE_PAGE' and translationGroup = 3
-update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUAL_TAB' and translationGroup = 1
-update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUAL_TAB' and translationGroup = 2
-update Translation set translation = 'Vide guides' where translationKey = 'USER_MANUAL_TAB' and translationGroup = 3
-update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUALS_HEADING' and translationGroup = 1
-update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUALS_HEADING' and translationGroup = 2
-update Translation set translation = 'Vide guides' where translationKey = 'USER_MANUALS_HEADING' and translationGroup = 3
+update Translation set translation = 'Videojuhendid' where translationKey = 'USER_GUIDE_PAGE' and translationGroup = 1;
+update Translation set translation = 'Videojuhendid' where translationKey = 'USER_GUIDE_PAGE' and translationGroup = 2;
+update Translation set translation = 'Video guides' where translationKey = 'USER_GUIDE_PAGE' and translationGroup = 3;
+update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUAL_TAB' and translationGroup = 1;
+update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUAL_TAB' and translationGroup = 2;
+update Translation set translation = 'Video guides' where translationKey = 'USER_MANUAL_TAB' and translationGroup = 3;
+update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUALS_HEADING' and translationGroup = 1;
+update Translation set translation = 'Videojuhendid' where translationKey = 'USER_MANUALS_HEADING' and translationGroup = 2;
+update Translation set translation = 'Video guides' where translationKey = 'USER_MANUALS_HEADING' and translationGroup = 3;
 
 SET foreign_key_checks = 1;
