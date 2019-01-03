1bb4f3ec9ad3a2268c6ee7d8af81bb6cf43ebd15
martin.kokser <martin.kokser@netgroup.ee>
2019-01-03 17:47:49 +0200

    EK-195 placeholder text corrected with flyway migration

---
+++ back-end/src/main/resources/db/migration/V35.10__Release_migration_1_28_0.sql
@@ -0,0 +1,5 @@
+SET foreign_key_checks = 0;
+
+call update_translations('CUSTOMER_SUPPORT_MESSAGE_PLACEHOLDER', 'Lühike probleemi kirjeldus (maksimaalselt 500 tähemärki)', 'Brief description of your issue (maximum of 500 characters)', 'Lühike probleemi kirjeldus (maksimaalselt 500 tähemärki)');
+
+SET foreign_key_checks = 1;
