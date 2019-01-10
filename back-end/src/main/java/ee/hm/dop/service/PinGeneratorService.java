8f4b311995c4ac813b025619f8e1ed602df325df
marekr <marek.reinhold@netgroup.ee>
2019-01-10 14:47:09 +0200

    EK-138. Modified pin generation functionality.

--- back-end/src/main/java/ee/hm/dop/service/PinGeneratorService.java
+++ back-end/src/main/java/ee/hm/dop/service/PinGeneratorService.java
@@ -1,19 +1,16 @@
 package ee.hm.dop.service;
 
-import org.apache.commons.lang3.RandomStringUtils;
+import java.security.SecureRandom;
 
 public class PinGeneratorService {
 
     public static int generatePin() {
 
-        int num;
+        SecureRandom secureRandom = new SecureRandom();
 
-        do {
-            num = Integer.parseInt(RandomStringUtils.randomNumeric(4));
+        String arv = String.format("%04d", secureRandom.nextInt(10000));
 
-        } while (String.valueOf(num).length() != 4);
-
-        return num;
+        return Integer.parseInt(arv);
 
     }
 }
