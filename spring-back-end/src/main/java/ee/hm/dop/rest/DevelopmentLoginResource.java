8cbcbd9af1abb143769eecff9c44daed92f74256
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-01-18 23:35:04 +0200

    EK-97 add default conf for devlogin

--- spring-back-end/src/main/java/ee/hm/dop/rest/DevelopmentLoginResource.java
+++ spring-back-end/src/main/java/ee/hm/dop/rest/DevelopmentLoginResource.java
@@ -10,17 +10,29 @@ import javax.ws.rs.core.MediaType;
 import ee.hm.dop.model.enums.LoginFrom;
 import ee.hm.dop.service.login.LoginService;
 import ee.hm.dop.service.login.dto.UserStatus;
+import org.apache.commons.configuration2.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import static java.lang.String.format;
 
 @Path("dev/")
+@Profile("test")
 public class DevelopmentLoginResource {
 
     @Inject
     private LoginService loginService;
+    @Inject
+    private Configuration configuration;
 
     @GET
     @Path("/login/{idCode}")
     @Produces(MediaType.APPLICATION_JSON)
     public UserStatus logIn(@PathParam("idCode") String idCode) {
-        return loginService.login(idCode, null, null, LoginFrom.DEV);
+        if (configuration.getBoolean("app.devLogin")) {
+            return loginService.login(idCode, null, null, LoginFrom.DEV);
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
