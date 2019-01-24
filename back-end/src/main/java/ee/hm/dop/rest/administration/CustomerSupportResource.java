4293316e6ddb39d586e8a9057055e9d259f4f990
Marek Reinhold <marek.reinhold@outlook.com>
2019-01-24 12:38:22 +0200

    EK-245 work in progress

--- back-end/src/main/java/ee/hm/dop/rest/administration/CustomerSupportResource.java
+++ back-end/src/main/java/ee/hm/dop/rest/administration/CustomerSupportResource.java
@@ -1,5 +1,6 @@
 package ee.hm.dop.rest.administration;
 
+import com.google.common.io.Files;
 import ee.hm.dop.model.CustomerSupport;
 import ee.hm.dop.model.enums.RoleString;
 import ee.hm.dop.rest.BaseResource;
@@ -12,6 +13,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Path("admin/customerSupport")
 public class CustomerSupportResource extends BaseResource {
@@ -24,6 +29,37 @@ public class CustomerSupportResource extends BaseResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @RolesAllowed({RoleString.ADMIN, RoleString.MODERATOR})
     public CustomerSupport saveCustomerSupportRequest(CustomerSupport customerSupport) {
-        return customerSupportService.save(customerSupport,getLoggedInUser());
-    }
+
+        final String IMAGE_PATTERN = "([^\\s]+(\\.(?i)(jpg|png|jpeg|gif|bmp|JPG|PNG|GIF|JPEG))$)";
+
+        boolean dataIsValidated = false;
+
+        List<String> imageFiles =customerSupport.getFiles().stream().map(f -> f.getName()).collect(Collectors.toList());
+
+        List<String> extensions = imageFiles.stream().map(f -> Files.getFileExtension(f)).collect(Collectors.toList());
+
+        for(String ext : extensions){
+
+            Pattern pattern = Pattern.compile(IMAGE_PATTERN);
+            Matcher matcher = pattern.matcher(ext);
+
+//            dataIsValidated = matcher.matches() ? true : false;
+
+            if (matcher.matches()){
+                dataIsValidated = true;
+
+            }
+            else{
+                dataIsValidated = false;
+                break;
+            }
+        }
+        if (dataIsValidated) {
+
+            return customerSupportService.save(customerSupport, getLoggedInUser());
+
+        }
+        else {
+            return null;
+        }    }
 }
--- spring-back-end/src/main/java/ee/hm/dop/rest/administration/CustomerSupportResource.java
+++ spring-back-end/src/main/java/ee/hm/dop/rest/administration/CustomerSupportResource.java
@@ -1,5 +1,7 @@
 package ee.hm.dop.rest.administration;
 
+import com.google.common.io.Files;
+import ee.hm.dop.model.AttachedFile;
 import ee.hm.dop.model.CustomerSupport;
 import ee.hm.dop.model.enums.RoleString;
 import ee.hm.dop.rest.BaseResource;
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("admin/customerSupport")
@@ -22,6 +29,38 @@ public class CustomerSupportResource extends BaseResource {
     @PostMapping
     @Secured({RoleString.ADMIN, RoleString.MODERATOR})
     public CustomerSupport saveCustomerSupportRequest(@RequestBody CustomerSupport customerSupport) {
-        return customerSupportService.save(customerSupport, getLoggedInUser());
+
+        final String IMAGE_PATTERN = "([^\\s]+(\\.(?i)(jpg|png|jpeg|gif|bmp))$)";
+
+        boolean dataIsValidated = false;
+
+        List<String> imageFiles =customerSupport.getFiles().stream().map(f -> f.getName()).collect(Collectors.toList());
+
+        List<String> extensions = imageFiles.stream().map(f -> Files.getFileExtension(f)).collect(Collectors.toList());
+
+        for(String ext : extensions){
+
+            Pattern pattern = Pattern.compile(IMAGE_PATTERN);
+            Matcher matcher = pattern.matcher(ext);
+
+//            dataIsValidated = matcher.matches() ? true : false;
+
+            if (matcher.matches()){
+                dataIsValidated = true;
+
+            }
+            else{
+                dataIsValidated = false;
+                break;
+            }
+        }
+        if (dataIsValidated) {
+
+            return customerSupportService.save(customerSupport, getLoggedInUser());
+
+        }
+        else {
+            return null;
+        }
     }
 }
