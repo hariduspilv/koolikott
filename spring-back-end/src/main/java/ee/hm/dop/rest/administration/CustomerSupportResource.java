0cf573574500545c10b8ae7d0ccf8f629f4d9129
Marek Reinhold <marek.reinhold@outlook.com>
2019-01-28 15:12:08 +0200

    EK-247 spring boot commit included

--- spring-back-end/src/main/java/ee/hm/dop/rest/administration/CustomerSupportResource.java
+++ spring-back-end/src/main/java/ee/hm/dop/rest/administration/CustomerSupportResource.java
@@ -6,61 +6,47 @@ import ee.hm.dop.model.CustomerSupport;
 import ee.hm.dop.model.enums.RoleString;
 import ee.hm.dop.rest.BaseResource;
 import ee.hm.dop.service.useractions.CustomerSupportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 
 @RestController
 @RequestMapping("admin/customerSupport")
 public class CustomerSupportResource extends BaseResource {
 
-    @Inject
+    public static final List<String> PICTURE_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png", "bmp", "gif");
+
+    @Autowired
     private CustomerSupportService customerSupportService;
 
-    @PostMapping
+    @PostMapping(produces = MediaType.APPLICATION_JSON,consumes = MediaType.APPLICATION_JSON)
     @Secured({RoleString.ADMIN, RoleString.MODERATOR})
-    public CustomerSupport saveCustomerSupportRequest(@RequestBody CustomerSupport customerSupport) {
+    public CustomerSupport saveCustomerSupportRequest(CustomerSupport customerSupport) {
 
-        final String IMAGE_PATTERN = "([^\\s]+(\\.(?i)(jpg|png|jpeg|gif|bmp))$)";
-
-        boolean dataIsValidated = false;
-
-        List<String> imageFiles =customerSupport.getFiles().stream().map(f -> f.getName()).collect(Collectors.toList());
-
-        List<String> extensions = imageFiles.stream().map(f -> Files.getFileExtension(f)).collect(Collectors.toList());
-
-        for(String ext : extensions){
-
-            Pattern pattern = Pattern.compile(IMAGE_PATTERN);
-            Matcher matcher = pattern.matcher(ext);
-
-//            dataIsValidated = matcher.matches() ? true : false;
-
-            if (matcher.matches()){
-                dataIsValidated = true;
-
-            }
-            else{
-                dataIsValidated = false;
-                break;
-            }
+        if (invalidForm(customerSupport)) {
+            throw new WebApplicationException("contains invalid files", Response.Status.BAD_REQUEST);
         }
-        if (dataIsValidated) {
+        return customerSupportService.save(customerSupport, getLoggedInUser());
+    }
 
-            return customerSupportService.save(customerSupport, getLoggedInUser());
-
+    private boolean invalidForm(CustomerSupport customerSupport) {
+        if (isEmpty(customerSupport.getFiles())){
+            return false;
         }
-        else {
-            return null;
-        }
+        return customerSupport.getFiles().stream()
+                .map(AttachedFile::getName)
+                .map(Files::getFileExtension)
+                .map(String::toLowerCase)
+                .noneMatch(PICTURE_EXTENSIONS::contains);
     }
 }
---
+++ spring-back-end/src/main/resources/db/migration/V36.13__Release_migration_1_29_0.sql
@@ -0,0 +1,7 @@
+SET foreign_key_checks = 0;
+
+call insert_translation('CUSTOMER_FILE_UPLOAD_ERROR', 'Lubatud vaid pildifailid', 'Allowed only image files');
+
+SET foreign_key_checks = 1;
+
+
