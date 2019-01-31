7d0b055ed502232c91c63bcc2edd2190ccc0e5d2
Marek Reinhold <marek.reinhold@outlook.com>
2019-01-31 16:03:29 +0200

    EK-260. code refacto && cleanup

--- front-end/app/directives/cookieNotice/cookieNotice.component.js
+++ front-end/app/directives/cookieNotice/cookieNotice.component.js
@@ -5,11 +5,8 @@
 
         constructor(...args) {
             super(...args)
-
             this.$scope.hasCookie = false
-
             this.hasCookie()
-
         }
 
         hasCookie() {
@@ -18,67 +15,57 @@
             }
         }
 
-        backupFuncToGetUserIp(){
+        backupFuncToGetUserIp() {
+            return new Promise(r => {
+                var w = window,
+                    a = new (w.RTCPeerConnection || w.mozRTCPeerConnection || w.webkitRTCPeerConnection)({iceServers: []}),
+                    b = () => {
+                    };
+                a.createDataChannel('');
+                a.createOffer(c => a.setLocalDescription(c, b, b), b);
+                a.onicecandidate = c => {
+                    try {
+                        c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)
 
-            return new Promise(r=>
-            {
-                var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};
-                    a.createDataChannel('');
-                    a.createOffer(c=>a.setLocalDescription(c,b,b),b);
-                    a.onicecandidate=c=>
-                        {
-                            try
-                                {
-                                    c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)
-
-                                }
-                                catch(e){}
-                        }
+                    } catch (e) {
+                    }
+                }
             })
         }
 
         saveUserCookie() {
-
             this.serverCallService.makeGet('https://ipinfo.io/')
                 .then(response => {
-                        this.setCookie(response.data.ip)
+                    this.setCookie(response.data.ip)
                 })
                 .catch(() => {
                     this.backupFuncToGetUserIp()
                         .then(ip => {
                             this.setCookie(ip)
                         })
-                        .catch(err =>{
+                        .catch(err => {
                             console.log(err)
                         })
                 })
         }
 
         setCookie(ip) {
-            this.$cookies.put('ip',ip)
-            this.$cookies.put('userAgent',this.getUserAgent())
-            this.$cookies.put('time',new Date().toLocaleString())
+            if (!!ip)
+                this.$cookies.put('ip', ip)
+
+            this.$cookies.put('userAgent', this.getUserAgent())
+            this.$cookies.put('time', new Date().toLocaleString())
             this.hasCookie()
         }
 
         getUserAgent() {
             return window.navigator.appVersion
         }
-
-        getLanguage() {
-            let language = this.translationService.getLanguage();
-            return language === 'est' ? 'et' : language === 'rus' ? 'ru' : 'en';
-
-        }
     }
 
     controller.$inject = [
         'serverCallService',
         '$scope',
-        '$window',
-        '$rootScope',
-        'translationService',
-        '$timeout',
         '$cookies'
     ]
     component('dopCookieNotice', {
