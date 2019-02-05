a2263b19451d06552432fccbeefc9f2fbd997729
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-02-05 17:18:06 +0200

    EK-279 update migration

--- back-end/src/main/resources/db/migration/V37.9__Release_migration_1_30_0.sql
+++ back-end/src/main/resources/db/migration/V37.9__Release_migration_1_30_0.sql
@@ -2,6 +2,7 @@ SET foreign_key_checks = 0;
 
 CREATE TABLE TaxonPosition
 (
+  id                 BIGINT AUTO_INCREMENT PRIMARY KEY ,
   taxon              BIGINT NOT NULL,
   educationalContext BIGINT NOT NULL,
   domain             BIGINT NULL,
@@ -12,6 +13,15 @@ CREATE TABLE TaxonPosition
   subtopic           BIGINT NULL
 );
 
-SET foreign_key_checks = 1;
+ALTER TABLE TaxonPosition
+  ADD CONSTRAINT FK_TaxonPosition_taxon FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_educationalContext FOREIGN KEY (educationalContext) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_domain FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_subject FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_module FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_specialization FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_topic FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_subtopic FOREIGN KEY (taxon) REFERENCES Taxon (id);
 
+SET foreign_key_checks = 1;
 
