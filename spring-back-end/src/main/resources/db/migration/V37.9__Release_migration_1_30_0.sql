a2b66dcddbbb0dcf78a429a54f2e470770790aea
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-02-05 17:10:05 +0200

    EK-279 add FK's to migration

--- spring-back-end/src/main/resources/db/migration/V37.9__Release_migration_1_30_0.sql
+++ spring-back-end/src/main/resources/db/migration/V37.9__Release_migration_1_30_0.sql
@@ -2,7 +2,7 @@ SET foreign_key_checks = 0;
 
 CREATE TABLE TaxonPosition
 (
-  id                 BIGINT AUTO_INCREMENT,
+  id                 BIGINT AUTO_INCREMENT PRIMARY KEY ,
   taxon              BIGINT NOT NULL,
   educationalContext BIGINT NOT NULL,
   domain             BIGINT NULL,
@@ -13,6 +13,14 @@ CREATE TABLE TaxonPosition
   subtopic           BIGINT NULL
 );
 
+ALTER TABLE TaxonPosition
+  ADD CONSTRAINT FK_TaxonPosition_taxon FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_educationalContext FOREIGN KEY (educationalContext) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_domain FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_subject FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_module FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_specialization FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_topic FOREIGN KEY (taxon) REFERENCES Taxon (id),
+  ADD CONSTRAINT FK_TaxonPosition_subtopic FOREIGN KEY (taxon) REFERENCES Taxon (id);
+
 SET foreign_key_checks = 1;
-
-
