ca9cadaced000d884e8514a1fa0731c5971df780
Marek Reinhold <marek.reinhold@outlook.com>
2019-02-05 16:41:39 +0200

    EK-269 migrations to spring boot

---
+++ spring-back-end/src/main/resources/db/migration/V37.9__Release_migration_1_30_0.sql
@@ -0,0 +1,17 @@
+SET foreign_key_checks = 0;
+
+CREATE TABLE TaxonPosition
+(
+  taxon              BIGINT NOT NULL,
+  educationalContext BIGINT NOT NULL,
+  domain             BIGINT NULL,
+  subject            BIGINT NULL,
+  module             BIGINT NULL,
+  specialization     BIGINT NULL,
+  topic              BIGINT NULL,
+  subtopic           BIGINT NULL
+);
+
+SET foreign_key_checks = 1;
+
+
