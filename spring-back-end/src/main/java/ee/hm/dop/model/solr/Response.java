956d12facda4c7359cd37681865afc2499fa6e4c
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-02-06 14:18:46 +0200

    EK-97 ignore unknown json properties

--- spring-back-end/src/main/java/ee/hm/dop/model/solr/Response.java
+++ spring-back-end/src/main/java/ee/hm/dop/model/solr/Response.java
@@ -1,9 +1,11 @@
 package ee.hm.dop.model.solr;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Response {
 
     @JsonProperty("doclist")
