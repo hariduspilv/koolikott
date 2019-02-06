9724b26dd2c2649a67b88ece0244ed304a9ecd1d
Marek Reinhold <marek.reinhold@outlook.com>
2019-02-06 16:06:47 +0200

    EK-206 - version change

--- back-end/src/main/java/ee/hm/dop/rest/VersionResource.java
+++ back-end/src/main/java/ee/hm/dop/rest/VersionResource.java
@@ -11,6 +11,6 @@ public class VersionResource extends BaseResource{
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getVersion() {
-        return "1.29.2";
+        return "1.30.0";
     }
 }
