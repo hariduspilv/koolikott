919a7ccfc2707be09d85f6d8e29dafe10a4b96cd
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-02-07 17:51:24 +0200

    EK-79 fix captcha

--- spring-back-end/src/main/java/ee/hm/dop/rest/CaptchaKeyResource.java
+++ spring-back-end/src/main/java/ee/hm/dop/rest/CaptchaKeyResource.java
@@ -1,6 +1,7 @@
 package ee.hm.dop.rest;
 
 import org.apache.commons.configuration2.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,12 @@ import static ee.hm.dop.utils.ConfigurationProperties.CAPTCHA_KEY;
 
 @RestController
 @RequestMapping("/captcha")
-public class CaptchaKeyResource extends BaseResource{
+public class CaptchaKeyResource extends BaseResource {
 
     @Inject
     private Configuration configuration;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     public String getKey() {
         return configuration.getString(CAPTCHA_KEY);
     }
