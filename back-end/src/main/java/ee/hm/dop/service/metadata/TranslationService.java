24dc9fcfab93d99ee61566ba92e24884d4eacc84
Marek Reinhold <marek.reinhold@outlook.com>
2019-02-07 11:21:37 +0200

    EK-284-back && front refactor

--- back-end/src/main/java/ee/hm/dop/service/metadata/TranslationService.java
+++ back-end/src/main/java/ee/hm/dop/service/metadata/TranslationService.java
@@ -14,7 +14,6 @@ public class TranslationService {
 
     public static final String DOMAIN = "DOMAIN_";
     public static final String SUBJECT = "SUBJECT";
-    public static final String HELPER = "HELPER_";
     public static final String LANDING_PAGE_DESCRIPTION = "LANDING_PAGE_DESCRIPTION";
     public static final String LANDING_PAGE_NOTICE = "LANDING_PAGE_NOTICE";
     @Inject
@@ -36,41 +35,8 @@ public class TranslationService {
             return null;
         }
 
-        //if (LanguageC.ENG.equals(language.getCode()) || LanguageC.RUS.equals(language.getCode())) {
-        //    Map<String, String> map = getDomainsAndSubjectsInEstonian();
-        //    if (map != null) {
-        //        map.putAll(translationGroupFor.getTranslations());
-        //        return map;
-        //    }
-        //}
 
         return translationGroupFor.getTranslations();
-    }
-
-    /**
-     * Temporary method that adds Estonian translations to eng and rus translation maps,
-     * because some taxons are not yet translated into all three languages.
-     * These translations are used in front-end to remove duplicate translations.
-     * For example:
-     * "Matemaatika" and "Mathematics" should be same, but since some taxons don't have translations
-     * "Matemaatika" is same in all three languages. As a workaround taxons are compared in estonian.
-     * <p>
-     * This method can be removed after all taxons are translated
-     *
-     * @return
-     */
-    private Map<String, String> getDomainsAndSubjectsInEstonian() {
-        Language language = languageDao.findByCode(LanguageC.EST);
-        if (language == null) {
-            return null;
-        }
-        TranslationGroup translationGroupFor = translationGroupDao.findTranslationGroupFor(language);
-        if (translationGroupFor == null) {
-            return null;
-        }
-        return translationGroupFor.getTranslations().entrySet().stream()
-                .filter(entry -> (entry.getKey().startsWith(DOMAIN) || entry.getKey().startsWith(SUBJECT)))
-                .collect(Collectors.toMap(entry -> HELPER.concat(entry.getKey()), Map.Entry::getValue));
     }
 
     public String getTranslationKeyByTranslation(String translation) {
