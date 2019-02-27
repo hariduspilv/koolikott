2dc8f7f5daa4d682196945f77870008df44794b2
Marek Reinhold <marek.reinhold@outlook.com>
2019-02-27 10:22:37 +0200

    EK-212

--- front-end/app/directives/sendEmail/sendEmailDialog.js
+++ front-end/app/directives/sendEmail/sendEmailDialog.js
@@ -5,9 +5,13 @@
         constructor(...args) {
             super(...args)
 
+            this.$scope.emailObject = {}
             this.$scope.captchaKey = ''
             this.getCaptchaKey()
             this.$scope.isSendBtnVisible = false
+
+            this.$scope.emailContent = ''
+            this.$scope.isSaving = false
 
             this.$scope.cancel = () => {
                 this.$mdDialog.hide();
@@ -16,12 +20,25 @@
 
         isSendButtonDisabled(){
 
-            return this.$scope.captchaSuccess
+            // const {emailContent} = this.$scope.emailObject;
+            if (this.$scope.emailContent.length > 1)
+                return true
+
+            // return this.emailContent.length > 1;
+            // return !this.$scope.captchaSuccess || this.emailContent.length < 1;
         }
 
+        // check()
+
         sendEmail(){
-            let content
+            let content;
+
             this.$scope.isSaving = true
+
+            if (this.$scope.emailContent.length > 0)
+                content = this.$scope.emailContent
+
+            const {emailcontent} = this.$scope.emailObject
 
             this.serverCallService.makePost('/rest/admin/???',  {})
                 .then(response => {
