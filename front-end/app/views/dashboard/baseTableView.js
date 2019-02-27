c39a7b90160e4bdf017bd721bac52f571f5de432
Marek Reinhold <marek.reinhold@outlook.com>
2019-02-27 09:16:10 +0200

    EK-302

--- front-end/app/views/dashboard/baseTableView.js
+++ front-end/app/views/dashboard/baseTableView.js
@@ -45,7 +45,7 @@ class controller extends Controller {
 
         this.getModerators();
         this.getMaximumUnreviewed();
-        this.sortedBy = 'byCreatedAt';
+        this.sortedBy = '-byCreatedAt';
         this.$scope.isFiltering = false
         this.$scope.isTaxonSelectVisible = true
         this.$scope.isExpertsSelectVisible = true
