49bc57e68d675e657dc24a65e2f169ef05860446
martin.kokser <martin.kokser@netgroup.ee>
2019-03-26 13:26:48 +0200

    changed version number from 1.31.0 to 1.32.0, added changelog

--- back-end/src/main/java/ee/hm/dop/rest/VersionResource.java
+++ back-end/src/main/java/ee/hm/dop/rest/VersionResource.java
@@ -11,6 +11,6 @@ public class VersionResource extends BaseResource{
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getVersion() {
-        return "1.31.0";
+        return "1.32.0";
     }
 }
