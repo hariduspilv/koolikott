7b1c4b0701ceff0733ce3ee65c4439bcc0e7868a
Marek Reinhold <marek.reinhold@outlook.com>
2019-03-28 11:20:58 +0200

    EK-210 migra+cleanup

--- front-end/app/directives/relatedPortfolios/relatedPortfolios.component.js
+++ front-end/app/directives/relatedPortfolios/relatedPortfolios.component.js
@@ -68,10 +68,6 @@
 
     controller.$inject = [
         '$scope',
-        '$rootScope',
-        '$location',
-        '$mdDialog',
-        'dialogService',
         'materialService',
         'authenticatedUserService'
     ]
@@ -80,18 +76,18 @@
         controller,
         template: `
               <div data-ng-if="relatedPortfolios.length > 0" class="portfolio-material" data-layout="column">
-            <span data-translate="SEOTUD_KOGUMIKUD"></span>
+            <span data-translate="PORTFOLIO_RELATED_PORTFOLIOS"></span>
             <div data-ng-repeat="portfolio in relatedPortfolios | limitTo : limit : startFrom " flex data-layout="row">
               <a
                 data-ng-style="{'pointer-events':$ctrl.userCanClickPortfolio(portfolio)}"
                 target="_blank"
-                data-ng-href="{{'/portfolio?id=' + portfolio.id}}">{{portfolio.title}}<span style="margin-left: 0px" data-ng-if="$ctrl.userCanClickPortfolio(portfolio) === 'none'" data-translate="PORTFOLIO_PRIVATE"></span></a>
+                data-ng-href="{{'/portfolio?id=' + portfolio.id}}">{{portfolio.title}}<span class="span-margin-left" data-ng-if="$ctrl.userCanClickPortfolio(portfolio) === 'none'" data-translate="PORTFOLIO_PRIVATE"></span></a>
             </div>
             <button
-              data-ng-show="showNextButton()" data-ng-click="showNextItems()">Show more
+              data-ng-show="showNextButton()" data-ng-click="showNextItems()"><span class="span-margin-left" data-translate="PORTFOLIO_SHOW_MORE"></span>
             </button>
             <button
-              data-ng-show="showFirstButton()" data-ng-click="reset()">Show less
+              data-ng-show="showFirstButton()" data-ng-click="reset()"><span class="span-margin-left" data-translate="PORTFOLIO_SHOW_LESS"></span>
             </button>
           </div>
         `,
