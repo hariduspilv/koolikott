8208c36d8a40ab9c262b1a6d9bd60e19b232daa2
Marek Reinhold <marek.reinhold@outlook.com>
2019-04-01 16:28:52 +0300

    EK-400,EK-410 - test,login modal

--- back-end/src/test/java/ee/hm/dop/service/login/HaridServiceTest.java
+++ back-end/src/test/java/ee/hm/dop/service/login/HaridServiceTest.java
@@ -43,6 +43,7 @@ public class HaridServiceTest {
 
     @Before
     public void setUp() throws Exception {
+
         String clientString = "clientsecret";
         haridService.postConstruct(clientString);
     }
