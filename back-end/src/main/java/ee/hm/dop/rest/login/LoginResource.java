0e46506d0cff1a14d4ab6d92fa27bdcc69a30066
Marek Reinhold <marek.reinhold@outlook.com>
2019-04-04 09:02:57 +0300

    EK-409 scope parameter modif

--- back-end/src/main/java/ee/hm/dop/rest/login/LoginResource.java
+++ back-end/src/main/java/ee/hm/dop/rest/login/LoginResource.java
@@ -38,7 +38,7 @@ public class LoginResource extends BaseResource {
     private static final String EKOOL_CALLBACK_PATH = "/rest/login/ekool/success";
     private static final String EKOOL_AUTHENTICATION_URL = "%s?client_id=%s&redirect_uri=%s&scope=read&response_type=code";
     private static final String STUUDIUM_AUTHENTICATION_URL = "%sclient_id=%s";
-    private static final String HARID_AUTHENTICATION_URL = "%s?client_id=%s&redirect_uri=%s&scope=name&response_type=code";
+    private static final String HARID_AUTHENTICATION_URL = "%s?client_id=%s&redirect_uri=%s&scope=profile&response_type=code";
     private static final String HARID_AUTHENTICATION_SUCCESS_URL = "/rest/login/harid/success";
     private static final String HARID_AUTHENTICATION_FAILURE_URL = "/rest/login/harid/failure";
     public static final String LOGIN_REDIRECT_WITH_TOKEN_AGREEMENT = "%s/#!/loginRedirect?token=%s&agreement=%s&existingUser=%s&loginFrom=%s";
