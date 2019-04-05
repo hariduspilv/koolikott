3d4b29746c4c7fd97559cc558cda2269e32acfd5
Marek Reinhold <marek.reinhold@outlook.com>
2019-04-05 16:22:45 +0300

    EK-400 migration

--- back-end/src/main/resources/db/migration/V39.5__Release_migration_1_34_0.sql
+++ back-end/src/main/resources/db/migration/V39.5__Release_migration_1_34_0.sql
@@ -1,5 +1,7 @@
 SET foreign_key_checks = 0;
 
-call insert_translation('ERROR_LOGIN_FAILED_HARID', 'Sisselogimine ebaõnnestus. HarID-s oled registreeritud kasutaja, kelle infost puudub isikukood. Logi sisse HarID-sse, salvesta sealse konto infosse oma isikukood ja tule siis e-Koolikotti tagasi. E-Koolikotti saad sisse logida ka ID-kaardi ja mID-ga..', 'Login failed. You are a registered user of HarID whose info is missing personal ID code. Login to HarID, save your personal ID code in you profile and return to e-Koolikott. You can also login to e-Koolikott with ID-card and mID.');
+call insert_translation('ERROR_LOGIN_FAILED_HARID', 'Sisselogimine ebaõnnestus. Oled HarID-s kasutaja, kellel puudub isikukood.
+HarID kaudu e-Koolikotti sisenemiseks logi esmalt HarID-sse sisse ning salvesta oma kontole isikukood. Seejärel sulge browseri aken ja logi uues aknas eKoolikotti HarID konto kaudu.',
+'Login failed. You are a registered user of HarID whose info is missing personal ID code. Login to HarID, save your personal ID code in you profile, close browser and return to e-Koolikott in new browser window with HarID.');
 
 SET foreign_key_checks = 1;
