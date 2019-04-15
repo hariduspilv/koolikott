bf7a1000744b01648a2f290adeb6e21330219323
Marek Reinhold <marek.reinhold@outlook.com>
2019-04-15 09:31:05 +0300

    EK-393 translation migration

--- back-end/src/main/resources/db/migration/V39.8__Release_migration_1_34_0.sql
+++ back-end/src/main/resources/db/migration/V39.8__Release_migration_1_34_0.sql
@@ -5,5 +5,6 @@ call insert_translation('EMAIL_LEARNINGOBJECT_TITLE', 'Õppevara pealkiri', 'Lea
 call insert_translation('EMAIL_CONTENT', 'Sisu', 'Content');
 call insert_translation('EMAIL_SENT_AT', 'Saadetud', 'Sent');
 call insert_translation('EMAIL_SENT_EMAILS', 'Minu saadetud teated', 'My sent emails');
+call insert_translation('EMAIL_SENT_EMAILS_CREATOR', 'Õppevara loojale/koostajale saadetud küsimus', 'Question sent to author/creator');
 
 SET foreign_key_checks = 1;
