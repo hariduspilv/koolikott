261af7891da6843aa96a1a68b768d4125b7c6e81
Marek Reinhold <marek.reinhold@outlook.com>
2019-05-03 10:46:37 +0300

    sendToCreator email content fix

--- spring-back-end/src/main/java/ee/hm/dop/service/SendMailService.java
+++ spring-back-end/src/main/java/ee/hm/dop/service/SendMailService.java
@@ -155,11 +155,13 @@ public class SendMailService {
 
     public Email sendEmailToCreator(EmailToCreator emailToCreator) {
         return EmailBuilder.startingBlank()
-                .from(emailToCreator.getSenderName(), emailToCreator.getSenderEmail())
+                .from("e-Koolikott", configuration.getString(EMAIL_NO_REPLY_ADDRESS))
                 .to(emailToCreator.getUser().getFullName(), emailToCreator.getCreatorEmail())
                 .withSubject("e-Koolikott: Aineeksperdi küsimus")
-                .withHTMLText("Aineekspert on esitanud küsimuse Teie õppevara kohta: " + emailToCreator.getLearningObjectTitle() + BREAK
-                        + BREAK + emailToCreator.getMessage())
+                .withHTMLText("Aineekspert " +  emailToCreator.getSenderName() + " on esitanud küsimuse Teie õppevara kohta: " + emailToCreator.getLearningObjectTitle() + BREAK
+                        + BREAK + emailToCreator.getMessage() + BREAK + BREAK +
+                        "Aineeksperdile vastamiseks kirjutage e-posti aadressile:" + "<a href=\"mailto:" + emailToCreator.getSenderName() +
+                        "\">" + emailToCreator.getSenderEmail() + "</a>")
                 .buildEmail();
     }
 
