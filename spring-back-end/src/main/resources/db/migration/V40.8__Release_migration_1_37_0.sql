59e20e0bfffba5fc07aa74e6bcbaf45ed500e9d6
Marek Reinhold <marek.reinhold@outlook.com>
2019-06-18 10:10:04 +0300

    EK-533 translations,cookie bind html

---
+++ spring-back-end/src/main/resources/db/migration/V40.8__Release_migration_1_37_0.sql
@@ -0,0 +1,15 @@
+SET foreign_key_checks = 0;
+
+call insert_translation('INTRO_TEXT', 'Pealkiri', 'Receiver');
+call insert_translation('FRONT_PAGE_FILTERED_TITLE', 'Õppevara pealkiri', 'Learningobject title');
+call insert_translation('FRONT_PAGE_VIDEO_URL', 'url', 'url');
+call insert_translation('COOKIE_EDIT', 'Muuda küpsise sisu', 'Change cookie content');
+call insert_translation('COOKIE_TEXT_LABEL', 'Küpsise sisu', 'Cookie content');
+call insert_translation('COOKIE_NOTICE_UPDATED', 'Küpsise teade muudetud.', 'Cookie notice updated.');
+call insert_translation('FRONT_PAGE_HEADLINE_TITLE_UPDATED', 'Pealkiri uuendatud.', 'Title updated.');
+call insert_translation('FRONT_PAGE_CHANGES_SAVED', 'Esilehe muudatused salvestatud.', 'Frontpage changes saved.');
+call insert_translation('FRONT_PAGE_VIDEO_URL_LABEL', 'Video url', 'Video url');
+
+SET foreign_key_checks = 1;
+
+
