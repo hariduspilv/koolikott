9130cbd3ea3aa9562ee015096286c95e5f41fe9d
Marek Reinhold <marek.reinhold@outlook.com>
2019-07-25 08:50:32 +0300

    EK-620 material file and picture upload fail toast message

---
+++ spring-back-end/src/main/resources/db/migration/V42.2__Release_migration_1_38_0.sql
@@ -0,0 +1,5 @@
+SET foreign_key_checks = 0;
+
+call insert_translation('MATERIAL_FAIL_UPLOAD_FAIL', 'Faili üleslaadimine ebaõnnestus, proovige uuesti', 'File upload failed, try again');
+
+SET foreign_key_checks = 1;
