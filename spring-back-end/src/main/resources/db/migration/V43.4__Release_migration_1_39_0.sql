d9720cca86b0083a6e0220391898704d027b1844
Marek Reinhold <marek.reinhold@outlook.com>
2019-08-16 13:06:33 +0300

    EK-692 changed condition back&EK-668 translation migra

--- spring-back-end/src/main/resources/db/migration/V43.4__Release_migration_1_39_0.sql
+++ spring-back-end/src/main/resources/db/migration/V43.4__Release_migration_1_39_0.sql
@@ -1,5 +1,5 @@
 SET foreign_key_checks = 0;
 
-call insert_translation('PROFILE_PAGE_TITLE_LEARNINGOBJECTS', 'Kasutaja ${user} õppevarad, ${count}tk', 'User ${user} learningobjects, ${count}pcs');
+call insert_translation('PROFILE_PAGE_TITLE_LEARNINGOBJECTS', 'Kasutaja ${user} õppevarad - ${count}tk', 'User ${user} learningobjects - ${count}pcs');
 
 SET foreign_key_checks = 1;
