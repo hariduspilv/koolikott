00cf4ae0c1ab1f745aaa0a7e0d324c5d53480867
Marek Reinhold <marek.reinhold@outlook.com>
2019-09-11 10:00:09 +0300

    EK-722 in case of move up/down updateChapterState

--- front-end/app/views/editPortfolio/editPortfolio.js
+++ front-end/app/views/editPortfolio/editPortfolio.js
@@ -77,7 +77,6 @@ class controller extends Controller {
     }
     move(idx, up = false) {
         this.updateChaptersStateFromEditors();
-        this.updateChapterEditorsFromState();
         this.$scope.portfolio.chapters.splice(
             up  ? idx - 1
                 : idx + 1,
