f743b275a6387f5263cadf67f9ecfefdba44ca15
Joosep Noot <joosep.noot@netgroup.com>
2019-09-30 08:09:47 +0300

    EK-728 add license type enum documentation

--- spring-back-end/src/main/java/ee/hm/dop/model/enums/LicenseType.java
+++ spring-back-end/src/main/java/ee/hm/dop/model/enums/LicenseType.java
@@ -2,6 +2,16 @@ package ee.hm.dop.model.enums;
 
 public enum LicenseType {
 
-    ALLRIGHTSRESERVED, CCBY, CCBYSA, CCBYND, CCBYNC, CCBYNCSA, CCBYNCND, YOUTUBE, CCBYSA30
+    /**
+     * ALLRIGHTSRESERVED, CCBY, CCBYSA, CCBYND, CCBYNC, CCBYNCSA, CCBYNCND, YOUTUBE
+     * are licenses which can be found on educational content which is created before the release of version 1.50.0
+     * These licenses will now be replaced to CCBYSA30 with scripts or during user logins
+     */
+    ALLRIGHTSRESERVED, CCBY, CCBYSA, CCBYND, CCBYNC, CCBYNCSA, CCBYNCND, YOUTUBE,
+
+    /**
+     * CCBYSA30 is only acceptable licenseType for new educational content to be public.
+     */
+    CCBYSA30
 
 }
