d6f3e20c653db3a18a3fcc9ca112e6b233bacb6e
joosep <joosep.noot@netgroup.com>
2019-10-10 15:01:37 +0300

    EK-768 update translation

---
+++ spring-back-end/src/main/resources/db/migration/V44.22__Release_migration_1_40_0.sql
@@ -0,0 +1,5 @@
+SET foreign_key_checks = 0;
+
+call update_translations ('MATERIAL_IS_DELETED', 'Materjal on kustutatud', 'Material is deleted', 'Materjal on kustutatud (RU)');
+
+SET foreign_key_checks = 1;
