f0b880ca25c6af4b9ab8e4f5d3e64da738c9931e
joosep <joosep.noot@netgroup.com>
2019-10-10 14:51:46 +0300

    EK-768 update translation

---
+++ spring-back-end/src/main/resources/db/migration/V44.21__Release_migration_1_40_0.sql
@@ -0,0 +1,5 @@
+SET foreign_key_checks = 0;
+
+call update_translations ('PORTFOLIO_DELETED_FROM', 'kogumikust (kustutatud)', 'portfolio (deleted)', 'kogumikust (kustutatud) (RU)');
+
+SET foreign_key_checks = 1;
