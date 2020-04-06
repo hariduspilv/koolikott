dc2aa02a6571ce37b85f292c42aaa67cb2e1eed0
Mihkel Meiusi <mihkel.meiusi@netgroup.com>
2020-04-06 11:19:53 +0000

    Merged in EK-1007 (pull request #30)
    Added info to LearningObject licenses
    
    * Added info to LearningObject licenses
    
    Approved-by: Martin Kokser

---
+++ spring-back-end/src/main/resources/db/migration/V202004021409__JIRA-1007-add-license-info.sql
@@ -0,0 +1,5 @@
+SET foreign_key_checks = 0;
+
+call insert_translation('LICENSE_INFO', 'Litsents kehtib ainult E-koolikotis asuvale tekstile ja üles laetud failidele, ei laiene lingiga jagatud sisule.', 'This license applies to the text and files uploaded to E-koolikott, but not to external links.');
+
+SET foreign_key_checks = 1;
