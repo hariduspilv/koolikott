d7d433c42e8c8a50a7a4d2764ed07641ec2b1c9f
Mihkel Meiusi <mihkel.meiusi@netgroup.com>
2020-04-13 12:51:52 +0300

    Updated logging

--- spring-back-end/src/main/java/ee/hm/dop/service/content/PortfolioMaterialService.java
+++ spring-back-end/src/main/java/ee/hm/dop/service/content/PortfolioMaterialService.java
@@ -68,8 +68,8 @@ public class PortfolioMaterialService {
 
     private void saveNew(Portfolio portfolio, List<Long> newToSave) {
         for (Long materialId : newToSave) {
-            logger.info(portfolio.getId().toString());
-            logger.info(materialId.toString());
+            logger.info("Portfolio id:" + portfolio.getId().toString());
+            logger.info("Material id:" + materialId.toString());
             Material material = materialDao.findById(materialId);
             portfolioMaterialDao.createOrUpdate(new PortfolioMaterial(portfolio, material));
         }
